6b333d5affcb7a05433769b7fa000803d268d227
Jaromir Benes <jaromir.benes@gmail.com>
2017-02-06 20:45:19 +0100

    Prepare irisgenpath for new help folder name, #help

--- irisgenpath.m
+++ irisgenpath.m
@@ -1,5 +1,5 @@
-function [Path,All] = irisgenpath(Root)
-% irisgenpath  [Not a public function] Generate IRIS folder names that need to be added on Matlab search path.
+function [ppath, everything] = irisgenpath(root)
+% irisgenpath  Generate IRIS folder names that need to be added on Matlab search path.
 %
 % Backend IRIS function.
 % No help provided.
@@ -8,47 +8,47 @@ function [Path,All] = irisgenpath(Root)
 % -Copyright (c) 2007-2017 IRIS Solutions Team.
 
 try
-    Root; %#ok<VUNUS>
+    root; %#ok<VUNUS>
 catch
-    Root = irisroot( );
+    root = irisroot( );
 end
 
 %--------------------------------------------------------------------------
 
 % All first-level folders in IRIS root.
-list = dir(Root);
+list = dir(root);
 list = list([list.isdir]);
 
-Path = struct( );
-Path.Begin = { }; % addpath -begin.
-Path.End = { }; % addpath -end.
-Path.OctBegin = { }; % addpath -begin in Octave.
-Path.OctEnd = { }; % addpath -end in Octave.
+ppath = struct( );
+ppath.Begin = { }; % addpath -begin.
+ppath.End = { }; % addpath -end.
+ppath.OctBegin = { }; % addpath -begin in Octave.
+ppath.OctEnd = { }; % addpath -end in Octave.
 
 for i = 1 : length(list)
     name = list(i).name;
-    % Exclude folders starting with special characters + @ - ^.
-    if strncmp(name,'.',1) ...
-            || strncmp(name,'+',1) ...
-            || strncmp(name,'@',1) ...
-            || strncmp(name,'-',1) ...
-            || strncmp(name,'^',1) ...
+    % Exclude folders starting with special characters + @ - $.
+    if strncmp(name, '.', 1) ...
+            || strncmp(name, '+', 1) ...
+            || strncmp(name, '@', 1) ...
+            || strncmp(name, '-', 1) ...
+            || strncmp(name, '#', 1)
         continue
     end
-    if strcmp(name,'octave')
-        Path.OctBegin{end+1} = fullfile(Root,name);
+    if strcmp(name, 'octave')
+        ppath.OctBegin{end+1} = fullfile(root, name);
         continue
     end
-    if strcmp(name,'_octave')
-        Path.OctEnd{end+1} = fullfile(Root,name);
+    if strcmp(name, '_octave')
+        ppath.OctEnd{end+1} = fullfile(root, name);
         continue
     end
-    if strncmp(name,'_',1)
-        Path.End{end+1} = fullfile(Root,name);
+    if strncmp(name, '_', 1)
+        ppath.End{end+1} = fullfile(root, name);
     end
-    Path.Begin{end+1} = fullfile(Root,name);
+    ppath.Begin{end+1} = fullfile(root, name);
 end
 
-All = [Path.OctBegin,Path.Begin,Path.End,Path.OctEnd];
+everything = [ppath.OctBegin, ppath.Begin, ppath.End, ppath.OctEnd];
 
 end
--- irispathmanager.m
+++ irispathmanager.m
@@ -1,5 +1,5 @@
-function varargout = irispathmanager(Req,varargin)
-% irispathmanager  [Not a public function] IRIS path manager.
+function varargout = irispathmanager(req, varargin)
+% irispathmanager  IRIS path manager.
 %
 % Backend IRIS function.
 % No help provided.
@@ -9,26 +9,26 @@ function varargout = irispathmanager(Req,varargin)
 
 %--------------------------------------------------------------------------
 
-switch lower(Req)
+switch lower(req)
     case 'cleanup'
         % Remove all IRIS roots and subs found on the Matlab temporary
         % and permanent search paths.
         thisRoot = fileparts(mfilename('fullpath'));
-        list = which('irisstartup.m','-all');
+        list = which('irisstartup.m', '-all');
         removed = { };
         for i = 1 : numel(list)
             root = fileparts(list{i});
-            if isempty(root) || strcmpi(root,thisRoot)
+            if isempty(root) || strcmpi(root, thisRoot)
                 continue
             end
-            [~,allp] = irisgenpath(root);
-            xxRmPath(allp{:},root);
+            [~, allp] = irisgenpath(root);
+            removePath(allp{:}, root);
             removed{end+1} = root; %#ok<AGROW>
         end
-        % Remove the current IRIS root last; otherwise `irisgenpath( )` will not be
+        % Remove the current IRIS root last; otherwise irisgenpath( ) will not be
         % found.
-        [~,allp] = irisgenpath(thisRoot);
-        xxRmPath(allp{:},thisRoot);
+        [~, allp] = irisgenpath(thisRoot);
+        removePath(allp{:}, thisRoot);
         removed{end+1} = thisRoot;
         
         varargout{1} = removed;
@@ -36,30 +36,30 @@ switch lower(Req)
     
     case 'addroot'
         % Add the specified root to the temporary search paths.
-        addpath(varargin{1},'-begin');
+        addpath(varargin{1}, '-begin');
     
     case 'addcurrentsubs'
         % Add subfolders within the current root to the temporary
         % search path.
-        [p,allp] = irisgenpath( );
+        [p, allp] = irisgenpath( );
         if true % ##### MOSW
             % Do nothing.
         else 
             if ~isempty(p.OctBegin) %#ok<UNRCH>
-                addpath(p.OctBegin{:},'-begin');
+                addpath(p.OctBegin{:}, '-begin');
             end
         end
         if ~isempty(p.Begin)
-            addpath(p.Begin{:},'-begin');
+            addpath(p.Begin{:}, '-begin');
         end
         if ~isempty(p.End)
-            addpath(p.End{:},'-end');
+            addpath(p.End{:}, '-end');
         end
         if true % ##### MOSW
             % Do nothing.
         else
             if ~isempty(p.OctEnd) %#ok<UNRCH>
-                addpath(p.OctEnd{:},'-end');
+                addpath(p.OctEnd{:}, '-end');
             end
         end
         varargout{1} = allp;
@@ -67,26 +67,22 @@ switch lower(Req)
     case 'removecurrentsubs'
         % Remove subfolders within the current root from the temporary
         % and permanent search paths.
-        [~,allp] = irisgenpath( );
-        xxRmPath(allp{:});
+        [~, allp] = irisgenpath( );
+        removePath(allp{:});
         varargout{1} = allp;
 end
 
 end
 
 
-% Subfunctions...
 
 
-%**************************************************************************
-
-
-function xxRmPath(varargin)
+function removePath(varargin)
 if isempty(varargin)
     return
 end
-status = warning('query','all');
-warning('off','MATLAB:rmpath:DirNotFound');
+status = warning('query', 'all');
+warning('off', 'MATLAB:rmpath:DirNotFound');
 rmpath(varargin{:});
 warning(status);
-end % xxRmPath( )
+end
