3b799f7326813d05a7be1cf2dd83688a9ffecd6b
Jaromir Benes <jaromir.benes@gmail.com>
2017-07-25 17:28:12 +0200

    Stop tracking frontend/@Series

--- frontend/@Series/Series.m
+++
@@ -1,8 +0,0 @@
-classdef (InferiorClasses={?matlab.graphics.axis.Axes, ?dates.Date}) ...
-        Series < tseries
-    methods
-        function this = Series(varargin)
-            this = this@tseries(varargin{:});
-        end
-    end
-end
