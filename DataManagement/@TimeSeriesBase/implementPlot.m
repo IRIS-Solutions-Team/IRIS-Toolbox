b6e3b1a4816818554ac2e202659a364b6671a3c1
Jaromir Benes <jaromir.benes@gmail.com>
2017-10-23 18:35:59 +0400

    Amend previous commit.

--- DataManagement/@TimeSeriesBase/implementPlot.m
+++ DataManagement/@TimeSeriesBase/implementPlot.m
@@ -1,4 +1,4 @@
-function hPlot = implementPlot(plotFun, varargin)
+function [hPlot, time, yData] = implementPlot(plotFun, varargin)
 % implementPlot  Plot functions for TimeSeriesBase objects.
 %
 % Backend function.
@@ -6,27 +6,32 @@ function hPlot = implementPlot(plotFun, varargin)
 
 % -Copyright (c) 2017 OGResearch Ltd.
 
+IS_ROUND = @(x) isnumeric(x) && all(x==round(x));
+
 persistent INPUT_PARSER
 if isempty(INPUT_PARSER)
-    INPUT_PARSER = extend.InputParser(['TimeSeriesBase/implementPlot', char(plotFun)]);
+    INPUT_PARSER = extend.InputParser(['TimeSeriesBase.implementPlot(', char(plotFun), ')']);
     INPUT_PARSER.KeepUnmatched = true;
-    INPUT_PARSER.addRequired('PlotFun', @(x) isequal(x, @plot));
+    INPUT_PARSER.addRequired('PlotFun', @(x) isequal(x, @plot) || isequal(x, @bar) || isequal(x, @area) || isequal(x, @stem));
     INPUT_PARSER.addRequired('Axes', @(x) isempty(x) || (isgraphics(x) && length(x)==1));
-    INPUT_PARSER.addRequired('Time', @(x) isa(x, 'Date') || isa(x, 'DateWrapper') || isequal(x, Inf) || isempty(x));
+    INPUT_PARSER.addRequired('Time', @(x) isa(x, 'Date') || isa(x, 'DateWrapper') || isequal(x, Inf) || isempty(x) || IS_ROUND(x));
     INPUT_PARSER.addRequired('Series', @(x) isa(x, 'TimeSeriesBase') && ~iscell(x.Data));
     INPUT_PARSER.addParameter('DateFormat', @default, @(x) isequal(x, @default) || ischar(x));
-    INPUT_PARSER.addParameter('DatePosition', 'middle', @(x) any(strcmpi(x, {'start', 'middle', 'end'})));
+    INPUT_PARSER.addParameter('PositionWithinPeriod', @auto, @(x) isequal(x, @auto) ||  any(strcmpi(x, {'start', 'middle', 'end'})));
 end
 
 if isgraphics(varargin{1})
-    hAxes = varargin{1};
+    handleAxes = varargin{1};
     varargin(1) = [ ];
 else
-    hAxes = gca( );
+    handleAxes = gca( );
 end
 
 if isa(varargin{1}, 'Date')  || isa(varargin{1}, 'DateWrapper') || isequal(varargin{1}, Inf)
     time = varargin{1};
+    varargin(1) = [ ];
+elseif ~isequal(varargin{1}, Inf) && IS_ROUND(varargin{1})
+    time = DateWrapper.fromDouble(varargin{1});
     varargin(1) = [ ];
 else
     time = Inf;
@@ -35,9 +40,9 @@ end
 this = varargin{1};
 varargin(1) = [ ];
 
-INPUT_PARSER.parse(plotFun, hAxes, time, this, varargin{:});
+INPUT_PARSER.parse(plotFun, handleAxes, time, this, varargin{:});
 opt = INPUT_PARSER.Results;
-unmatchedOptions = INPUT_PARSER.Unmatched;
+unmatchedOptions = INPUT_PARSER.UnmatchedInCell;
 
 enforceXLim = true;
 if isequal(time, Inf)
@@ -58,21 +63,50 @@ assert( ...
 
 %--------------------------------------------------------------------------
 
-yData = getData(this, time);
+[yData, time] = getData(this, time);
 timeFrequency = getFrequency(time);
 
-if timeFrequency==Frequency.INTEGER
-    xData = getSerial(time);
-else
-    xData = datetime(time, lower(opt.DatePosition));
-    if ~isequal(opt.DateFormat, @default)
-        xData.Format = opt.DateFormat;
-    end
-end
+positionWithinPeriod = resolvePositionWithinPeriod( );
+xData = createDateAxis( );
 
-hPlot = plotFun(hAxes, xData, yData, unmatchedOptions);
+hPlot = plotFun(handleAxes, xData, yData, unmatchedOptions{:});
 if enforceXLim
-    set(hAxes, 'XLim', xData([1, end]));
+    set(handleAxes, 'XLim', xData([1, end]));
 end
 
+setappdata(handleAxes, 'IRIS_PositionWithinPeriod', positionWithinPeriod);
+
+return
+
+
+    function positionWithinPeriod = resolvePositionWithinPeriod( )
+        axesPositionWithinPeriod = getappdata(handleAxes, 'IRIS_PositionWithinPeriod');
+        positionWithinPeriod = opt.PositionWithinPeriod;
+        if isempty(axesPositionWithinPeriod) 
+            if isequal(positionWithinPeriod, @auto)
+                positionWithinPeriod = 'start';
+            end
+        else
+            if isequal(positionWithinPeriod, @auto)
+                positionWithinPeriod = axesPositionWithinPeriod;
+            elseif ~isequal(axesPositionWithinPeriod, positionWithinPeriod)
+                warning( ...
+                    'TimeSeriesBase:implementPlot:DifferentPositionWithinPeriod', ...
+                    'Option PositionWithinPeriod= differs from the value set in the current Axes.' ...
+                );
+            end
+        end
+    end
+
+
+    function xData = createDateAxis( )
+        if timeFrequency==Frequency.INTEGER
+            xData = getSerial(time);
+        else
+            xData = datetime(time, lower(positionWithinPeriod));
+            if ~isequal(opt.DateFormat, @default)
+                xData.Format = opt.DateFormat;
+            end
+        end
+    end
 end
--- DataManagement/@TimeSeriesBase/plot.m
+++
@@ -1,5 +0,0 @@
-function varargout = plot(varargin)
-
-[varargout{1:nargout}] = implementPlot(@plot, varargin{:});
-
-end
