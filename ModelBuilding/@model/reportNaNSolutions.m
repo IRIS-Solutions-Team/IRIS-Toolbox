bd44d9507220bfcec2f003048c1030be7f1555e6
Jaromir Benes <jaromir.benes@gmail.com>
2018-01-09 19:39:51 +0100

    Add mode/reportNaNSolution.

---
+++ ModelBuilding/@model/reportNaNSolutions.m
@@ -0,0 +1,20 @@
+function indexNaNSolutions = reportNaNSolutions(this)
+% reportNaNSolutions  Throw warnings for parameter variants for which solution does not exist
+%
+% Backend IRIS function.
+% No help provided.
+
+% -IRIS Macroeconomic Modeling Toolbox.
+% -Copyright (c) 2007-2017 IRIS Solutions Team.
+
+%--------------------------------------------------------------------------
+
+[~, indexNaNSolutions] = isnan(this, 'Solution');
+
+assert( ...
+    ~any(indexNaNSolutions), ...
+    exception.Base('Model:SolutionNotAvailable', 'warning'), ...
+    exception.Base.alt2str(indexNaNSolutions) ...
+);
+
+end
