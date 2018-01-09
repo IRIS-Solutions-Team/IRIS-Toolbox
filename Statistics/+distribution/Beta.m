d55795cfaf0ea1594913a343db6bbb07d74e9ffe
Jaromir Benes <jaromir.benes@gmail.com>
2018-01-09 18:56:36 +0100

    New distributions distribution.Beta, distribution.Student

---
+++ Statistics/+distribution/Beta.m
@@ -0,0 +1,188 @@
+% Beta  Beta distribution object
+%
+%
+% Beta methods:
+%
+% __Constructors__
+%
+%   distribution.Beta.fromABeta - Beta distribution from parameters A and B
+%   distribution.Beta.fromMeanVar - Beta distribution from mean and variance
+%   distribution.Beta.fromMeanStd - Beta distribution from mean and std deviation
+%   distribution.Beta.fromModeVar - Beta distribution from mode and variance
+%   distribution.Beta.fromModeStd - Beta distribution from mode and std deviation
+%
+%
+% __Distribution Properties__
+%
+% These properties are directly accessible through the distribution object,
+% followed by a dot and the name of a property.
+%
+%   A - Parameter A of Beta distribution
+%   Beta - Parameter B of Beta distribution
+%   Lower - Lower bound of distribution domain
+%   Upper - Upper bound of distribution domain
+%   Mean - Mean (expected value) of distribution
+%   Var - Variance of distribution
+%   Std - Standard deviation of distribution
+%   Mode - Mode of distribution
+%   Median - Median of distribution
+%   Location - Location parameter of distribution
+%   Scale - Scale parameter of distribution
+%
+%
+% __Density Related Functions__
+%
+%   pdf - Probability density function
+%   logPdf - Log of probability density function up to constant
+%   info - Minus second derivative of log of probability density function
+%   inDomain - True for data points within domain of distribution function
+%
+%
+% __Description__
+%
+
+% -IRIS Macroeconomic Modeling Toolbox.
+% -Copyright (c) 2007-2017 IRIS Solutions Team.
+
+%--------------------------------------------------------------------------
+
+classdef Beta < distribution.Abstract
+    properties (SetAccess=protected)
+        % A  Parameter A of Beta distribution
+        A = NaN       
+
+        % B  Parameter B of Beta distribution
+        B = NaN        
+    end
+
+
+    properties (SetAccess=protected, Hidden)
+        Constant = NaN    % Integration constant
+    end
+
+
+    properties (Constant)
+        MAX_A = 1e10;
+    end
+
+
+    methods
+        function this = Beta(varargin)
+            this = this@distribution.Abstract(varargin{:});
+            this.Name = 'Beta';
+            this.Lower = 0;
+            this.Upper = 1;
+        end
+
+
+        function y = logPdf(this, x)
+            y = zeros(size(x));
+            indexInDomain = inDomain(this, x);
+            x = x(indexInDomain);
+            y(indexInDomain) = (this.A-1)*log(x) + (this.B-1)*log(1-x);
+            y(~indexInDomain) = -Inf;
+        end
+
+
+        function y = pdf(this, x)
+            y = zeros(size(x));
+            indexInDomain = inDomain(this, x);
+            x = x(indexInDomain);
+            y(indexInDomain) = x.^(this.A-1).*(1-x).^(this.B-1) * this.Constant;
+        end
+
+
+        function y = info(this, x)
+            y = nan(size(x));
+            indexInDomain = inDomain(this, x);
+            x = x(indexInDomain);
+            y(indexInDomain) = (this.B - 1)./(x - 1).^2 + (this.A - 1)./x.^2;
+        end
+    end
+
+
+    methods (Access=protected)
+        function populateParameters(this)
+            if ~isfinite(this.Mean)
+                this.Mean = this.A / (this.A + this.B);
+            end
+            if ~isfinite(this.Mode)
+                if this.A>1 && this.B>1
+                    this.Mode = (this.A - 1)/(this.A + this.B - 2);
+                else
+                    this.Mode = NaN;
+                end
+            end
+            if ~isfinite(this.Var)
+                this.Var = this.A * this.B / ( (this.A + this.B)^2 * (this.A + this.B + 1) );
+            end
+            if ~isfinite(this.Std)
+                this.Std = sqrt(this.Var);
+            end
+            this.Constant = 1./beta(this.A, this.B);
+        end
+
+
+        function ABFromMeanVar(this)
+            this.A = (1-this.Mean)*this.Mean^2/this.Var - this.Mean;
+            this.B = this.A*(1/this.Mean - 1);
+        end
+
+
+        function ABFromModeVar(this)
+            Mode = this.Mode;
+            Var = this.Var;
+            B = @(A) (A-1)/Mode - A + 2;
+            f = @(A) Var*(A + B(A))^2*(A + B(A) + 1) - A*B(A);
+            this.A = fzero(f, [1+eps( ), this.MAX_A]);
+            this.B = B(this.A);
+        end
+    end
+
+
+    methods (Static)
+        function this = fromAB(varargin)
+            % distribution.Beta.fromAB  Beta distribution from parameters A and B
+            this = distribution.Beta( );
+            [this.A, this.B] = varargin{1:2};
+            populateParameters(this);
+        end
+
+
+        function this = fromMeanVar(varargin)
+            % distribution.Beta.fromMeanVar  Beta distribution from mean and variance
+            this = distribution.Beta( );
+            [this.Mean, this.Var] = varargin{1:2};
+            ABFromMeanVar(this);
+            populateParameters(this);
+        end
+
+
+        function this = fromMeanStd(varargin)
+            % distribution.Beta.fromMeanStd  Beta distribution from mean and std deviation
+            this = distribution.Beta( );
+            [this.Mean, this.Std] = varargin{1:2};
+            this.Var = this.Std.^2;
+            ABFromMeanVar(this);
+            populateParameters(this);
+        end
+
+
+        function this = fromModeVar(varargin)
+            % distribution.Beta.fromModeVar  Beta distribution from mode and variance
+            this = distribution.Beta( );
+            [this.Mode, this.Var] = varargin{1:2};
+            ABFromModeVar(this);
+            populateParameters(this);
+        end
+
+
+        function fromModeStd(varargin)
+            % distribution.Beta.fromModeStd  Beta distribution from mode and std deviation
+            [this.Mode, this.Std] = varargin{1:2};
+            this.Var = this.Std^2;
+            ABFromModeVar(this);
+            populateParameters(this);
+        end
+    end
+end
---
+++ Statistics/+distribution/Student.m
@@ -0,0 +1,244 @@
+% Student  Student distribution object
+%
+%
+% Student methods:
+%
+% __Constructors__
+%
+%   distribution.Student.standardized - Standardized Student distribution
+%   distribution.Student.fromMeanVar - Student distribution from mean and variance
+%   distribution.Student.fromMeanStd - Student distribution from mean and std deviation
+%   distribution.Student.fromMedianVar - Student distribution from median and variance
+%   distribution.Student.fromMedianStd - Student distribution from median and std deviation
+%   distribution.Student.fromModeVar - Student distribution from mode and variance
+%   distribution.Student.fromModeStd - Student distribution from mode and std deviation
+%
+%
+% __Distribution Properties__
+%
+% These properties are directly accessible through the distribution object,
+% followed by a dot and the name of a property.
+%
+%   Lower - Lower bound of distribution domain
+%   Upper - Upper bound of distribution domain
+%   Mean - Mean (expected value) of distribution
+%   Var - Variance of distribution
+%   Std - Standard deviation of distribution
+%   Mode - Mode of distribution
+%   Median - Median of distribution
+%   Location - Location parameter of distribution
+%   Scale - Scale parameter of distribution
+%
+%
+% __Density Related Functions__
+%
+%   pdf - Probability density function
+%   logPdf - Log of probability density function up to constant
+%   info - Minus second derivative of log of probability density function
+%   inDomain - True for data points within domain of distribution function
+%
+%
+% __Description__
+%
+
+% -IRIS Macroeconomic Modeling Toolbox.
+% -Copyright (c) 2007-2017 IRIS Solutions Team.
+
+%--------------------------------------------------------------------------
+
+classdef Student < distribution.Abstract
+    properties 
+        DegreesFreedom = NaN
+        Mu = 0
+        Sigma = 1
+        Constant = NaN
+    end
+
+
+    methods
+        function this = Student(varargin)
+            this = this@distribution.Abstract(varargin{:});
+            this.Name = 'Student';
+            this.Lower = -Inf;
+            this.Upper = Inf;
+        end
+
+
+        function y = logPdf(this, x)
+            nu = this.DegreesFreedom;
+            mu = this.Mu;
+            sigma = this.Sigma;
+            w = (nu + 1)/2;
+            y = -w*log(1 + (1/nu)*((x - mu)/sigma).^2);
+        end
+
+
+        function y = pdf(this, x)
+            y = logPdf(this, x);
+            y = this.Constant * exp(y);
+        end
+
+
+        function y = info(this, x)
+            nu = this.DegreesFreedom;
+            mu = this.Mu;
+            sigma = this.Sigma;
+            sigma2 = sigma^2;
+            w = (nu + 1)/2;
+            if mu==0
+                xmu2 = x.^2;
+            else
+                xmu2 = (x - mu).^2;
+            end
+            if sigma==1
+                nusigma2 = nu;
+            else
+                nusigma2 = nu*sigma2;
+            end
+            y = 2*w*(nusigma2 - xmu2)./(nusigma2 + xmu2).^2;
+        end
+    end
+
+
+    methods (Access=protected)
+        function populateParameters(this)
+            nu = this.DegreesFreedom;
+            mu = this.Mu;
+            if this.Sigma==1
+                sigma = 1;
+                sigma2 = 1;
+            else
+                sigma = this.Sigma;
+                sigma2 = sigma^2;
+            end
+            w = (nu + 1)/2;
+            this.Constant = gamma(w)/(gamma(nu/2)*sqrt(pi*nu)*sigma);
+            if ~isfinite(this.Mean)
+                if nu>1
+                    this.Mean = mu;
+                else
+                    this.Mean = NaN;
+                end
+            end
+            if ~isfinite(this.Var)
+                if nu>2
+                    this.Var = sigma2*nu/(nu - 2);
+                else
+                    this.Var = NaN;
+                end
+            end
+            if ~isfinite(this.Std)
+                this.Std = sqrt(this.Var);
+            elseif ~isfinite(this.Var)
+                this.Std = NaN;
+            end
+            this.Mode = this.Mean;
+            this.Median = this.Mean;
+            this.Location = this.Mu;
+            this.Scale = this.Sigma;
+        end
+
+
+        function muSigmaFromMeanVar(this)
+            nu = this.DegreesFreedom;
+            if nu>1
+                this.Mu = this.Mean;
+            else
+                this.Mu = NaN;
+                this.Mean = NaN;
+            end
+            if nu>2
+                this.Sigma = sqrt(this.Var*(nu-2)/nu);
+            else
+                this.Var = NaN;
+                this.Sigma = NaN;
+            end
+        end
+    end
+
+
+    methods (Static)
+        function this = standardized(varargin)
+            % distribution.Student.standardized  Standardized Student distribution
+            this = distribution.Student( );
+            this.DegreesFreedom = varargin{1};
+            this.Mu = 0;
+            this.Sigma = 1;
+            populateParameters(this);
+        end
+
+
+        function this = fromLocationScale(varargin)
+            % distribution.Student.fromLocaionScale  Student distribution from location and scale parameters
+            this = distribution.Student( );
+            this.DegreesFreedom = varargin{1};
+            [this.Mu, this.Sigma] = varargin{2:3};
+            populateParameters(this);
+        end
+
+
+        function this = fromMeanVar(varargin)
+            % distribution.Student.fromMeanVar  Student distribution from mean and variance
+            this = distribution.Student( );
+            this.DegreesFreedom = varargin{1};
+            [this.Mean, this.Var] = varargin{2:3};
+            muSigmaFromMeanVar(this);
+            populateParameters(this);
+        end
+
+
+        function this = fromMeanStd(varargin)
+            % distribution.Student.fromMeanStd  Student distribution from mean and std deviation
+            this = distribution.Student( );
+            this.DegreesFreedom = varargin{1};
+            [this.Mean, this.Std] = varargin{2:3};
+            this.Var = this.Std^2;
+            muSigmaFromMeanVar(this);
+            populateParameters(this);
+        end
+
+
+        function this = fromMedianVar(varargin)
+            % distribution.Student.fromMedianVar  Student distribution from median and variance
+            this = distribution.Student( );
+            this.DegreesFreedom = varargin{1};
+            [this.Median, this.Var] = varargin{2:3};
+            this.Mean = this.Median;
+            muSigmaFromMeanVar(this);
+            populateParameters(this);
+        end
+
+
+        function this = fromMedianStd(varargin)
+            % distribution.Student.fromMedianStd  Student distribution from median and std deviation
+            this.DegreesFreedom = varargin{1};
+            [this.Median, this.Std] = varargin{2:3};
+            this.Mean = this.Median;
+            this.Var = this.Std^2;
+            muSigmaFromMeanVar(this);
+            populateParameters(this);
+        end
+
+
+        function this = fromModeVar(varargin)
+            % distribution.Student.fromModeVar  Student distribution from mode and variance
+            this = distribution.Student( );
+            this.DegreesFreedom = varargin{1};
+            [this.Mode, this.Var] = varargin{2:3};
+            this.Mean = this.Mode;
+            muSigmaFromMeanVar(this);
+            populateParameters(this);
+        end
+
+
+        function this = fromModeStd(varargin)
+            % distribution.Student.fromModeStd  Student distribution from mode and std deviation
+            this.DegreesFreedom = varargin{1};
+            [this.Mode, this.Std] = varargin{2:3};
+            this.Mean = this.Mode;
+            this.Var = this.Std^2;
+            muSigmaFromMeanVar(this);
+            populateParameters(this);
+        end
+    end
+end
