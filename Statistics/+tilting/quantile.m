e19fe2e62de346c2fa27d7105d1143cf9e6ea667
Jaromir Benes <jaromir.benes@gmail.com>
2018-01-26 13:49:53 +0100

    Use numeric.redim in tilting.quantile.

--- Statistics/+tilting/quantile.m
+++ Statistics/+tilting/quantile.m
@@ -1,4 +1,4 @@
-function [q, dim] = quantile(x, w, tau, varargin{:})
+function [q, dim] = quantile(x, w, tau, varargin)
 % tilting.quantile  Quantiles of possibly weighted observations
 %
 % __Syntax__
@@ -46,16 +46,16 @@ if ~isnumeric(tau)
     end
 end
 
-persistent INPUT_PARSER
-if isempty(INPUT_PARSER)
-    INPUT_PARSER = extend.InputParser('tilting/quantile');
-    INPUT_PARSER.addRequired('InputData', @(x) isnumeric(x) || isa(x, 'TimeSubscriptable'));
-    INPUT_PARSER.addRequired('Weights', @isnumeric);
-    INPUT_PARSER.addRequired('Tau', @(x) isnumeric(x) && all(x(:)>0 & x(:)<1));
-    INPUT_PARSER.addOptional('Dim', @(x) isnumeric(x) && numel(x)==1 && x==round(x) && x>=1);
+persistent inputParser
+if isempty(inputParser)
+    inputParser = extend.InputParser('tilting/quantile');
+    inputParser.addRequired('InputData', @(x) isnumeric(x) || isa(x, 'TimeSubscriptable'));
+    inputParser.addRequired('Weights', @isnumeric);
+    inputParser.addRequired('Tau', @(x) isnumeric(x) && all(x(:)>0 & x(:)<1));
+    inputParser.addOptional('Dim', 2, @(x) isnumeric(x) && numel(x)==1 && x==round(x) && x>=1);
 end
-INPUT_PARSER.parse(x, w, tau, varargin{:});
-dim = INPUT_PARSER.Results.Dim;
+inputParser.parse(x, w, tau, varargin{:});
+dim = inputParser.Results.Dim;
 
 if isa(x, 'TimeSubscriptable')
     [x, dim] = applyFunctionAlongDim(x, @tilting.quantile, w, tau, dim);
@@ -65,12 +65,12 @@ end
 %--------------------------------------------------------------------------
 
 if isempty(tau)
-    sizeOfInputData = size(x);
-    if dim>length(sizeOfInputData)
-        sizeOfInputData(end+1:dim) = 1;
+    sizeInputData = size(x);
+    if dim>length(sizeInputData)
+        sizeInputData(end+1:dim) = 1;
     end
-    sizeOfInputData(dim) = 0;
-    q = zeros(sizeOfInputData);
+    sizeInputData(dim) = 0;
+    q = zeros(sizeInputData);
     return
 end
 
@@ -79,29 +79,31 @@ if ~isempty(w) && all(w(1)==w)
 end
 
 tau = tau(:);
-numOfQuantiles = numel(tau);
+numQuantiles = numel(tau);
 
 % Put dim-th dimension first, and unfold higher dimensions into 2D (so that x
-% is a 2D matrix).
-[x, shape] = tilting.reshape(x, dim, 2);
-numOfRows = size(x, 1);
+% is a 2D matrix)
+[x, redimStruct] = numeric.redim(x, dim);
+numRows = size(x, 1);
 
 [xs, pos] = sort(x, 1);
 if isempty(w)
-    grid = ((0:numOfRows-1) + (1:numOfRows)) / (2*numOfRows);
+    xs = sort(x, 1);
+    grid = ((0:numRows-1) + (1:numRows)) / (2*numRows);
     grid = grid(:);
     q = interp1([0; grid; 1], [xs(1, :); xs; xs(end, :)], tau, 'linear');
 else
-    numOfColumns = size(x, 2);
-    q = nan(numOfQuantiles, numOfColumns);
-    for i = 1 : numOfColumns
-        wi = w(pos(:, i));
-        cumw = cumsum(wi(:), 1);
-        grid = ([0; cumw(1:end-1)] + cumw) / (2*cumw(end));
-        q(:, i) = interp1([0; grid; 1], [xs(1, i); xs(:, i); xs(end, i)], tau, 'linear');
+    numColumns = size(x, 2);
+    q = nan(numQuantiles, numColumns);
+    for i = 1 : numColumns
+        [ithX, pos] = sort(x(:, i), 1);
+        ithW = w(pos);
+        ithCumW = cumsum(ithW(:), 1);
+        grid = ([0; ithCumW(1:end-1)] + ithCumW) / (2*ithCumW(end));
+        q(:, i) = interp1([0; grid; 1], [ithX(1); ithX(:); ithX(end)], tau, 'linear');
     end
 end
 
-q = tilting.reshape(q, shape);
+q = numeric.redim(q, dim, redimStruct);
 
 end
