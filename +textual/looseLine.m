4ad27e8f82736d796e9734433cd55e45eb46e828
Jaromir Benes <jaromir.benes@gmail.com>
2018-02-09 18:31:45 +0100

    Make textual.looseLine compatile with earlier versions of Matlab.

--- +textual/looseLine.m
+++ +textual/looseLine.m
@@ -1,6 +1,12 @@
 function looseLine( )
 
-if strcmp(matlab.internal.display.formatSpacing, 'loose')
+try
+    isLoose = strcmpi(matlab.internal.display.formatSpacing, 'loose');
+catch
+    isLoose = strcmpi(get(0, 'FormatSpacing'), 'loose');
+end
+
+if isLoose
     fprintf('\n');
 end
 
