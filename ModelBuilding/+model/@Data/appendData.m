54c860e719211e0a5b609f30a1cc8332cd19bad0
Jaromir Benes <jaromir.benes@gmail.com>
2018-03-16 17:12:03 +0100

    Upgrade model.Data.appendData( )

--- ModelBuilding/+model/@Data/appendData.m
+++ ModelBuilding/+model/@Data/appendData.m
@@ -1,77 +1,143 @@
-function outputData = appendData(this, inputData, outputData, range, opt)
-% appendData  Execute DbOverlay= or AppendPresample= options
+function outputData = appendData(this, inputData, outputData, range, varargin)
+% appendData  Append presample or postsample data
 %
-% Backend IRIS function.
-% No help provided.
+% Backend IRIS function
+% No help provided
 
-% -IRIS Macroeconomic Modeling Toolbox.
-% -Copyright (c) 2007-2018 IRIS Solutions Team.
+% -IRIS Macroeconomic Modeling Toolbox
+% -Copyright (c) 2007-2018 IRIS Solutions Team
+
+if numel(varargin)==2
+    presample = varargin{1};
+    postsample = varargin{2};
+elseif numel(varargin)==1 && isstruct(varargin{1})
+    opt = varargin{1};
+    if isfield(opt, 'DbOverlay') && ~isequal(opt.DbOverlay, false)
+        presample = opt.DbOverlay;
+        postsample = opt.DbOverlay;
+    else
+        presample = opt.AppendPresample;
+        postsample = opt.AppendPostsample;
+    end
+end
 
 %--------------------------------------------------------------------------
 
-if isequal(opt.DbOverlay, false) && isequal(opt.AppendPresample, false)
+if isequal(presample, false) && isequal(postsample, false)
     return
 end
 
-% Append only names already included in outputDataut databank
-inpFields = fieldnames(inputData);
-outputDataFields = fieldnames(outputData);
-inputData = rmfield(inputData, setdiff(inpFields, outputDataFields));
-
-% Overlay the input (or user-supplied) database with the simulation
-% database.
-if isequal(opt.DbOverlay, true)
-    outputData = dboverlay(inputData, outputData);
-    return
-elseif isstruct(opt.DbOverlay)
-    outputData = dboverlay(opt.DbOverlay, outputData);
-    return
-elseif isequal(opt.AppendPresample, true)
-    outputData = appendPresample(this, inputData, outputData, range);
-    return
-elseif isstruct(opt.AppendPresample)
-    outputData = appendPresample(this, opt.AppendPresample, outputData, range);
-    return
+preDatabank = [ ];
+if isequal(presample, true)
+    pre = true;
+    preDatabank = inputData;
+elseif isstruct(presample)
+    pre = true;
+    preDatabank = presample;
+else
+    pre = false;
 end
 
-end%
+postDatabank = [ ];
+if isequal(postsample, true)
+    post = true;
+    postDatabank = inputData;
+elseif isstruct(postsample)
+    post = true;
+    postDatabank = postsample;
+else
+    post = false;
+end
 
+freq = round(getFrequency(range(1)));
+serialRangeStart = round(range(1));
+serialRangeEnd = round(range(end));
 
-function outputData = appendPresample(this, inputData, outputData, range)
-    TYPE = @int8;
-    freq = round(getFrequency(range(1)));
-    indexYXEG = this.Quantity.Type==TYPE(1) ...
-        | this.Quantity.Type==TYPE(2) ...
-        | this.Quantity.Type==TYPE(31) ...
-        | this.Quantity.Type==TYPE(32) ...
-        | this.Quantity.Type==TYPE(5);
-    list = this.Quantity.Name(indexYXEG);
-    for i = 1 : numel(list)
-        ithName = list{i};
-        if ~isfield(inputData, ithName) ...
-            || ~isa(inputData.(ithName), 'TimeSubscriptable') ...
-            || double(inputData.(ithName).Frequency)~=freq
-            continue
+list = this.NamesAppendable;
+for i = 1 : numel(list)
+    ithName = list{i};
+    preSeries = [ ];
+    postSeries = [ ];
+    if isstruct(preDatabank)
+        if isfield(preDatabank, ithName) ...
+            && isa(preDatabank.(ithName), 'TimeSubscriptable') ...
+            || round(preDatabank.(ithName).Frequency)~=freq
+            preSeries = preDatabank.(ithName);
         end
-        x = outputData.(ithName);
-        serialXStart = round(x.Start);
-        appendData = getDataFromTo(inputData.(ithName), -Inf, serialXStart-1);
-        if isempty(appendData)
-            continue
-        end
-        serialNewStart = serialXStart - size(appendData, 1);
-        sizeXData = size(x.Data);
-        sizeAppendData = size(appendData);
-        ncolXData = prod(sizeXData(2:end));
-        ncolAppendData = prod(sizeAppendData(2:end));
-        if ncolAppendData==1 && ncolXData>1
-            appendData = repmat(appendData, [1, sizeXData(2:end)]);
-        elseif ~isequal(sizeAppendData(2:end), sizeXData(2:end))
-            continue
-        end
-        x.Data = [appendData; x.Data];
-        x.Start = DateWrapper.fromSerial(freq, serialNewStart);
-        outputData.(ithName) = x;
     end
+    if isstruct(postDatabank)
+        if isfield(postDatabank, ithName) ...
+            && isa(postDatabank.(ithName), 'TimeSubscriptable') ...
+            || round(postDatabank.(ithName).Frequency)~=freq
+            postSeries = postDatabank.(ithName);
+        end
+    end
+
+    if isempty(preSeries) && isempty(postSeries)
+        continue
+    end
+
+    x = outputData.(ithName);
+    serialXStart = round(x.Start);
+    serialXStart0 = serialXStart;
+    if isnan(serialXStart)
+        serialXStart = serialRangeStart;
+    elseif serialXStart>serialRangeStart
+        serialXStart = serialRangeStart;
+    end
+    xData = getDataFromTo(x, serialXStart, serialRangeEnd);
+    sizeXData2 = size(xData);
+    sizeXData2 = sizeXData2(2:end);
+    ncolXData = prod(sizeXData2);
+
+    if ~isempty(preSeries)
+        appendPresample( );
+    end
+    if ~isempty(postSeries)
+        appendPostsample( );
+    end
+
+    if serialXStart~=serialXStart0
+        x.Start = DateWrapper.fromSerial(freq, serialXStart);
+    end
+    x.Data = xData;
+    x = trim(x);
+    outputData.(ithName) = x;
+end
+
+return
+
+
+    function appendPresample( )
+        preData = getDataFromTo(preSeries, -Inf, serialXStart-1);
+        if isempty(preData)
+            return
+        end
+        sizePreData = size(preData);
+        ncolPreData = prod(sizePreData(2:end));
+        if ncolPreData==1 && ncolXData>1
+            preData = repmat(preData, [1, sizeXData2]);
+        elseif ~isequal(sizePreData(2:end), sizeXData2)
+            return
+        end
+        xData = [preData; xData];
+        serialXStart = serialXStart - sizePreData(1);
+    end%
+
+
+    function appendPostsample( )
+        postData = getDataFromTo(postSeries, serialRangeEnd+1, Inf);
+        if isempty(postData)
+            return
+        end
+        sizePostData = size(postData);
+        ncolPostData = prod(sizePostData(2:end));
+        if ncolPostData==1 && ncolXData>1
+            postData = repmat(postData, [1, sizeXData2]);
+        elseif ~isequal(sizePostData(2:end), sizeXData2)
+            return
+        end
+        xData = [xData; postData];
+    end%
 end%
 
