881abeff9b1f313131bda746950fb70f43386811
Jaromir Benes <jaromir.benes@gmail.com>
2018-04-19 21:23:00 +0200

    Fix the order in which graphics properties are processed in visual.style

--- +visual/style.m
+++ +visual/style.m
@@ -20,17 +20,23 @@ end
 numHandles = numel(handles);
 specsTypes = fieldnames(specs);
 errors = cell.empty(0, 1);
-for i = 1 : numHandles
+count = struct( );
+for i = numHandles : -1 : 1
     ithHandle = handles(i);
     handleVisible = get(ithHandle, 'HandleVisibility');
     if ~strcmpi(handleVisible, 'On')
         continue
     end
     ithType = get(ithHandle, 'Type');
+    if isfield(count, ithType)
+        count.(ithType) = count.(ithType) + 1;
+    else
+        count.(ithType) = 1;
+    end
     indexMatchType = strcmpi(specsTypes, ithType);
     if any(indexMatchType)
         pos = find(indexMatchType, 1);
-        errors = apply(ithHandle, ithType, specs.(specsTypes{pos}), i, errors);
+        errors = apply(ithHandle, ithType, specs.(specsTypes{pos}), count.(ithType), errors);
     end
     if ~isequal(opt.StyleChildren, true)
         continue
@@ -41,13 +47,13 @@ end
 
 if ~isempty(errors)
     errors = unique(errors);
-    disp(errors)
+    % disp(errors)
 end
 end%
 
 
-function errors = apply(handle, type, specs, i, errors)
-    errors = extra('PreStyle', handle, type, specs, i, errors); 
+function errors = apply(handle, type, specs, j, errors)
+    errors = extra('PreStyle', handle, type, specs, j, errors); 
     propertyNames = fieldnames(specs);
     numProperties = numel(propertyNames);
     for i = 1 : numProperties
@@ -56,13 +62,25 @@ function errors = apply(handle, type, specs, i, errors)
             continue
         end
         ithPropertyValue = specs.(ithPropertyName);
+        if iscell(ithPropertyValue)
+            assignValue = ithPropertyValue{min(j, end)};
+        else
+            assignValue = ithPropertyValue;
+        end
         try
-            set(handle, ithPropertyName, ithPropertyValue);
+            currentValue = get(handle, ithPropertyName);
+            if isgraphics(currentValue)
+                errors = apply(currentValue, ithPropertyName, assignValue, j, errors);
+                continue
+            end
+        end
+        try
+            set(handle, ithPropertyName, assignValue);
         catch
             errors{end+1, 1} = sprintf('%s.%s', type, ithPropertyName);
         end
     end
-    errors = extra('PostStyle', handle, type, specs, i, errors); 
+    errors = extra('PostStyle', handle, type, specs, j, errors); 
 end%
 
 
@@ -79,7 +97,7 @@ function errors = extra(which, handle, type, specs, i, errors)
     end
     numFunc = numel(func);
     for i = 1 : numFunc
-        ithFunc = func{i};
+        ithFunc = func{i}(handle);
         if ~isa(ithFunc, 'function_handle')
             continue 
         end
