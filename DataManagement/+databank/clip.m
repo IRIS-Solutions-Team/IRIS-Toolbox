16af187e995c86d2870aa8fdf008d70aedd0d8dd
Jaromir Benes <jaromir.benes@gmail.com>
2018-04-19 21:26:26 +0200

    Allow (startDate, endDate) syntax in databank.clip

--- DataManagement/+databank/clip.m
+++ DataManagement/+databank/clip.m
@@ -1,11 +1,23 @@
 function d = clip(d, newStart, newEnd)
 
-frequency = newStart.Frequency;
+isNewStartInf = isequal(newStart, -Inf);
+isNewEndInf = isequal(newEnd, Inf);
+
+if isNewStartInf && isNewEndInf
+    return
+end
+
+if ~isNewStartInf
+    frequency = DateWrapper.getFrequencyFromNumeric(newStart);
+else
+    frequency = DateWrapper.getFrequencyFromNumeric(newEnd);
+end
+
 listOfFields = fieldnames(d);
 numberOfFields = numel(listOfFields);
 for i = 1 : numberOfFields
     ithField = listOfFields{i};
-    if isa(d.(ithField), 'TimeSeries')
+    if isa(d.(ithField), 'TimeSubscriptable')
         if d.(ithField).Frequency==frequency
             d.(ithField) = clip(d.(ithField), newStart, newEnd);
         end
