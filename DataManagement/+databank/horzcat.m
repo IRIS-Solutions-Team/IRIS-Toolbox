10c0438447307da9c78f628220aefed8245bfc6a
Jaromir Benes <jaromir.benes@gmail.com>
2018-04-19 21:27:14 +0200

    Create new function databank.horzcat to horizontally concatenate databank fields

--- DataManagement/+databank/horzcat.m
+++ DataManagement/+databank/horzcat.m
@@ -1,32 +1,72 @@
-function outputDatabank = horzcat(varargin)
+function mainDatabank = horzcat(mainDatabank, varargin)
 
-persistent INPUT_PARSER
-if isempty(INPUT_PARSER)
-    INPUT_PARSER = extend.InputParser('databank/horzcat');
-    INPUT_PARSER.addRequired('DatabanksToCombine', @(x) ~isempty(x) && all(cellfun(@isstruct, x)));
-end
-
-INPUT_PARSER.parse(varargin);
-
-%--------------------------------------------------------------------------
-
-if nargin==1
-    outputDatabank = varargin{1};
+if isempty(varargin)
     return
 end
 
-numberOfDatabanks = nargin;
-namesOfFields = fieldnames(varargin{1});
-numberOfFields = numel(namesOfFields);
+indexStruct = cellfun('isclass', varargin, 'struct');
+if ~indexStruct(1)
+    return
+end
 
-outputDatabank = struct( );
-for i = 1 : numberOfFields
-    name = namesOfFields{i};
-    outputDatabank.(name) = cell(1, numberOfDatabanks);
-    for j = 1 : numberOfDatabanks
-        outputDatabank.(name){j} = varargin{j}.(name);
+if all(indexStruct)
+    mergeWith = varargin;
+    varargin(:) = [ ];
+else
+    posFirstNonStruct = find(~indexStruct, 1);
+    posLastStruct = posFirstNonStruct - 1;
+    mergeWith = varargin(1:posLastStruct);
+    varargin(1:posLastStruct) = [ ];
+end
+
+persistent inputParser
+if isempty(inputParser)
+    inputParser = extend.InputParser('databank.horzcat');
+    inputParser.addRequired('InputDatabank', @isstruct);
+    inputParser.addRequired('MergeWith');
+    inputParser.addParameter('MissingField', @rmfield);
+end
+inputParser.parse(mainDatabank, mergeWith, varargin{:});
+opt = inputParser.Options;
+
+%-------------------------------------------------------------------
+
+numMergeWith = numel(mergeWith);
+
+for i = 1 : numMergeWith
+    mainDatabank = mergeWithOneDatabank(mainDatabank, mergeWith{i}, opt.MissingField);
+end
+
+end%
+
+
+function mainDatabank = mergeWithOneDatabank(mainDatabank, mergeWith, missingField)
+    fieldsMainDatabank = fieldnames(mainDatabank);
+    fieldsMergeWith = fieldnames(mergeWith);
+    fieldsMerged = [fieldsMainDatabank; fieldsMergeWith];
+    fieldsMerged = unique(fieldsMerged, 'stable');
+    numFieldsMerged = numel(fieldsMerged);
+    for i = 1 : numFieldsMerged
+        ithName = fieldsMerged{i};
+        if isequal(missingField, @rmfield) 
+            if ~isfield(mergeWith, ithName)
+                mainDatabank = rmfield(mainDatabank, ithName);
+                continue
+            elseif ~isfield(mainDatabank, ithName);
+                continue
+            end
+        end
+        if isfield(mainDatabank, ithName)
+            mainDatabankField = mainDatabank.(ithName);
+        else
+            mainDatabankField = missingField;
+        end
+        if isfield(mergeWith, ithName)
+            mergeWithField = mergeWith.(ithName);
+        else
+            mergeWithField = missingField;
+        end
+        mainDatabank.(ithName) = horzcat(mainDatabankField, mergeWithField);
     end
-    outputDatabank.(name) = horzcat(outputDatabank.(name){:});
-end
+end%
 
-end
