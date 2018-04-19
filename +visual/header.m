34ff01cf2a0c4189851542d7711f5c041c3d5874
Jaromir Benes <jaromir.benes@gmail.com>
2018-04-19 21:22:32 +0200

    Create new visual.header for placing headers (figure titles) to the top of figure windows.

---
+++ +visual/header.m
@@ -0,0 +1,63 @@
+function handleText = header(varargin)
+
+handleText = gobjects(0, 1);
+
+if nargin==0
+    return
+end
+
+if nargin==1 && all(isgraphics(varargin{1}))
+    return
+end
+
+if all(isgraphics(varargin{1}))
+    varargin([1, 2]) = varargin([2, 1]);
+end
+
+persistent inputParser
+if isempty(inputParser)
+    inputParser = extend.InputParser('visual.header');
+    inputParser.KeepUnmatched = true;
+    inputParser.addRequired('String', @(x) ischar(x) || isa(x, 'string'));
+    inputParser.addOptional('HandleFigure', gobjects(0), @(x) all(isgraphics(x)));
+end
+inputParser.parse(varargin{:});
+string = inputParser.Results.String;
+handleFigure = inputParser.Results.HandleFigure;
+unmatched = inputParser.UnmatchedInCell;
+
+if isempty(string)
+    return
+end
+
+if isempty(handleFigure)
+    handleFigure = visual.backend.getCurrentFigureIfExists( );
+    if isempty(handleFigure)
+        return
+    end
+end
+
+%--------------------------------------------------------------------------
+
+for i = 1 : numel(handleFigure)
+    handleText = [
+        handleText
+        printHeader(handleFigure(i), string, unmatched{:})
+    ];
+end
+
+end%
+
+
+function handleText = printHeader(handleFigure, string, varargin)
+    handleText = annotation( ...
+        handleFigure, ...
+        'TextBox', [0, 1, 1, 0], ...
+        'String', string, ...
+        'Horizontal', 'Center', ...
+        'Vertical', 'Top', ...
+        'LineStyle', 'None', ...
+        varargin{:} ...
+    );
+end%
+
