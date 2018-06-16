fffdc8c8b9be472e741fc6603519bde3bd321a21
Jaromir Benes <jaromir.benes@gmail.com>
2018-06-16 05:46:40 +0200

    Fix duplicate Shortcut/shortcut folder problem, part 2.

---
+++ Shortcuts/plotneigh.m
@@ -0,0 +1,11 @@
+function varargout = plotneigh(varargin)
+% plotneigh  Shortcut for grfun.plotneigh.
+%
+% See help on [`grfun.plotneigh`](grfun/plotneigh).
+
+% -IRIS Macroeconomic Modeling Toolbox.
+% -Copyright (c) 2007-2018 IRIS Solutions Team.
+
+[varargout{1:nargout}] = grfun.plotneigh(varargin{:});
+
+end
