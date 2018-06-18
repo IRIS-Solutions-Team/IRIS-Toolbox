05711f7590c9f3d1f0225998f877fbfcd7c4d74b
Jaromir Benes <jaromir.benes@gmail.com>
2018-06-18 07:01:20 -0500

    Fix issues with duplicate DTrends and dtrends in iris.options.auto.

---
+++ +iris/+options/+auto/DTrends.m
@@ -0,0 +1,14 @@
+function opt = dtrends(opt)
+% dtrends  Auto value for option DTrends
+%
+% Backend IRIS function
+% No help provided
+
+% -IRIS Macroeconomic Modeling Toolbox
+% -Copyright (c) 2007-2018 IRIS Solutions Team
+
+%--------------------------------------------------------------------------
+
+opt.DTrends = ~opt.Deviation;
+
+end
