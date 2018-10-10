c294777727da01ea1d8d048c4111a5e79e86d23b
Jaromir Benes <jaromir.benes@gmail.com>
2018-10-10 16:33:20 +0200

    Redirect tseries.resize to tseries.clip

--- DataManagement/@tseries/resize.m
+++ DataManagement/@tseries/resize.m
@@ -32,68 +32,28 @@ function [this, newRange] = resize(this, range)
 
 %--------------------------------------------------------------------------
 
+isDateWrapper = isa(range, 'DateWrapper');
+
 if isempty(range) || isnan(this.Start)
-    newRange = [ ];
     this = this.empty(this);
-    return
-elseif all(isinf(range)) || isequal(range, @all)
-    newRange = this.Range;
-    return
-end
-
-serialXStart = round(this.Start);
-serialXEnd = serialXStart + size(this.Data, 1) - 1;
-freq = getFrequency(this.Start);
-rangeFirst = range(1);
-rangeLast = range(end);
-
-% Frequency of input tseries must be the same as frequency of new date
-% range.
-freqRangeFirst = DateWrapper.getFrequencyFromNumeric(rangeFirst);
-freqRangeLast = DateWrapper.getFrequencyFromNumeric(rangeLast);
-if ~freqcmp(freqRangeFirst, freq) || ~freqcmp(freqRangeLast, freq)
-    utils.error('tseries:resize', ...
-        ['Frequency of the tseries object and ', ...
-        'the date frequency of the new range must be the same.']);
-end
-
-serialRangeFirst = round(rangeFirst);
-serialRangeLast = round(rangeLast);
-
-% Return immediately if start of new range is before start of input tseries
-% and end of new range is after end of input tseries.
-if serialRangeFirst<=serialXStart && serialRangeLast>=serialXEnd
-    newRange = this.Range;
+    if isDateWrapper
+        newRange = DateWrapper.empty(1, 0);
+    else
+        newRange = double.empty(1, 0);
+    end
     return
 end
 
-if isinf(serialRangeFirst)
-    serialStartDate = serialXStart;
+if isa(range, 'DateWrapper')
+    rangeFirst = getFirst(range);
+    rangeLast = getLast(range);
 else
-    serialStartDate = serialRangeFirst;
+    rangeFirst = range(1);
+    rangeLast = range(end);
 end
 
-if isinf(serialRangeLast)
-    serialEndDate = serialXEnd;
-else
-    serialEndDate = serialRangeLast;
-end
+[this, newStart, newEnd] = clip(this, rangeFirst, rangeLast);
+newRange = newStart : newEnd;
 
-serialNewRange = serialStartDate : serialEndDate;
-sizeData = size(this.Data);
-numDates = sizeData(1);
-pos = serialNewRange - serialXStart + 1;
-indexDeleteRows = pos<1 | pos>numDates;
-serialNewRange(indexDeleteRows) = [ ];
-pos(indexDeleteRows) = [ ];
+end%
 
-if ~isempty(pos)
-    this.Data = this.Data(:, :);
-    this.Data = this.Data(pos, :);
-    this.Data = reshape(this.Data, [length(pos), sizeData(2:end)]);
-    this.Start = DateWrapper.fromSerial(freq, serialNewRange(1));
-else
-    this = this.empty(this);
-end
-
-end
