a9436eefef0b07e1cacad751e6d295f5804c36fc
Jaromir Benes <jaromir.benes@gmail.com>
2018-10-10 16:30:53 +0200

    Allow for both dates (numeric, DateWrapper) and serials to be used as inputs into tseries.getDataFromTo

--- DataManagement/@tseries/getDataFromTo.m
+++ DataManagement/@tseries/getDataFromTo.m
@@ -1,5 +1,5 @@
-function [x, range] = getDataFromTo(this, serialFrom, serialTo)
-% getDataFromTo  Retrieve time series data from serial to serial
+function [x, actualFrom, actualTo] = getDataFromTo(this, from, to)
+% getDataFromTo  Retrieve time series data from date to date
 %
 % Backend IRIS function
 % No help provided
@@ -9,38 +9,52 @@ function [x, range] = getDataFromTo(this, serialFrom, serialTo)
 
 %-------------------------------------------------------------------------- 
 
+% No frequency check can be performed here; this is a responsibility of the
+% caller
+
 if nargin==1
     x = this.Data;
     range = this.Range;
     return
 end
 
-frequency = getFrequency(this.Start);
-serialStart = round(this.Start);
+if nargin==2
+    to = from;
+end
+
+% Input dates from and to may be either date codes or serials; convert to
+% serials anyway
+serialFrom = DateWrapper.getSerial(from);
+serialTo = DateWrapper.getSerial(to);
+
+freqOfStart = DateWrapper.getFrequencyAsNumeric(this.Start);
+serialOfStart = DateWrapper.getSerial(this.Start);
 data = this.Data;
 sizeOfData = size(data);
 missingValue = this.MissingValue;
 
 if ~isinf(serialFrom) && ~isinf(serialTo) && serialTo<serialFrom
     x = repmat(missingValue, [0, sizeOfData(2:end)]);
+    range = double.empty(1, 0);
     return
 end
 
 if isinf(serialFrom)
     posFrom = 1; 
 else
-    posFrom = round(serialFrom - serialStart + 1);
+    posFrom = round(serialFrom - serialOfStart + 1);
 end
 
 if isinf(serialTo)
     posTo = sizeOfData(1);
 else
-    posTo = round(serialTo - serialStart + 1);
+    posTo = round(serialTo - serialOfStart + 1);
 end
 
-if isnan(serialStart) || isempty(data)
+if isnan(serialOfStart) || isempty(data)
     lenOfRange = round(posTo - posFrom + 1);
     x = repmat(this.MissingValue, [lenOfRange, sizeOfData(2:end)]);
+    range = double.empty(1, 0);
     return
 end
 
@@ -68,8 +82,9 @@ if length(sizeOfData)>2
 end
 
 if nargout>1
-    range = round(serialStart + (posFrom : posTo) - 1);
-    range = DateWrapper.fromSerial(frequency, range);
+    actualFrom = DateWrapper.getDateCodeFromSerial(freqOfStart, serialOfStart + posFrom - 1);
+    actualTo = DateWrapper.getDateCodeFromSerial(freqOfStart, serialOfStart + posTo - 1);
 end
 
-end
+end%
+
