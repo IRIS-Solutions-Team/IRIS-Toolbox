f008013ea9c2fb813aaa27b622fdcc987b84ea07
Jaromir Benes <jaromir.benes@gmail.com>
2018-10-10 16:55:15 +0200

    Consolidate IRIS solvers (qnsd and qad) in one universal solver differentiated by options settings, introduce a new pure Newton variant.

--- Modeling/+simulate/+selective/run.m
+++ Modeling/+simulate/+selective/run.m
@@ -1,20 +1,20 @@
-function [YRpt,XxRpt,EaRpt,EuRpt,WRpt,Exit,Dcy,Addf] = run(S)
-% run  [Not a public function] Equation-selective nonlinear simulation.
+function [YRpt, XxRpt, EaRpt, EuRpt, WRpt, Exit, Dcy, Addf] = run(S)
+% run  Equation-selective nonlinear simulation
 %
-% Backend IRIS function.
-% No help provided.
+% Backend IRIS function
+% No help provided
 
-% -IRIS Macroeconomic Modeling Toolbox.
-% -Copyright (c) 2007-2018 IRIS Solutions Team.
+% -IRIS Macroeconomic Modeling Toolbox
+% -Copyright (c) 2007-2018 IRIS Solutions Team
 
 %--------------------------------------------------------------------------
 
-ny = size(S.Z,1);
-nxx = size(S.T,1);
-nb = size(S.T,2);
+ny = size(S.Z, 1);
+nxx = size(S.T, 1);
+nb = size(S.T, 2);
 nf = nxx - nb;
-ne = size(S.Ea,1);
-nPer = size(S.Ea,2);
+ne = size(S.Ea, 1);
+nPer = size(S.Ea, 2);
 ixNonl = S.Selective.IxHash;
 nn = sum(ixNonl);
 nPerNonl = S.NPerNonlin;
@@ -39,9 +39,9 @@ nPerMax = max( nPer , sgm(end)+nPerNonl-1 );
 anch = S.Anch;
 wght = S.Wght;
 tune = S.Tune;
-yxxAnch = anch(1:ny+nxx,:);
-eaAnch = anch(ny+nxx+(1:ne),:);
-euAnch = anch(ny+nxx+ne+(1:ne),:);
+yxxAnch = anch(1:ny+nxx, :);
+eaAnch = anch(ny+nxx+(1:ne), :);
+euAnch = anch(ny+nxx+ne+(1:ne), :);
 isEaAnch = any(eaAnch(:) ~= 0);
 isEuAnch = any(euAnch(:) ~= 0);
 if S.IsDeviation && S.IsAddSstate
@@ -59,7 +59,7 @@ lastEndgA = utils.findlast(eaAnch);
 S.Selective.Jv = [ ];
 S.Selective.IxUpd =  [ ];
 isFastUpd = false;
-doFastUpdMatrices( );
+fastUpdate( );
 
 S.Anch = [ ];
 S.Wght = [ ];
@@ -73,28 +73,28 @@ end
 % expand the below arrays accordingly, so that it is easier to set up their
 % segment-specific version in S.
 if nPer < nPerMax
-    ea(:,end+1:nPerMax) = 0;
-    eu(:,end+1:nPerMax) = 0;
+    ea(:, end+1:nPerMax) = 0;
+    eu(:, end+1:nPerMax) = 0;
     if isEaAnch || isEuAnch
-        anch(:,end+1:nPerMax) = false;
-        wght(:,end+1:nPerMax) = 0;
-        tune(:,end+1:nPerMax) = NaN;
+        anch(:, end+1:nPerMax) = false;
+        wght(:, end+1:nPerMax) = 0;
+        tune(:, end+1:nPerMax) = NaN;
     end
 end
 
 % Arrays reported in nonlinear simulations.
-YRpt = zeros(ny,0);
-WRpt = zeros(nxx,0);
-XxRpt = zeros(nxx,0);
-EaRpt = zeros(ne,0);
-EuRpt = zeros(ne,0);
+YRpt = zeros(ny, 0);
+WRpt = zeros(nxx, 0);
+XxRpt = zeros(nxx, 0);
+EaRpt = zeros(ne, 0);
+EuRpt = zeros(ne, 0);
 
 % Correction vector for nonlinear equations.
-v = zeros(nn,0);
+v = zeros(nn, 0);
 
-Exit = zeros(1,nSgm);
-Addf = nan(nn,nPerMax,nSgm);
-Dcy = nan(nn,nPer);
+Exit = zeros(1, nSgm);
+Addf = nan(nn, nPerMax, nSgm);
+Dcy = nan(nn, nPer);
 
 for iSgm = 1 : nSgm
     % The segment dates are defined by `first` to `last`, a total of `nper1`
@@ -111,53 +111,53 @@ for iSgm = 1 : nSgm
     % Last period simulated in nonlinear mode.
     lastNonl = first + nPerNonl - 1;
     % Last period simulated.
-    lastSim = max([lastRpt,lastNonl,lastEa,lastEndgA,lastExg]);
+    lastSim = max([lastRpt, lastNonl, lastEa, lastEndgA, lastExg]);
     % Number of periods reported in the final output data.
     nPerRpt = lastRpt - first + 1;
     % Number of periods simulated.
     nPerSim = lastSim - first + 1;
-    nPerUsed = min(nPerRpt,nPerNonl);
+    nPerUsed = min(nPerRpt, nPerNonl);
     
     % Prepare shocks: Combine anticipated shocks on the whole segment with
     % unanticipated shocks in the initial period.
     rangeSim = first : lastSim;
-    S.Ea = ea(:,rangeSim);
-    S.Eu = eu(:,first);
+    S.Ea = ea(:, rangeSim);
+    S.Eu = eu(:, first);
     
     % Steady-state trends; they include pre-sample init condition.
     if S.IsDeviation && S.IsAddSstate
-        S.XBar = xBar(:,1+(first-1:lastSim));
-        S.YBar = yBar(:,1+(first-1:lastSim));
+        S.XBar = xBar(:, 1+(first-1:lastSim));
+        S.YBar = yBar(:, 1+(first-1:lastSim));
     end
     
     % Prepare anchors: Anticipated and unanticipated endogenised shocks cannot
     % be combined in non-linear simulations. If there is no anchors, we can
     % leave the fields empty.
     if isEaAnch
-        S.Anch = anch(:,rangeSim);
-        S.Wght = wght(:,rangeSim);
-        S.Tune = tune(:,rangeSim);
+        S.Anch = anch(:, rangeSim);
+        S.Wght = wght(:, rangeSim);
+        S.Tune = tune(:, rangeSim);
     elseif isEuAnch
-        S.Anch = [anch(:,first),false(ny+nxx+ne+ne,nPerSim-1)];
-        S.Wght = [wght(:,first),zeros(ne+ne,nPerSim-1)];
-        S.Tune = [tune(:,first),nan(ny+nxx,nPerSim-1)];
+        S.Anch = [anch(:, first), false(ny+nxx+ne+ne, nPerSim-1)];
+        S.Wght = [wght(:, first), zeros(ne+ne, nPerSim-1)];
+        S.Tune = [tune(:, first), nan(ny+nxx, nPerSim-1)];
     end
-    S.Anch(ny+nxx+ne+(1:ne),2:end) = false; % Reset unanticipated anchors.
-    S.Wght(ne+(1:ne),2:end) = 0; % Reset unanticipated weights.
+    S.Anch(ny+nxx+ne+(1:ne), 2:end) = false; % Reset unanticipated anchors.
+    S.Wght(ne+(1:ne), 2:end) = 0; % Reset unanticipated weights.
 
     % Reset counters and flags.
     S.Stop = 0;
     
     % Re-use addfactors from the previous segment.
-    v(:,end+1:nPerNonl) = 0;
+    v(:, end+1:nPerNonl) = 0;
 
     % Create segment string.
-    s = sprintf('%g:%g[%g]#%g',...
-        S.ZerothSegment+first, ...
-        S.ZerothSegment+lastRpt, ...
-        S.ZerothSegment+lastSim, ...
-        nPerNonl);
-    S.Selective.SegmentString = sprintf('%16s',s);
+    s = sprintf( '%g:%g[%g]#%g', ...
+                 S.ZerothSegment+first, ...
+                 S.ZerothSegment+lastRpt, ...
+                 S.ZerothSegment+lastSim, ...
+                 nPerNonl );
+    S.Selective.SegmentString = sprintf('%16s', s);
     
     % Simulate this segment
     %-----------------------
@@ -170,55 +170,54 @@ for iSgm = 1 : nSgm
         if isFastUpd
             % Get a multliplier matrix only for the elements of yxx that enter
             % nonlinear equations.
-            yxAnch = repmat(S.Selective.IxUpdN,1,nPerNonl);
-            S.Selective.Je = simulate.linear.multipliers(S,yxAnch);
+            yxAnch = repmat(S.Selective.IxUpdN, 1, nPerNonl);
+            S.Selective.Je = simulate.linear.multipliers(S, yxAnch);
         end
     end
     
     % Iterate over nonlinear factors.
-    [v,iDcy,iExit] = simulate.selective.segment(S,v);
+    [v, iDcy, iExit] = simulate.selective.segment(S, v);
     
     % Rerun simulation with nonlinear factors for `nPerSim` periods.
-    [iY,iXx,iEa,iEu,iW] = simulate.linear.run(S,nPerSim,v);
+    [iY, iXx, iEa, iEu, iW] = simulate.linear.run(S, nPerSim, v);
     
-    YRpt = [YRpt,iY(:,1:nPerRpt)]; %#ok<AGROW>
-    WRpt = [WRpt,iW(:,1:nPerRpt)]; %#ok<AGROW>
-    XxRpt = [XxRpt,iXx(:,1:nPerRpt)]; %#ok<AGROW>
-    EaRpt = [EaRpt,iEa(:,1:nPerRpt)]; %#ok<AGROW>
-    EuRpt = [EuRpt,iEu(:,1),zeros(ne,nPerRpt-1)]; %#ok<AGROW>
+    YRpt = [YRpt, iY(:, 1:nPerRpt)]; %#ok<AGROW>
+    WRpt = [WRpt, iW(:, 1:nPerRpt)]; %#ok<AGROW>
+    XxRpt = [XxRpt, iXx(:, 1:nPerRpt)]; %#ok<AGROW>
+    EaRpt = [EaRpt, iEa(:, 1:nPerRpt)]; %#ok<AGROW>
+    EuRpt = [EuRpt, iEu(:, 1), zeros(ne, nPerRpt-1)]; %#ok<AGROW>
     
     % Update initial condition for next segment.
-    S.Alp0 = iW(nf+1:end,nPerRpt);
+    S.Alp0 = iW(nf+1:end, nPerRpt);
     
     % Report diagnostic output arguments.
-    Dcy(:,first+(0:nPerUsed-1)) = iDcy(:,1:nPerUsed);
-    Addf(:,first+(0:size(v,2)-1),iSgm) = v;
+    Dcy(:, first+(0:nPerUsed-1)) = iDcy(:, 1:nPerUsed);
+    Addf(:, first+(0:size(v, 2)-1), iSgm) = v;
     Exit(iSgm) = iExit;
     
     % Remove add-factors within the current segment's reported range. Any
     % add-factors going beyond the reported range end will be used as starting
     % values in the next segment.
-    v(:,1:nPerUsed) = [ ];
+    v(:, 1:nPerUsed) = [ ];
     
     % Update progress bar only if no iteration reports are printed.
-    if ~isempty(S.progress) && S.Selective.Display==0
-        update(S.progress,((S.ILoop-1)*nSgm+iSgm)/(S.NLoop*nSgm));
+    if ~isempty(S.progress)
+        update(S.progress, ((S.ILoop-1)*nSgm+iSgm)/(S.NLoop*nSgm));
     end
 end
 
 return
 
     
-    
-    function doFastUpdMatrices( )
+    function fastUpdate( )
         % Index of elements in [y;xx] vector that need to be updated in nonlinear
         % simulations:
-        % * variables that occur in nonlinear equations, and
+        % * variables that occur in nonlinear equations
         % * exogenized variables.
-        nPerJ =  max(nPerNonl,lastExg);
+        nPerJ =  max(nPerNonl, lastExg);
         ixUpd = S.Selective.IxUpdN;
         if isAnch
-            ixUpd = ixUpd | any(S.Anch(1:ny+nxx,:),2);
+            ixUpd = ixUpd | any(S.Anch(1:ny+nxx, :), 2);
         end
         nIxUpd = nnz(ixUpd);
         
@@ -231,29 +230,26 @@ return
             return
         end
         
-        Jvt = zeros(ny+nxx,nn*nPerNonl);
-        Jv = zeros(nIxUpd*nPerJ,nn*nPerNonl); % Effect of nonlinear addfactors.
+        Jvt = zeros(ny+nxx, nn*nPerNonl);
+        Jv = zeros(nIxUpd*nPerJ, nn*nPerNonl); % Effect of nonlinear addfactors.
         for tt = 1 : nPerJ
             if tt==1
-                Q = S.Q(:,1:nn*nPerNonl);
+                Q = S.Q(:, 1:nn*nPerNonl);
                 Jwv = Q;
             elseif tt<=nPerNonl
-                Q(:) = [ zeros(nxx,nn), Q(:,1:end-nn) ];
-                Jwv(:) = S.T*Jwv(nf+1:end,:) + Q;
+                Q(:) = [ zeros(nxx, nn), Q(:, 1:end-nn) ];
+                Jwv(:) = S.T*Jwv(nf+1:end, :) + Q;
             else
-                Jwv(:) = S.T*Jwv(nf+1:end,:);
+                Jwv(:) = S.T*Jwv(nf+1:end, :);
             end
             Jvt(:) = [ ...
-                S.Z*Jwv(nf+1:end,:); ...
-                Jwv(1:nf,:); ...
-                S.U*Jwv(nf+1:end,:); ...
+                S.Z*Jwv(nf+1:end, :); ...
+                Jwv(1:nf, :); ...
+                S.U*Jwv(nf+1:end, :); ...
                 ];
-            Jv((tt-1)*nIxUpd+(1:nIxUpd),:) = Jvt(ixUpd,:);
+            Jv((tt-1)*nIxUpd+(1:nIxUpd), :) = Jvt(ixUpd, :);
         end
         S.Selective.Jv = Jv;
         S.Selective.IxUpd = ixUpd;
-    end % doUpdMatrices( )
-
-
-
-end
+    end%
+end%
--- Modeling/+simulate/+selective/segment.m
+++ Modeling/+simulate/+selective/segment.m
@@ -1,22 +1,22 @@
 function [v, dcy, exit] = segment(s, v0)
-% segment  Nonlinear simulation of one segment.
+% segment  Nonlinear simulation of one segment
 %
-% Backend IRIS function.
-% No help provided.
+% Backend IRIS function
+% No help provided
 
-% -IRIS Macroeconomic Modeling Toolbox.
-% -Copyright (c) 2007-2018 IRIS Solutions Team.
+% -IRIS Macroeconomic Modeling Toolbox
+% -Copyright (c) 2007-2018 IRIS Solutions Team
 
 %--------------------------------------------------------------------------
 
 dcy = [ ];
 exit = 0;
 
-ny = size(s.Z,1);
-nxx = size(s.T,1);
-nb = size(s.T,2);
+ny = size(s.Z, 1);
+nxx = size(s.T, 1);
+nb = size(s.T, 2);
 nf = nxx - nb;
-ne = size(s.Ea,1);
+ne = size(s.Ea, 1);
 nEqtn = length(s.Eqtn);
 eqtnNI = s.Selective.EqtnNI;
 sgmStr = s.Selective.SegmentString;
@@ -36,42 +36,42 @@ if isAnch
     % `eaAnch`, `euAnch` do not include init cond.
     % `ixUpdN` does include init cond.
     Je = s.Selective.Je;
-    yxxAnch = s.Anch(1:ny+nxx,:);
-    eaAnch = s.Anch(ny+nxx+(1:ne),:);
-    euAnch = s.Anch(ny+nxx+ne+(1:ne),:);
-    lastExg = max([0,find(any(yxxAnch,1),1,'last')]);
-    lastEndgA = max([0,find(any(eaAnch,1),1,'last')]);
-    lastEndgU = max([0,find(any(euAnch,1),1,'last')]);
-    eaAnch = eaAnch(:,1:lastEndgA);
-    euAnch = euAnch(:,1:lastEndgU);
-    ixUpdN = [ false(ny+nxx,1), repmat(s.Selective.IxUpdN,1,nPerNonl) ];
+    yxxAnch = s.Anch(1:ny+nxx, :);
+    eaAnch = s.Anch(ny+nxx+(1:ne), :);
+    euAnch = s.Anch(ny+nxx+ne+(1:ne), :);
+    lastExg = max([0, find(any(yxxAnch, 1), 1, 'last')]);
+    lastEndgA = max([0, find(any(eaAnch, 1), 1, 'last')]);
+    lastEndgU = max([0, find(any(euAnch, 1), 1, 'last')]);
+    eaAnch = eaAnch(:, 1:lastEndgA);
+    euAnch = euAnch(:, 1:lastEndgU);
+    ixUpdN = [ false(ny+nxx, 1), repmat(s.Selective.IxUpdN, 1, nPerNonl) ];
 end
 
 % Prepare nonlinear update
 %--------------------------
-nPerUpd = max(nPerNonl,lastExg);
-[y,xx] = simulate.linear.plain(s, ...
-    s.IsDeviation,s.Alp0,s.Ea,s.Eu,nPerUpd,[ ]);
+nPerUpd = max(nPerNonl, lastExg);
+[y, xx] = simulate.linear.plain(s, ...
+    s.IsDeviation, s.Alp0, s.Ea, s.Eu, nPerUpd, [ ]);
 % Add init condition.
-yxx = [ [ nan(ny,1); nan(nf,1); s.U*s.Alp0 ] , [y; xx] ];
+yxx = [ [ nan(ny, 1); nan(nf, 1); s.U*s.Alp0 ] , [y; xx] ];
 if s.IsDeviation && s.IsAddSstate
     yxx = yxx + [ 
-        s.YBar(:,1+(0:nPerUpd))
-        s.XBar(:,1+(0:nPerUpd))
+        s.YBar(:, 1+(0:nPerUpd))
+        s.XBar(:, 1+(0:nPerUpd))
         ];
     if isAnch
         % Add steady-state lines to tunes because all yxx results here do include
         % the steady-state lines, too. It is easier to add them here than subtract
         % them in doEvalDcy( ).
-        s.Tune = s.Tune + [s.YBar(:,2:end);s.XBar(:,2:end)];
+        s.Tune = s.Tune + [s.YBar(:, 2:end);s.XBar(:, 2:end)];
     end
 end
 e = s.Ea;
-e(:,1) = e(:,1) + s.Eu;
-e = [ zeros(ne,1), e ];
+e(:, 1) = e(:, 1) + s.Eu;
+e = [ zeros(ne, 1), e ];
 
 Jv = s.Selective.Jv( 1:nnz(s.Selective.IxUpd)*nPerUpd, : );
-ixUpd = [ false(ny+nxx,1), repmat(s.Selective.IxUpd,1,nPerUpd) ];
+ixUpd = [ false(ny+nxx, 1), repmat(s.Selective.IxUpd, 1, nPerUpd) ];
 
 % Delog only the variables that enter nonlinear equations.
 ixLogUpdN = [s.IxYLog;s.IxXLog] & s.Selective.IxUpdN;
@@ -89,38 +89,31 @@ p = real( s.Update.Quantity(1, s.NameType==4) );
 tVecAbs = -s.MinT + s.First + rangeNonl - 1;
 L = s.L;
 
-if isequal(s.Solver, @qad)
-    % __QaD Solver__
-    [v,dcy,exit] = simulate.selective.qad(@doEvalDcy,v0,s.Selective);
+solverName = s.Solver.SolverName;
 
+if any(strcmpi(solverName, {'IRIS-qad', 'IRIS-newton', 'IRIS-qnsd'}))
+    % IRIS Solver
+    [v, dcy, flag] = solver.algorithm.qnsd(@doEvalDcy, v0, s.Solver);
+
+elseif strcmpi(solverName, 'fsolve')
+    % Optimization Tbx
+    [v, dcy, flag] = fsolve(@doEvalDcy, v0, s.Solver);
+
+elseif strcmpi(solverName, 'lsqnonlin')
+    % Optimization Tbx
+    [v, ~, dcy, flag] = lsqnonlin(@doEvalDcy, v0, [ ], [ ], s.Solver);
+
+end
+
+v = reshape(v, nn, nPerNonl);
+if flag>0
+    exit = 1;
 else
-    % __IRIS Solver__
-    if strcmpi(s.Solver, 'IRIS')
-        opt = solver.Options( 'Steady', ...
-                              'Lambda=', [], ...
-                              'StepUp=', false, ...
-                              'SpecifyObjectiveGradient', false, ...
-                              'MaxFunctionEvaluations', 5000, ...
-                              'MaxIterations', 10000 );
-        [v, flag] = solver.algorithm.lm(@doEvalDcy, v0, opt);
-        dcy = zeros(size(v0));
-    
-    % __Optimization Tbx__
-    elseif isequal(s.Solver, @fsolve)
-        [v,dcy,flag] = fsolve(@doEvalDcy,v0,s.OptimSet);
-    elseif isequal(s.Solver,@lsqnonlin)
-        [v,~,dcy,flag] = lsqnonlin(@doEvalDcy,v0,[ ],[ ],s.OptimSet);
-    end
-    v = reshape(v,nn,nPerNonl);
-    if flag>0
-        exit = 1;
-    else
-        exit = -1;
-    end
+    exit = -1;
 end
 
 % __Failed to Converge__
-if exit < 0
+if exit<0
     doRptFailure( );
 end
 
@@ -138,26 +131,26 @@ return
         
         % Delog variables that occur in nonlinear equations.
         if isLogUpdN
-            yxx1(ixLogUpdN,:) = real(exp(yxx1(ixLogUpdN,:)));
+            yxx1(ixLogUpdN, :) = real(exp(yxx1(ixLogUpdN, :)));
         end
         
         % Evaluate discrepancies.
-        y1 = yxx1(1:ny,:);
-        xx1 = yxx1(ny+1:end,:);
+        y1 = yxx1(1:ny, :);
+        xx1 = yxx1(ny+1:end, :);
         errMsg = { };
-        Dcy = zeros(nEqtn,nPerNonl);
-        ixNan = false(1,nEqtn);
+        Dcy = zeros(nEqtn, nPerNonl);
+        ixNan = false(1, nEqtn);
         for j = find(ixNonl)
             try
-                jDcy = eqtnNI{j}(y1,xx1,e1,p,tVec,L,tVecAbs);
+                jDcy = eqtnNI{j}(y1, xx1, e1, p, tVec, L, tVecAbs);
                 ixNan(j) = any(~isfinite(jDcy));
-                Dcy(j,:) = jDcy;
+                Dcy(j, :) = jDcy;
             catch Err
                 errMsg{end+1} = s.Eqtn{j}; %#ok<AGROW>
                 errMsg{end+1} = Err.message; %#ok<AGROW>
             end
         end
-        Dcy = Dcy(ixNonl,:);
+        Dcy = Dcy(ixNonl, :);
         if any(ixNan) || ~isempty(errMsg)
             doErrors( );
         end
@@ -172,9 +165,9 @@ return
             else
                 % Incremental simulation of nonlinear addfactors.
                 isDeviation = true;
-                [addY,addXx] = simulate.linear.plain(s, ...
-                    isDeviation,[ ],[ ],[ ],nPerUpd,V);
-                yxx1(:,2:end) = yxx1(:,2:end) + [ addY; addXx ];
+                [addY, addXx] = simulate.linear.plain(s, ...
+                    isDeviation, [ ], [ ], [ ], nPerUpd, V);
+                yxx1(:, 2:end) = yxx1(:, 2:end) + [ addY; addXx ];
             end
         end 
         
@@ -187,22 +180,22 @@ return
             %
             % `S.Tune` already includes steady-state line, so that the tunes are
             % compatible with the results in `yxx1`.
-            [ea1,eu1,addEa,addEu] = ...
-                simulate.linear.exogenize(s,s.M,yxx1(:,2:end),s.Ea,s.Eu);
+            [ea1, eu1, addEa, addEu] = ...
+                simulate.linear.exogenize(s, s.M, yxx1(:, 2:end), s.Ea, s.Eu);
             if isFastUpd
                 yxx1(ixUpdN) = yxx1(ixUpdN) ...
                     + Je*[ addEa(eaAnch); addEu(euAnch) ];
             else
                 % Incremental simulation of endogenized shocks.
                 isDeviation = true;
-                [addY,addXx] = simulate.linear.plain(s, ...
-                    isDeviation,[ ],addEa,addEu,nPerNonl,[ ]);
-                yxx1(:,1+(1:nPerNonl)) = yxx1(:,1+(1:nPerNonl)) ...
+                [addY, addXx] = simulate.linear.plain(s, ...
+                    isDeviation, [ ], addEa, addEu, nPerNonl, [ ]);
+                yxx1(:, 1+(1:nPerNonl)) = yxx1(:, 1+(1:nPerNonl)) ...
                     + [ addY; addXx ];
             end
             e1 = ea1;
-            e1(:,1) = e1(:,1) + eu1(:,1);
-            e1 = [ zeros(ne,1), e1 ];
+            e1(:, 1) = e1(:, 1) + eu1(:, 1);
+            e1 = [ zeros(ne, 1), e1 ];
         end
        
         
@@ -238,12 +231,12 @@ return
                 msgFunc('simulate:selective:segment', ...
                     ['Nonlinear simulation #%g, segment %s, ', ...
                     'reached max number of iterations without convergence.'], ...
-                    s.ILoop,strtrim(sgmStr));
+                    s.ILoop, strtrim(sgmStr));
             case -2
                 msgFunc('simulate:selective:segment', ...
                     ['Nonlinear simulation #%g, segment %s, ', ...
                     'crashed at Inf, -Inf, or NaN.'], ...
-                    s.ILoop,strtrim(sgmStr));
+                    s.ILoop, strtrim(sgmStr));
         end
     end
 end
