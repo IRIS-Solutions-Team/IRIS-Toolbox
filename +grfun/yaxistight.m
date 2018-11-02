2c34dc2c1de4fb536b7e89ff542ec6d8bf0cfb0b
Jaromir Benes <jaromir.benes@gmail.com>
2018-11-02 09:20:29 -0500

    Clean up

--- +grfun/yaxistight.m
+++ +grfun/yaxistight.m
@@ -14,17 +14,19 @@ function yaxistight(handlesAxes)
 %
 % __Description__
 %
-% Behaviour of `grfun.yaxistight` differs from the standard function `axis`
-% in that it disregards `grfun.vline`, `grfun.zeroline` and
-% `grfun.highlight` objects when determining the minimum and maximum on the
-% vertical axis.
+% Calling `grfun.yaxistight( )` can be now directly replaced by setting the
+% following axes property
+%
+%     set(h, 'YLimSpec', 'Tight')
+%
+% where `h` is the handle to the respective axes object.
 %
 %
 % __Example__
 %
 
-% -IRIS Macroeconomic Modeling Toolbox.
-% -Copyright (c) 2007-2018 IRIS Solutions Team.
+% -IRIS Macroeconomic Modeling Toolbox
+% -Copyright (c) 2007-2018 IRIS Solutions Team
 
 if nargin==0
     handlesAxes = visual.backend.getCurrentAxesIfExists( );
@@ -38,41 +40,5 @@ end
 
 set(handlesAxes, 'YLimSpec', 'Tight');
 
-end
+end%
 
-%{
-for ithAxes = handlesToAxes
-    [yMin, yMax] = getActualYDataLimits(ithAxes);
-    if isinf(yMin) && isinf(yMax)
-        continue
-    end
-    if yMin>=yMax
-        continue
-    end
-    set(ithAxes, 'YLim', [yMin, yMax], 'YLimMode', 'Manual');
-end
-
-end
-
-
-function [yMin, yMax] = getActualYDataLimits(ax)
-    obj = findobj(ax, ...
-        '-property', 'YData', ...
-        '-not', 'Tag', 'vline', ...
-        '-not', 'Tag', 'hline', ...
-        '-not', 'Tag', 'highlight' ...
-    );
-    yMin = Inf;
-    yMax = -Inf;
-    for i = 1 : numel(obj)
-        ithObj = obj(i);
-        ithYData = get(ithObj, 'YData');
-        if isempty(ithYData)
-            continue
-        end
-        yMin = min(yMin, min(ithYData(:)));
-        yMax = max(yMax, max(ithYData(:)));
-    end
-    keyboard
-end
-%}
