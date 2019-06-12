cafa0cd77b34260cf9f13b7031e3c8edd9451656
Jaromir Benes <jaromir.benes@gmail.com>
2019-06-12 11:34:03 +0200

    Remove obsolete functions

--- utils/baracf.m
+++
@@ -1,9 +0,0 @@
-function handle = baracf(c,x,y,varargin)
-
-order = size(c,3) - 1;
-c1 = c(x,y,end:-1:1);
-c2 = c(y,x,2:end);
-aux = [c1(:);c2(:)]';
-handle = bar(-order:order,aux,varargin{:});
-
-end
