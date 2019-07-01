32e82511e848e073162a715300e1e2bdb4487e1b
Jaromir Benes <jaromir.benes@gmail.com>
2019-07-01 14:47:26 +0200

    Introduce modular get queries for @model and @Model equations: Dynamic/Steady-Transition/Measurement-Equations

--- Modeling/+model/+component/@Equation/implementGet.m
+++ Modeling/+model/+component/@Equation/implementGet.m
@@ -4,6 +4,7 @@ TYPE = @int8;
 PTR = @int16;
 answ = [ ];
 isValid = true;
+numOfEquations = numel(this.Input);
 
 query1 = query;
 query1 = lower(query1);
@@ -11,30 +12,42 @@ query1 = strrep(query1, 'eqtns', 'eqtn');
 query1 = strrep(query1, 'labels', 'label');
 query1 = regexprep(query1, '[^\w]', '');
 
+flagStruct = hereGetFlags( );
 
 if any(strcmpi(query1, {'eqtn', 'equations', 'allEquations'}))
     answ = this.Input;
-    answ = answ.';
-
+    if flagStruct.Dynamic
+        answ = model.component.Equation.extractInput(answ, 'Dynamic');
+    elseif flagStruct.Steady
+        answ = model.component.Equation.extractInput(answ, 'Steady');
+    end
+    select = true(1, numOfEquations);
+    if flagStruct.Measurement
+        select = this.Type==TYPE(1);
+    elseif flagStruct.Transition
+        select = this.Type==TYPE(2);
+    end
+    answ = answ(select);
+    answ = transpose(answ);
 
 elseif any(strcmpi(query1, {'EqtnLabel', 'Label'}))
     answ = this.Label;
-    answ = answ.';
+    answ = transpose(answ);
     
     
 elseif any(strcmpi(query1, {'EqtnAlias'}))
     answ = this.Alias;
-    answ = answ.';
+    answ = transpose(answ);
     
     
-elseif strcmpi(query1, 'EqtnSteady')
+elseif any(strcmpi(query1, {'EqtnSteady'}))
     answ = model.component.Equation.extractInput(this.Input, 'Steady');
-    answ = answ.';
+    answ = transpose(answ);
     
     
-elseif strcmpi(query1, 'EqtnDynamic')
+elseif any(strcmpi(query1, {'EqtnDynamic'}))
     answ = model.component.Equation.extractInput(this.Input, 'Dynamic');
-    answ = answ.';
+    answ = transpose(answ);
     
     
 elseif any(strcmpi(query1, ...
@@ -54,11 +67,26 @@ elseif any(strcmpi(query1, ...
     elseif strcmpi(query1(2:end), 'EqtnAlias')
         answ = this.Alias(ix);
     end
-    answ = answ.';
+    answ = transpose(answ);
     
 
 else
     isValid = false;
 end
 
+return
+
+
+    function flagStruct = hereGetFlags( )
+        listOfFlags = { 'Dynamic'
+                        'Steady'
+                        'Measurement'
+                        'Transition' };
+        flagStruct = struct( );
+        for i = 1 : numel(listOfFlags)
+            temp = query1;
+            query1 = regexprep(query1, listOfFlags{i}, '', 'IgnoreCase');
+            flagStruct.(listOfFlags{i}) = ~isequal(temp, query1);
+        end
+    end%
 end%
