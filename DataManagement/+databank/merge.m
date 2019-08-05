8643056f0db11b0abb17894caaffa93e7d2aae75
Jaromir Benes <jaromir.benes@gmail.com>
2019-08-05 10:26:52 +0200

    Add 'error' as another databank.merge method

--- DataManagement/+databank/merge.m
+++ DataManagement/+databank/merge.m
@@ -3,38 +3,53 @@ function mainDatabank = merge(method, mainDatabank, varargin)
 %{
 % ## Syntax ##
 %
-%     d = databank.merge(d, d1, ...)
+%     d = databank.merge(method, d, d1, ...)
 %
 %
 % ## Input Arguments ##
 %
-% **`Method`** [ `'horzcat'` | `'replace'` | `'discard'` ] -
-% Method applied when two or more of the input databanks contain a field of
-% the same name; see Description.
+% __`method`__ [ `'horzcat'` | `'replace'` | `'discard'` | `'error'` ] -
+% Action to perform when two or more of the input databanks contain a field
+% of the same name; see Description.
 %
-% **`d`** [ struct ] -
+% __`d`__ [ struct | Dictionary | containers.Map ] -
 % Databank that will be merged with the other input databanks, `d1`, etc.
 % using the method specified by `Method=`.
 %
-% **`d1`** [ struct ] -
+% __`d1`__ [ struct | Dictionary | containers.Map ] -
 % One or more databanks which will be merged with the input databank `d`.
 %
 %
 % ## Output Arguments ##
 %
-% **`d`** [ struct ] -
+% __`d`__ [ struct | Dictionary | containers.Map ] -
 % Output databank created by merging the input databanks using the method
 % specified by `Method=`.
 %
 %
 % ## Options ##
 %
-% **`MissingField=@rmfield`** [ `@rmfield` | `NaN` | `[ ]` | * ] -
-% What to do when a field is missing from one or more of the input
+% __`MissingField=@rmfield`__ [ `@rmfield` | `NaN` | `[ ]` | * ] -
+% Action to perform when a field is missing from one or more of the input
 % databanks when applying the method `'horzcat'`.
 %
 %
 % ## Description ##
+%
+% The fields from each of the additional databnaks (`d1` and further) are
+% added to the main databank `d`. If the name of a field to be added
+% already exists in the main databank, `d`, one of the following actions is
+% performed:
+%
+% * `horzcat` - the fields will be horizontally concatenated;
+%
+% * `replace` - the field in the main databank will be replaced with the
+% new field;
+% 
+% * `discard` - the field in the main databank will be kept unchanged, and
+% the new field will be discarded;
+% 
+% * `error` - an error will be thrown.
 %
 %
 % ## Example ##
@@ -66,8 +81,8 @@ end
 persistent parser
 if isempty(parser)
     parser = extend.InputParser('databank.merge');
-    parser.addRequired('Method', @(x) any(strcmpi(x, {'horzcat', 'replace', 'discard'})));
-    parser.addRequired('InputDatabank', @isstruct);
+    parser.addRequired('Method', @(x) Valid.anyString(x, 'horzcat', 'replace', 'discard', 'error'));
+    parser.addRequired('InputDatabank', @Valid.databank);
     parser.addRequired('MergeWith');
     parser.addParameter('MissingField', @rmfield);
 end
@@ -82,8 +97,10 @@ if strcmpi(method, 'horzcat')
     mergeNext = @horzcatNext;
 elseif strcmpi(method, 'replace')
     mergeNext = @replaceNext;
-else
+elseif strcmpi(method, 'discard')
     mergeNext = @discardNext;
+else
+    mergeNext = @errorNext;
 end
 
 for i = 1 : numOfMergeWith
@@ -124,6 +141,8 @@ function mainDatabank = horzcatNext(mainDatabank, mergeWith, opt)
 end%
 
 
+
+
 function mainDatabank = replaceNext(mainDatabank, mergeWith, ~)
     newFields = fieldnames(mergeWith);
     numOfNewFields = numel(newFields);
@@ -132,6 +151,8 @@ function mainDatabank = replaceNext(mainDatabank, mergeWith, ~)
         mainDatabank.(name) = mergeWith.(name);
     end
 end%
+
+
 
 
 function mainDatabank = discardNext(mainDatabank, mergeWith, ~)
@@ -143,6 +164,29 @@ function mainDatabank = discardNext(mainDatabank, mergeWith, ~)
             continue
         end
         mainDatabank.(name) = mergeWith.(name);
+    end
+end%
+
+
+
+
+function mainDatabank = errorNext(mainDatabank, mergeWith, ~)
+    newFields = fieldnames(mergeWith);
+    numOfNewFields = numel(newFields);
+    inxOfErrorFields = false(1, numOfNewFields);
+    for i = 1 : numOfNewFields
+        name = newFields{i};
+        if isfield(mainDatabank, name)
+            inxOfErrorFields(i) = true;
+            continue
+        end
+        mainDatabank.(name) = mergeWith.(name);
+    end
+    if any(inxOfErrorFields)
+        THIS_ERROR = { 'Databank:ErrorMergingFieldsWithSameName'
+                       'This field name occurs in more than one input databank: %s ' };
+        throw( exception.Base(THIS_ERROR, 'error'), ...
+               newFields{inxOfErrorFields} );
     end
 end%
 
