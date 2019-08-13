803df9d80f9e331c58dda67bb752109bb5d7683a
Jaromir Benes <jaromir.benes@gmail.com>
2019-08-13 09:35:25 +0200

    Case sensitive and insensitive Dictionary

--- DataManagement/@Dictionary/Dictionary.m
+++ DataManagement/@Dictionary/Dictionary.m
@@ -1,6 +1,56 @@
+% Dictionary  
+%
+% Dictionary is a databank like object similar to the standard Matlab
+% `struct`.
+% 
+% 
+% Dictionary methods:
+%
+% ## Summary of Dictionary Functions by Category ##
+%
+% ### Constructor ###
+% ------------------------------------------------------------------------------------------------------------
+%   Dictionary                - 
+%
+%
+% ### Creating New Copies of Existing Dictionary Objects ###
+% ------------------------------------------------------------------------------------------------------------
+%   copy                      - 
+%
+%
+% ### Getting Information about Dictionary Objects ###
+% ------------------------------------------------------------------------------------------------------------
+%   keys                      - 
+%   values                    - 
+%   isKey                     - 
+%
+%
+% ### Adding, Getting and Removing Dictionary Fields ###
+% ------------------------------------------------------------------------------------------------------------
+%   subsref                   - 
+%   subsasgn                  - 
+%   store                     - 
+%   retrieve                  - 
+%   remove                    - 
+%
+%
+% #### Alternative Function Names ###
+% ------------------------------------------------------------------------------------------------------------
+%   setfield                  - 
+%   getfield                  - 
+%   fieldnames                - 
+%   isfield                   - 
+%   rmfield                   - 
+%
+
+
+% -IRIS Macroeconomic Modeling Toolbox
+% -Copyright (c) 2007-2019 IRIS Solutions Team
+
+
 classdef Dictionary < matlab.mixin.Copyable
     properties
-        EnforceCase
+        CaseSensitive = true
     end
 
 
@@ -21,8 +71,23 @@ classdef Dictionary < matlab.mixin.Copyable
 
 
 
+    properties (Constant, Hidden)
+        WARNING_KEY_NOT_FOUND =    { 'Dictionary:KeyNotFound'
+                                     'This key does not exist in the Dictionary: %s ' }
+
+        ERROR_INVALID_REFERENCE =  { 'Dictionary:InvalidSubscriptedReference'
+                                     'Invalid subscripted reference to a Dictionary' }
+
+        ERROR_INVALID_ASSIGNMENT = { 'Dictionary:InvalidSubscriptedAssignment'
+                                     'Invalid subscripted assignment to a Dictionary' }
+    end
+
+
+
+
     methods
         function this = Dictionary(varargin)
+% Dictionary  Create new Dictionary object
             if nargin==0
                 return
             end
@@ -31,6 +96,13 @@ classdef Dictionary < matlab.mixin.Copyable
                 return
             end
             this = store(this, varargin{:});
+        end%
+
+
+
+
+        function this = caseSensitive(this, newValue)
+            this.CaseSensitive = newValue;
         end%
 
 
@@ -55,9 +127,7 @@ classdef Dictionary < matlab.mixin.Copyable
             end
             return
                 function hereThrowError( )
-                    THIS_ERROR = { 'Dictionary:InvalidSubscriptedReference'
-                                   'Invalid subscripted reference to a Dictionary' };
-                    throw( exception.Base(THIS_ERROR, 'error') );
+                    throw( exception.Base(this.ERROR_INVALID_REFERENCE, 'error') );
                 end%
         end%
 
@@ -80,9 +150,7 @@ classdef Dictionary < matlab.mixin.Copyable
             this = store(this, key, value);
             return
                 function hereThrowError( )
-                    THIS_ERROR = { 'Dictionary:InvalidSubscriptedAssignment'
-                                   'Invalid subscripted assignment to a Dictionary' };
-                    throw( exception.Base(THIS_ERROR, 'error') );
+                    throw( exception.Base(this.ERROR_INVALID_ASSIGNMENT, 'error') );
                 end%
         end%
 
@@ -153,10 +221,16 @@ classdef Dictionary < matlab.mixin.Copyable
 
 
 
+        function flag = isKey(this, key)
+            flag = lookupKey(this, key);
+        end%
+
+
+
+
         function this = store(this, varargin)
             for i = 1 : 2 : numel(varargin)
                 key = string(varargin{i});
-                key = enforceCase(this, key);
                 value = varargin{i+1};
                 [flag, pos] = lookupKey(this, key);
                 if flag
@@ -173,32 +247,31 @@ classdef Dictionary < matlab.mixin.Copyable
 
         function value = retrieve(this, key)
             [flag, pos] =  lookupKey(this, key);
-            if flag
-                value = this.Values{pos};
-            else
-                THIS_ERROR = { 'Dictionary:KeyNotFound'
-                               'This key does not exist in the Dictionary: %s ' };
-                throw( exception.Base(THIS_ERROR, 'error'), ...
+            if ~flag
+                throw( exception.Base(this.WARNING_KEY_NOT_FOUND, 'error'), ...
                        key );
             end
+            value = this.Values{pos};
         end%
 
 
 
 
         function this = remove(this, keys)
+            if isa(keys, 'Except')
+                keys.CaseSensitive = this.CaseSensitive;
+                keys = resolve(keys, this.Keys);
+            end
             pos = lookupKeys(this, keys);
             inxFound = ~isnan(pos);
+            if any(~inxFound)
+                keysNotFound = cellstr(keys(~inxFound));
+                throw( exception.Base(this.WARNING_KEY_NOT_FOUND, 'error'), ...
+                       keysNotFound{:} );
+            end
             pos = pos(inxFound);
             this.Keys(pos) = [ ];
             this.Values(pos) = [ ];
-            if any(~inxFound)
-                keysNotFound = cellstr(keys(~inxFound));
-                THIS_WARNING = { 'Dictionary:KeyNotFound'
-                                 'This key does not exist in the Dictionary: %s ' };
-                throw( exception.Base(THIS_WARNING, 'warning'), ...
-                       keysNotFound{:} );
-            end
         end%
     end        
 
@@ -215,18 +288,12 @@ classdef Dictionary < matlab.mixin.Copyable
 
 
     methods (Access=protected)
-        function key = enforceCase(this, key)
-            if ~isempty(this.EnforceCase)
-                key = this.EnforceCase(key);
-            end
-        end%
-
-
-
-
         function [flag, pos] = lookupKey(this, key)
-            key = enforceCase(this, key);
-            inx = this.Keys==key;
+            if isequal(this.CaseSensitive, true)
+                inx = this.Keys==key;
+            else
+                inx = lower(this.Keys)==lower(key);
+            end
             flag = any(inx);
             if flag
                 pos = find(inx, 1);
@@ -239,9 +306,12 @@ classdef Dictionary < matlab.mixin.Copyable
 
 
         function pos = lookupKeys(this, keys)
-            keys = enforceCase(this, keys);
             numOfInquiries = numel(keys);
-            inx = this.Keys(:)==transpose(keys(:));
+            if isequal(this.CaseSensitive, true)
+                inx = this.Keys(:)==transpose(keys(:));
+            else
+                inx = lower(this.Keys(:))==lower(transpose(keys(:)));
+            end
             pos = nan(size(keys));
             for i = find(any(inx, 1))
                 pos(i) = find(inx(:, i), 1);
