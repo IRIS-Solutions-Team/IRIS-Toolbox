0a2c08198857f6691bc4d0b985e5d29bab9f44cd
Jaromir Benes <jaromir.benes@gmail.com>
2019-09-02 09:38:50 +0200

    Adjust funtion backend function databank.backend.fromDoubleArrayNoFrills(~) to work with any OutputType=

--- DataManagement/+databank/+backend/fromDoubleArrayNoFrills.m
+++ DataManagement/+databank/+backend/fromDoubleArrayNoFrills.m
@@ -1,4 +1,10 @@
-function outputDatabank = fromDoubleArrayNoFrills(array, listOfNames, start, comments, inxToInclude, timeSeriesConstructor)
+function outputDatabank = fromDoubleArrayNoFrills( array, ...
+                                                   listOfNames, ...
+                                                   start, ...
+                                                   comments, ...
+                                                   inxToInclude, ...
+                                                   timeSeriesConstructor, ...
+                                                   outputType )
 % fromDoubleArrayNoFrills  Create databank from double array
 %
 % Backend IRIS function
@@ -7,47 +13,62 @@ function outputDatabank = fromDoubleArrayNoFrills(array, listOfNames, start, com
 % -IRIS Macroeconomic Modeling Toolbox
 % -Copyright (c) 2007-2019 IRIS Solutions Team
 
-try
-    timeSeriesConstructor;
-catch
+if isequal(timeSeriesConstructor, @default)
     timeSeriesConstructor = iris.get('DefaultTimeSeriesConstructor');
 end
 TIME_SERIES_TEMPLATE = timeSeriesConstructor( );
 
+if isequal(outputType, @default)
+    outputType = 'struct';
+end
+
 %--------------------------------------------------------------------------
 
-numOfDataSets = size(array, 3);
-numOfRows = size(array, 1);
+numPages = size(array, 3);
+numRows = size(array, 1);
 
 if isa(listOfNames, 'string')
     listOfNames = cellstr(listOfNames);
 end
 
 if isempty(comments)
-    comments = repmat({''}, 1, numOfRows);
+    comments = repmat({''}, 1, numRows);
 elseif isa(comments, 'string')
     comments = cellstr(comments);
 end
 
-if  numOfRows~=numel(listOfNames) || numOfRows~=numel(comments)
+if  numRows~=numel(listOfNames) || numRows~=numel(comments)
     THIS_ERROR = { 'Databank:InvalidSizeOfInputArguments'
                    'Invalid size of input arguments' };
     throw( exception.Base(THIS_ERROR, 'error') );
 end
 
-outputDatabank = struct( );
-try
-    inxToInclude;
-catch
-    inxToInclude = true(1, numOfRows);
+outputDatabank = databank.backend.ensureTypeConsistency([ ], outputType);
+
+if isequal(inxToInclude, @all)
+    inxToInclude = true(1, numRows);
 end
+
 for i = find(inxToInclude)
     ithData = array(i, :, :);
     ithData = permute(ithData, [2, 3, 1]);
     ithName = listOfNames{i};
     ithComment = comments{i};
-    outputDatabank.(ithName) = fill(TIME_SERIES_TEMPLATE, ithData, start, ithComment);
+    ithSeries = fill(TIME_SERIES_TEMPLATE, ithData, start, ithComment);
+    hereStoreNewField( );
 end
 
+return
+
+
+    function hereStoreNewField( )
+        if strcmpi(outputType, 'struct')
+            outputDatabank = setfield(outputDatabank, ithName, ithSeries);
+        elseif strcmpi(outputType, 'Dictionary')
+            outputDatabank = store(outputDatabank, ithName, ithSeries);
+        elseif strcmpi(outputType, 'containers.Map')
+            outputDatabank(ithName) = ithSeries;
+        end
+    end%
 end%
 
