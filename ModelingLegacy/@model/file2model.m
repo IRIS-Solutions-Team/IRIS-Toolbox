3d5689481575e4b06fcc805c73a4c2eef45d58dc
Jaromir Benes <jaromir.benes@gmail.com>
2019-09-06 10:51:05 +0200

    Clean up

--- ModelingLegacy/@model/file2model.m
+++ ModelingLegacy/@model/file2model.m
@@ -21,10 +21,10 @@ function [this, opt] = file2model(this, modelFile, opt, parserOpt, optimalOpt)
 
 % Export files; they must be available before we run the postparser because
 % we check for syntax errors by evaluating model equations which may refer
-% to exportable functions.
+% to exportable functions
 export(this);
 
-% Create database of parameters occurring in control expressions.
+% Create database of parameters occurring in control expressions
 d = struct( );
 for i = 1 : length(ctrlParameters)
     name = ctrlParameters{i};
@@ -32,7 +32,7 @@ for i = 1 : length(ctrlParameters)
 end
 this.PreparserControl = d;
 
-% Run the main model-specific parser.
+% Run the main model-specific parser
 the = parser.TheParser('model', this.FileName, code, opt.Assign);
 [quantity, equation, euc, puc] = parse(the, parserOpt);
 opt.Assign = the.AssignedDatabank;
@@ -40,4 +40,5 @@ opt.Assign = the.AssignedDatabank;
 % Run model-specific postparser
 this = postparse(this, quantity, equation, euc, puc, opt, optimalOpt);
 
-end
+end%
+
