568e993db680c47774c12ce7013e411977cba4ee
Jaromir Benes <jaromir.benes@gmail.com>
2019-09-20 10:20:38 +0200

    Add 'vertcat' method to +databank.merge

--- DataManagement/+databank/merge.m
+++ DataManagement/+databank/merge.m
@@ -8,15 +8,15 @@ function mainDatabank = merge(method, mainDatabank, varargin)
 %
 % ## Input Arguments ##
 %
-% __`method`__ [ `'horzcat'` | `'replace'` | `'discard'` | `'error'` ] -
+% __`method`__ [ `'horzcat'` | `'vertcat'` | `'replace'` | `'discard'` | `'error'` ] -
 % Action to perform when two or more of the input databanks contain a field
-% of the same name; see Description.
+% of the same ithName; see Description.
 %
 % __`d`__ [ struct | Dictionary | containers.Map ] -
 % Databank that will be merged with the other input databanks, `d1`, etc.
 % using the method specified by `Method=`.
 %
-% __`d1`__ [ struct | Dictionary | containers.Map ] -
+% __`d1`__ [ struct | Dictionary ] -
 % One or more databanks which will be merged with the input databank `d`.
 %
 %
@@ -37,7 +37,7 @@ function mainDatabank = merge(method, mainDatabank, varargin)
 % ## Description ##
 %
 % The fields from each of the additional databnaks (`d1` and further) are
-% added to the main databank `d`. If the name of a field to be added
+% added to the main databank `d`. If the ithName of a field to be added
 % already exists in the main databank, `d`, one of the following actions is
 % performed:
 %
@@ -63,16 +63,16 @@ if isempty(varargin)
     return
 end
 
-inxOfDatabanks = cellfun(@validate.databank, varargin);
-if ~inxOfDatabanks(1)
+inxDatabanks = cellfun(@validate.databank, varargin);
+if ~inxDatabanks(1)
     return
 end
 
-if all(inxOfDatabanks)
+if all(inxDatabanks)
     mergeWith = varargin;
     varargin(:) = [ ];
 else
-    posFirstNonStruct = find(~inxOfDatabanks, 1);
+    posFirstNonStruct = find(~inxDatabanks, 1);
     posLastStruct = posFirstNonStruct - 1;
     mergeWith = varargin(1:posLastStruct);
     varargin(1:posLastStruct) = [ ];
@@ -81,10 +81,11 @@ end
 persistent parser
 if isempty(parser)
     parser = extend.InputParser('databank.merge');
-    parser.addRequired('Method', @(x) validate.anyString(x, 'horzcat', 'replace', 'discard', 'error'));
+    parser.addRequired('Method', @(x) validate.anyString(char(x), 'horzcat', 'vertcat', 'replace', 'discard', 'error'));
     parser.addRequired('InputDatabank', @validate.databank);
     parser.addRequired('MergeWith');
-    parser.addParameter('MissingField', @rmfield);
+    parser.addParameter('MissingField', @remove);
+    parser.addParameter({'Names', 'List'}, @all, @(x) isequal(x, @all) || validate.list(x));
 end
 parse(parser, method, mainDatabank, mergeWith, varargin{:});
 opt = parser.Options;
@@ -92,9 +93,12 @@ opt = parser.Options;
 %--------------------------------------------------------------------------
 
 numMergeWith = numel(mergeWith);
+method = char(method);
 
 if strcmpi(method, 'horzcat')
-    mergeNext = @horzcatNext;
+    mergeNext = @(varargin) catNext(@horzcat, varargin{:});
+elseif strcmpi(method, 'vertcat')
+    mergeNext = @(varargin) catNext(@vertcat, varargin{:});
 elseif strcmpi(method, 'replace')
     mergeNext = @replaceNext;
 elseif strcmpi(method, 'discard')
@@ -115,15 +119,19 @@ end%
 %
 
 
-function mainDatabank = horzcatNext(mainDatabank, mergeWith, opt)
-    fieldsMainDatabank = fieldnames(mainDatabank);
-    fieldsMergeWith = fieldnames(mergeWith);
-    fieldsMerged = [fieldsMainDatabank; fieldsMergeWith];
-    fieldsMerged = unique(fieldsMerged, 'stable');
-    numFieldsMerged = numel(fieldsMerged);
-    for i = 1 : numFieldsMerged
-        ithName = fieldsMerged{i};
-        if isequal(opt.MissingField, @rmfield) 
+function mainDatabank = catNext(func, mainDatabank, mergeWith, opt)
+    if isequal(opt.Names, @all)
+        fieldsMainDatabank = fieldnames(mainDatabank);
+        fieldsMergeWith = fieldnames(mergeWith);
+        fieldsToMerge = [fieldsMainDatabank; fieldsMergeWith];
+    else
+        fieldsToMerge = reshape(cellstr(opt.Names), 1, [ ]);
+    end
+    fieldsToMerge = unique(fieldsToMerge, 'stable');
+    numFieldsToMerge = numel(fieldsToMerge);
+    for i = 1 : numFieldsToMerge
+        ithName = fieldsToMerge{i};
+        if isequal(opt.MissingField, @remove) || isequal(opt.MissingField, @rmfield)
             if ~isfield(mergeWith, ithName)
                 mainDatabank = rmfield(mainDatabank, ithName);
                 continue
@@ -132,66 +140,89 @@ function mainDatabank = horzcatNext(mainDatabank, mergeWith, opt)
             end
         end
         if isfield(mainDatabank, ithName)
-            mainDatabankField = mainDatabank.(ithName);
+            mainDatabankField = getfield(mainDatabank, ithName);
         else
             mainDatabankField = opt.MissingField;
         end
         if isfield(mergeWith, ithName)
-            mergeWithField = mergeWith.(ithName);
+            mergeWithField = getfield(mergeWith, ithName);
         else
             mergeWithField = opt.MissingField;
         end
-        mainDatabank.(ithName) = horzcat(mainDatabankField, mergeWithField);
+        mainDatabankField = func(mainDatabankField, mergeWithField);
+        mainDatabank = setfield(mainDatabank, ithName, mainDatabankField);
     end
 end%
 
 
 
 
-function mainDatabank = replaceNext(mainDatabank, mergeWith, ~)
-    newFields = fieldnames(mergeWith);
-    numNewFields = numel(newFields);
-    for i = 1 : numNewFields
-        name = newFields{i};
-        mainDatabank.(name) = mergeWith.(name);
+function mainDatabank = replaceNext(mainDatabank, mergeWith, opt)
+    if isequal(opt.Names, @all)
+        newFields = fieldnames(mergeWith);
+    else
+        newFields = reshape(cellstr(opt.Names), 1, [ ]);
     end
-end%
-
-
-
-
-function mainDatabank = discardNext(mainDatabank, mergeWith, ~)
-    newFields = fieldnames(mergeWith);
     numNewFields = numel(newFields);
     for i = 1 : numNewFields
-        name = newFields{i};
-        if isfield(mainDatabank, name)
+        ithName = newFields{i};
+        if ~isfield(mergeWith, ithName)
             continue
         end
-        mainDatabank.(name) = mergeWith.(name);
+        if isfield(mergeWith, ithName)
+            mainDatabank.(ithName) = mergeWith.(ithName);
+        end
     end
 end%
 
 
 
 
-function mainDatabank = errorNext(mainDatabank, mergeWith, ~)
-    newFields = fieldnames(mergeWith);
+function mainDatabank = discardNext(mainDatabank, mergeWith, opt)
+    if isequal(opt.Names, @all)
+        newFields = fieldnames(mergeWith);
+    else
+        newFields = reshape(cellstr(opt.Names), 1, [ ]);
+    end
     numNewFields = numel(newFields);
-    inxOfErrorFields = false(1, numNewFields);
     for i = 1 : numNewFields
-        name = newFields{i};
-        if isfield(mainDatabank, name)
-            inxOfErrorFields(i) = true;
+        ithName = newFields{i};
+        if ~isfield(mergeWith, ithName)
             continue
         end
-        mainDatabank.(name) = mergeWith.(name);
+        if ~isfield(mainDatabank, ithName)
+            mainDatabank.(ithName) = mergeWith.(ithName);
+        end
     end
-    if any(inxOfErrorFields)
+end%
+
+
+
+
+function mainDatabank = errorNext(mainDatabank, mergeWith, opt)
+    if isequal(opt.Names, @all)
+        newFields = fieldnames(mergeWith);
+    else
+        newFields = reshape(cellstr(opt.Names), 1, [ ]);
+    end
+    numNewFields = numel(newFields);
+    inxErrorFields = false(1, numNewFields);
+    for i = 1 : numNewFields
+        ithName = newFields{i};
+        if ~isfield(mergeWith, ithName)
+            continue
+        end
+        if isfield(mainDatabank, ithName)
+            inxErrorFields(i) = true;
+            continue
+        end
+        mainDatabank.(ithName) = mergeWith.(ithName);
+    end
+    if any(inxErrorFields)
         THIS_ERROR = { 'Databank:ErrorMergingFieldsWithSameName'
-                       'This field name occurs in more than one input databank: %s ' };
+                       'This field ithName occurs in more than one input databank: %s ' };
         throw( exception.Base(THIS_ERROR, 'error'), ...
-               newFields{inxOfErrorFields} );
+               newFields{inxErrorFields} );
     end
 end%
 
