1698f4c9967da55e64b283c172290c48c491949c
Jaromir Benes <jaromir.benes@gmail.com>
2019-10-01 07:41:03 +0200

    >>> Frontend: databank.compareFields

--- DataManagement/+databank/compareFields.m
+++ DataManagement/+databank/compareFields.m
@@ -3,6 +3,7 @@ function flag = compareFields(d1, d2)
 [keys1, keys2] = hereGetKeys( );
 
 if ~isequal(sort(keys1), sort(keys2))
+    warning('Number of fields or field names do not match');
     flag = false;
     return
 end
@@ -18,7 +19,19 @@ for key = keys1
     elseif isstruct(d2)
         field2 = getfield(d2, char(key));
     end
-    if ~isequal(field1, field2)
+    if isa(field1, 'NumericTimeSubscriptable') && isa(field2, 'NumericTimeSubscriptable')
+        if ~isequal(field1.Start, field2.Start)
+            warning('Start dates for these fields do not match: %s', key);
+            flag = false;
+            return
+        end
+        if ~isequal(field1.Data, field2.Data)
+            warning('Time series data for these fields do not match: %s', key);
+            flag = false;
+            return
+        end
+    elseif ~isequal(field1, field2)
+        warning('This field does not match: %s', key);
         flag = false;
         return
     end
