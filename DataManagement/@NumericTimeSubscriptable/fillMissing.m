299f8dda7dd72dbf1f0fd0b2c17bebd806062f16
Jaromir Benes <jaromir.benes@gmail.com>
2019-10-01 07:34:42 +0200

    >>> Frontend: Enhance @NumericTimeSeries.fillMissing to return missing dates filled

--- DataManagement/@NumericTimeSubscriptable/fillMissing.m
+++ DataManagement/@NumericTimeSubscriptable/fillMissing.m
@@ -1,5 +1,10 @@
-function this = fillMissing(this, range, varargin)
+function [this, datesMissing] = fillMissing(this, range, varargin)
 % fillMissing  Fill missing time series observations
+%{
+% ## Syntax ##
+%
+%     [x, datesMissing] = fillMissing(x, range, method)
+%}
 
 % -IRIS Macroeconomic Modeling Toolbox
 % -Copyright (c) 2007-2019 IRIS Solutions Team
@@ -26,6 +31,14 @@ endDate = range(end);
 
 missingTest = this.MissingTest;
 inxMissing = missingTest(data);
+if nargout>=2
+    if any(inxMissing)
+        datesMissing = (round(100*startDate) + round(100*(find(inxMissing)-1)))/100;
+        datesMissing = DateWrapper(datesMissing);
+    else
+        datesMissing = DateWrapper.empty(0, 1);
+    end
+end
 
 if ~any(inxMissing(:))
     return
