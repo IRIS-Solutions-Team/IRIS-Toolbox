df9bf8671c2d0c3ba3f5afe68a8d26fb7c3c722a
Jaromir Benes <jaromir.benes@gmail.com>
2019-12-07 09:10:39 +0100

    Clean up

--- +parser/+theparser/Generic.m
+++ +parser/+theparser/Generic.m
@@ -1,11 +1,11 @@
 classdef Generic < handle
     properties
+        Name = ''
         Keyword
         IsEssential = false
         Replace = cell(0, 2)
+        Parse = true
     end
-    
-    
     
     
     methods (Abstract)
@@ -13,34 +13,30 @@ classdef Generic < handle
     end
     
     
-    
-    
     methods
         function precheck(varargin)
-        end
+        end%
     end
-    
-    
     
     
     methods (Static)
         function [label, alias] = splitLabelAlias(label)
-        if isempty(label)
-            alias = label;
-            return
-        end
-        alias = cell(size(label));
-        alias(:) = {''};
-        for i = 1 : length(label)
-            pos = strfind(label{i},'!!');
-            if isempty(pos)
-                continue
+            if isempty(label)
+                alias = label;
+                return
             end
-            alias{i} = label{i}(pos+2:end);
-            label{i} = label{i}(1:pos-1);
-        end
-        alias = strtrim(alias);
-        label = strtrim(label);
-        end        
+            alias = cell(size(label));
+            alias(:) = {''};
+            for i = 1 : length(label)
+                pos = strfind(label{i},'!!');
+                if isempty(pos)
+                    continue
+                end
+                alias{i} = label{i}(pos+2:end);
+                label{i} = label{i}(1:pos-1);
+            end
+            alias = strtrim(alias);
+            label = strtrim(label);
+        end%      
     end
 end
