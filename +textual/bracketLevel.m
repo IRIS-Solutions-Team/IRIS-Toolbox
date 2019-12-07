c05f7b08d3a54336416e3673f29c7f04c900f84e
Jaromir Benes <jaromir.benes@gmail.com>
2019-12-07 09:15:43 +0100

    >>> Frontend: New functions textual.locate and textual.bracketLevel

---
+++ +textual/bracketLevel.m
@@ -0,0 +1,94 @@
+function [level, allClosed] = bracketLevel(inputString, bracketTypes)
+% bracketLevel  Return the nested bracket level for each character in a string
+%{
+% ## Syntax ##
+%
+%
+%     [level, allClosed] = textual.bracetLevel(inputString, bracketTypes)
+%
+%
+% ## Input Arguments ##
+%
+%
+% __`inputString`__ [ char | string ]
+% >
+% Input string; for each of the characters in the `inputString`, a number
+% greater than or equal to 0 will be returned indicating the level of
+% nested brackets at the position.
+%
+%
+% __`bracketTypes`__ [ cellstr | string ]
+% >
+% List of bracket types that will be counted; can be any combination of the
+% following four types of brackets: `()`, `[]`, `{}`, `<>`.
+%
+%
+% ## Output Arguments ##
+%
+%
+% __`level`__ [ numeric ]
+% >
+% A vector of numbers greater than or equal to 0 indicating the the level
+% of nested brackets at the respective position in the `inputString`; all
+% opening and closing brackets are counted as inside themselves.
+%
+%
+% __`allClosed`__ [ `true` | `false` ]
+% True if all brackets are closed by the end of the string.
+%
+%
+% ## Description ##
+%
+%
+% ## Example ##
+%
+%}
+
+% -[IrisToolbox] for Macroeconomic Modeling
+% -Copyright (c) 2007-2019 IRIS Solutions Team
+
+persistent pp
+if isempty(pp)
+    pp = extend.InputParser('+textual/bracketLevel');
+    addRequired(pp, 'InputString', @validate.string);
+    addRequired(pp, 'BracketTypes', @(x) validate.list(x) && all(cellfun(@(y) any(strcmp(y, {'()', '[]', '{}', '<>'})), cellstr(x))));
+end
+
+%--------------------------------------------------------------------------
+
+inputString = reshape(char(inputString), 1, [ ]);
+bracketTypes = cellstr(bracketTypes);
+
+inputString = [inputString, ' '];
+level = zeros(size(inputString));
+
+if any(strcmp(bracketTypes, '()'))
+    hereLevel('(', ')');
+end
+
+if any(strcmp(bracketTypes, '[]'))
+    hereLevel('[', ']');
+end
+
+if any(strcmp(bracketTypes, '{}'))
+    hereLevel('{', '}');
+end
+
+if any(strcmp(bracketTypes, '<>'))
+    hereLevel('<', '>');
+end
+
+allClosed = level(end)==0;
+level(end) = [ ];
+
+return
+
+    function hereLevel(open, close)
+        tempLevel = zeros(size(inputString));
+        tempLevel(inputString==open) = 1;
+        tempLevel([' ', inputString(1:end-1)]==close) = -1;
+        tempLevel = cumsum(tempLevel);
+        level = level + tempLevel;
+    end%
+end%
+
---
+++ +textual/locate.m
@@ -0,0 +1,109 @@
+function pos = locate(items, list, varargin)
+% locate  Find positions of selected items in a list
+%{
+% ## Syntax ##
+%
+%     pos = textual.locate(items, list)
+%
+%
+% ## Input Arguments ##
+%
+% __`items`__ [ string | cellstr | char ]
+% >
+% List of strings, each of which will be located in the `list`.
+%
+% __`list`__ [ string | cellstr | char ]
+% >
+% List of strings within which the `items` will be located.
+%
+%
+% ## Output Arguments ##
+%
+% __`pos`__ [ numeric ]
+% >
+% Position of each item from the `items` in the `list`.
+%
+%
+% ## Description ##
+%
+% The function returns a numeric array of the same size as the input
+% `items`. Each element of the `pos` is either the position (linear index)
+% of the corresponding item in the `list`, or `NaN` if the item is not
+% found. If there are multiple occurences of an item in the `list`, the
+% first will be reported.
+%
+%
+% ## Example ##
+%
+%     >> items = ["aa", "BB", "C_"];
+%     >> list = ["BB", "x", "y", "zz", "C_", "C_"];
+%     >> textual.locate(items, list)                
+%     ans =
+%        NaN     1     5
+%}
+
+% -[IrisToolbox] for Macroeconomic Modeling
+% -Copyright (c) 2007-2019 IRIS Solutions Team
+
+% Invoke unit tests
+%(
+if nargin==1 && isequal(items, '--test')
+    pos = functiontests(localfunctions( ));
+    return
+end
+%)
+
+
+%--------------------------------------------------------------------------
+
+items = string(items);
+list = string(list);
+pos = nan(size(items));
+for i = 1 : numel(items)
+    x_ = find(items(i)==list, 1, varargin{:});
+    if ~isempty(x_)
+        pos(i) = x_;
+    end
+end
+
+end%
+
+
+
+
+%
+% Unit Tests 
+%
+%(
+function stringTest(this)
+    items = ["aa", "BB", "C_"];
+    list = ["BB", "x", "y", "zz", "C_", "C_"];
+    pos = textual.locate(items, list);
+    assertEqual(this, pos, [NaN, 1, 5]);
+end%
+
+
+function stringFirstTest(this)
+    items = ["aa", "BB", "C_"];
+    list = ["BB", "x", "y", "zz", "C_", "C_"];
+    pos = textual.locate(items, list, 'first');
+    assertEqual(this, pos, [NaN, 1, 5]);
+end%
+
+
+function stringLastTest(this)
+    items = ["aa", "BB", "C_"];
+    list = ["BB", "x", "y", "zz", "C_", "C_"];
+    pos = textual.locate(items, list, 'last');
+    assertEqual(this, pos, [NaN, 1, 6]);
+end%
+
+
+function cellstrTest(this)
+    items = {'aa', 'BB', 'C_'};
+    list = {'BB', 'x', 'y', 'zz', 'C_', 'C_'};
+    pos = textual.locate(items, list);
+    assertEqual(this, pos, [NaN, 1, 5]);
+end%
+%)
+
