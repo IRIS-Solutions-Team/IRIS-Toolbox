7403d33694f0d50149d069d44acf2c0669a259bd
Jaromir Benes <jaromir.benes@gmail.com>
2019-12-13 11:35:21 +0000

    >>>>> Frontend: new lookup function Model/equationStartsWith

---
+++ Modeling/+model/+component/@Equation/equationStartsWith.m
@@ -0,0 +1,15 @@
+function varargout = equationStartsWith(this, varargin)
+% equationByLhs  Look up equation by its LHS
+%
+% Backend IRIS method
+% No help provided
+
+% -[IrisToolbox] for Macroeconomic Modeling
+% -Copyright (c) 2007-2019 IRIS Solutions Team
+
+%--------------------------------------------------------------------------
+
+eqtn = reshape(string(this.Input), [ ], 1);
+varargout = cellfun(@(x) eqtn(startsWith(eqtn, x)), varargin, 'UniformOutput', false);
+
+end%
