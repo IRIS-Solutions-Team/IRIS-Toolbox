019c5676c83238fdd2274394f3ce7a99a9662377
Jaromir Benes <jaromir.benes@gmail.com>
2019-12-13 17:44:56 +0000

    >>>>> Frontend: New method Plan/multiplySigma

---
+++ Modeling/@Plan/multiplySigma.m
@@ -0,0 +1,77 @@
+function this = multiplySigma(this, dates, names, varargin)
+% assign  Multiply sigmas (std deviations) of exogenous quantities in a simulation Plan
+%{
+% ## Syntax ##
+%
+%
+%     output = function(input, ...)
+%
+%
+% ## Input Arguments ##
+%
+%
+% __`input`__ [ | ]
+% >
+% Description
+%
+%
+% ## Output Arguments ##
+%
+%
+% __`output`__ [ | ]
+% >
+% Description
+%
+%
+% ## Options ##
+%
+%
+% __`OptionName=Default`__ [ | ]
+% >
+% Description
+%
+%
+% ## Description ##
+%
+%
+% ## Example ##
+%
+%}
+
+% -[IrisToolbox] for Macroeconomic Modeling
+% -Copyright (c) 2007-2019 IRIS Solutions Team
+
+persistent pp
+if isempty(pp)
+    pp = extend.InputParser('Plan.assign');
+    addRequired(pp, 'plan', @(x) isa(x, 'Plan'));
+    addRequired(pp, 'datesToAssign', @(x) isequal(x, @all) || DateWrapper.validateDateInput(x));
+    addRequired(pp, 'namesToAssign', @(x) isequal(x, @all) || ischar(x) || iscellstr(x) || isa(x, 'string'));
+    addRequired(pp, 'variantsToAssign', @(x) isequal(x, @all) || isnumeric(x));
+    addRequired(pp, 'multiplier', @isnumeric);
+end
+if numel(varargin)==1
+    variants = @all;
+    multiplier = varargin{1};
+else
+    [variants, multiplier] = varargin{:};
+end
+pp.parse(this, dates, names, variants, multiplier);
+opt = pp.Options;
+
+%--------------------------------------------------------------------------
+
+inxDates = resolveDates(this, dates);
+
+context = 'be assigned sigmas';
+inxNames = this.resolveNames(names, this.NamesOfExogenous, context);
+if ~any(inxNames)
+    return
+end
+
+inxVariants = resolveVariants(this, variants);
+
+this.SigmasOfExogenous(inxNames, inxDates, inxVariants) = this.SigmasOfExogenous(inxNames, inxDates, inxVariants) .* multiplier;
+
+end%
+
