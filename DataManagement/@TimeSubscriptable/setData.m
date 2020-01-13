8e1dd51affcab1e23f8c67428f5c8870cf6fc4c1
Jaromir Benes <jaromir.benes@gmail.com>
2020-01-13 09:21:47 +0100

    Fix bug in @TimeSubscriptable/setData, the comment array was not set to the correct dimensions u.s.c.

--- DataManagement/@TimeSubscriptable/setData.m
+++ DataManagement/@TimeSubscriptable/setData.m
@@ -35,31 +35,31 @@ end
 
 % Convert LHS NaNs to complex if LHS is real and RHS is complex
 if isnumeric(this.Data) && isreal(this.Data) && ~isreal(y)
-    inxOfMissing = this.MissingTest(this.Data);
+    inxMissing = this.MissingTest(this.Data);
     complexMissing = complex(this.MissingValue, this.MissingValue);
-    this.Data(inxOfMissing) = complexMissing;
+    this.Data(inxMissing) = complexMissing;
 end
 
 % If RHS has only one row but multiple cols (or size>1 in other dims), 
 % time series is multivariate, and assigned are multiple dates, then expand RHS
 % in 1st dimension
 xSize = size(this.Data);
-sizeOfRhs = size(y);
+sizeRhs = size(y);
 if length(y)>1 && size(y, 1)==1 ...
    && length(s.subs{1})>1 ...
    && any(xSize(2:end)>1)
     n = length(s.subs{1});
-    y = reshape(y(ones(1, n), :), [n, sizeOfRhs(2:end)]);
+    y = reshape(y(ones(1, n), :), [n, sizeRhs(2:end)]);
 end
 
 % Report frequency mismatch
 % Remove the rows from RHS that do not pass the frequency test; LHS dates
 % have already been removed and a warning thrown in hereExpand(~)
-sizeOfRhs = size(y);
+sizeRhs = size(y);
 if any(~freqTest)
-    if sizeOfRhs(1)==numel(freqTest)
+    if sizeRhs(1)==numel(freqTest)
         y = y(freqTest, :);
-        y = reshape(y, [size(y, 1), sizeOfRhs(2:end)]);
+        y = reshape(y, [size(y, 1), sizeRhs(2:end)]);
     end
 end
 
@@ -105,8 +105,8 @@ function [this, s, dates, freqTest] = hereExpand(this, s)
     testColon = @(x) (ischar(x) || isa(x, 'string')) && isequal(x, ':');
 
     convertToDateWrapper = isa(this.Start, 'DateWrapper');
-    startOfThis = double(this.Start);
-    freqOfThis = DateWrapper.getFrequencyAsNumeric(startOfThis);
+    startThis = double(this.Start);
+    freqThis = DateWrapper.getFrequencyAsNumeric(startThis);
 
     % If LHS data are complex, use NaN+NaNi to pad missing observations
     missingValue = this.MissingValue;
@@ -123,26 +123,26 @@ function [this, s, dates, freqTest] = hereExpand(this, s)
     timeRef = s.subs{1};
     if testColon(timeRef) || isequal(timeRef, Inf) || isequal(timeRef, [-Inf, Inf])
         s.subs{1} = ':';
-        if isnan(startOfThis)
+        if isnan(startThis)
             % LHS is empty
             dates = double.empty(1, 0);
         else
-            dates = startOfThis + (0 : size(this.Data, 1)-1);
+            dates = startThis + (0 : size(this.Data, 1)-1);
         end
         freqTest = true(size(dates));
     elseif (isa(timeRef, 'DateWrapper') || isnumeric(timeRef)) && ~isempty(timeRef)
         dates = double(s.subs{1});
         if ~isempty(dates)
-            freqOfDates = DateWrapper.getFrequencyAsNumeric(dates);
-            if isnan(startOfThis)
+            freqDates = DateWrapper.getFrequencyAsNumeric(dates);
+            if isnan(startThis)
                 % If LHS series is empty time series, set start date to the minimum
                 % date with the same frequency as the first date
-                startOfThis = min(dates(freqOfDates==freqOfDates(1)));
-                freqOfThis = freqOfDates(1);
+                startThis = min(dates(freqDates==freqDates(1)));
+                freqThis = freqDates(1);
             end
-            freqTest = freqOfThis==freqOfDates;
+            freqTest = freqThis==freqDates;
             dates(~freqTest) = [ ];
-            s.subs{1} = round(dates - startOfThis + 1);
+            s.subs{1} = round(dates - startThis + 1);
         end
     else
         dates = double.empty(1, 0);
@@ -168,13 +168,13 @@ function [this, s, dates, freqTest] = hereExpand(this, s)
     % Add empty strings for comments to comply with the new size.
     % This modifies standard Matlab matrix assignment, which produces zeros.
     for i = find(~strcmp(':', s.subs))
-        % Non-positive index in 1st dimension.
+        % Non-positive index in 1st dimension
         if i==1 && any(s.subs{1}<1)
             n = 1 - min(s.subs{1});
             currentSize = size(this.Data);
             currentSize(1) = n;
             this.Data = [repmat(missingValue, currentSize); this.Data];
-            startOfThis = startOfThis - n;
+            startThis = startThis - n;
             s.subs{1} = s.subs{1} + n;
         end
         % If index exceeds current size, add NaNs. This is different than
@@ -187,10 +187,9 @@ function [this, s, dates, freqTest] = hereExpand(this, s)
             this.Data = cat(i, this.Data, repmat(missingValue, addSize));
             if i>1
                 % Add an appropriate empty cellstr to comments if time
-                % series data are expanded in 2nd or higher dimensions.
-                comment = cell([1, addSize(2:end)]);
-                comment(:) = {''};
-                this.Comment = cat(i, this.Comment, comment);
+                % series data are expanded in 2nd or higher dimensions
+                addComment = repmat({TimeSubscriptable.EMPTY_COMMENT}, [1, addSize(2:end)]);
+                this.Comment = cat(i, this.Comment, addComment);
             end
         end
     end
@@ -201,27 +200,29 @@ function [this, s, dates, freqTest] = hereExpand(this, s)
             this.Data = reshape(this.Data, tempSize);
             this.Comment = reshape(this.Comment, [1, tempSize(2:end)]);
         catch %#ok<CTCH>
-            throw( exception.Base(ERROR_GROW_AMBIGUOUS_DIM, 'error') );
+            throw(exception.Base(ERROR_GROW_AMBIGUOUS_DIM, 'error'));
         end
     end
 
-    if convertToDateWrapper && ~isa(startOfThis, 'DateWrapper')
-        startOfThis = DateWrapper(startOfThis);
+    if convertToDateWrapper && ~isa(startThis, 'DateWrapper')
+        startThis = DateWrapper(startThis);
     end
-    this.Start = startOfThis;
+    this.Start = startThis;
 
     %
     % Report frequency mismatch as an error
     %
     if any(~freqTest)
-        charFreqOfThis = Frequency.toChar(freqOfThis);
-        freqOfDates = unique(freqOfDates(~freqTest), 'stable');
-        charFreqOfDates = arrayfun(@Frequency.toChar, freqOfDates, 'UniformOutput', false);
-        thisError = { 'TimeSubscriptable:FrequencyMismatch'
-                      'Cannot reference %s dates when assigning to %1 time series ' };
+        charFreqThis = Frequency.toChar(freqThis);
+        freqDates = unique(freqDates(~freqTest), 'stable');
+        charFreqDates = arrayfun(@Frequency.toChar, freqDates, 'UniformOutput', false);
+        thisError = [ "TimeSubscriptable:FrequencyMismatch"
+                      "Cannot reference %s dates when assigning to %1 time series " ];
         %throw( exception.Base('TimeSubscriptable:FrequencyMismatch', 'error'), ...
-        throw( exception.Base(thisError, 'error'), ...
-               Frequency.toChar(freqOfThis), charFreqOfDates{:} );
+        throw( ...
+            exception.Base(thisError, 'error'), ...
+            Frequency.toChar(freqThis), charFreqDates{:} ...
+        );
     end
 end%
 
