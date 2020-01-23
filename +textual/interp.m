a48856fdfe87d04bd05d11de95b9952811218e84
Jaromir Benes <jaromir.benes@gmail.com>
2020-01-23 05:00:30 -0300

    >>>>> Frontend: Fix bug #231. Bug in textual.interp with input arguments of the form {char, char} or {char, numeric}

--- +textual/interp.m
+++ +textual/interp.m
@@ -3,49 +3,58 @@ function c = interp(c, varargin)
 %{
 % ## Syntax ##
 %
+%
 %     c = textual.interp(c, s1, s2, ...)
 %
 %
 % ## Input Arguments ##
 %
-% __`c`__ [ char | cellstr | string ] -
+%
+% __`c`__ [ char | cellstr | string ]
+% >
 % Text string(s) containing interpolation fields (valid Matlab names
 % parenthesized and prefixed with `$`) that will be replaced with values
 % found in the earliest input struct `s1`, `s2`, etc.
 %
-% __`s1`__ [ struct | char | cellstr | string ] -
-% Structs or text strings that will be searched for interpolation fields occuring in the
-% input string(s) `c`; the earliest value found will be used to replace the
-% respective interpolation field in the input string(s); the value must be
-% a string itself, otherwise no interpolation will be performed.
+%
+% __`s1`__ [ struct | char | cellstr | string ]
+% >
+% Structs or text strings that will be searched for interpolation fields
+% occuring in the input string(s) `c`; the earliest value found will be
+% used to replace the respective interpolation field in the input
+% string(s); the value must be a string or a numeric scalar, otherwise no
+% interpolation will be performed.
 %
 %
 % ## Output Arguments ##
 %
-% __`c`__ [ char | cellstr | string ] -
+%
+% __`c`__ [ char | cellstr | string ]
+% >
 % Interpolated output text string(s).
 %
 %
 % ## Description ##
+%
 %
 % Each occurrence of an interpolation field formatted as `$(key)` where
 % `key` is a valid Matlab name (a string starting with a letter a
 % containing alphanumeric characters and underscores) will trigger the
 % following actions:
 %
-% * the input structs `s1`, `s2`, etc. will be searched (in the input
-% order) for the `key`;
+% * the inputs `s1`, `s2`, etc. will be searched (in the order of
+% appearance) for the `key`;
 %
 % * the first occurrence of a struct field named `key` and containing a
-% char or string value will be used to replace `$(key)` in the output
-% string.
+% char or a string or a numeric scalar will be used to replace `$(key)` in
+% the output string.
 %
 % * if none such field is found in any of the structs, `s1`, `s2`, etc.,
 % the interpolation field will remain unchanged in output string.
 %
 % ### Text Strings ##
 %
-% The text strings may be have one of the following four forms:
+% The text strings may be one of the following four forms:
 %
 % * a single character vector `'key: value'`;
 %
@@ -69,6 +78,7 @@ function c = interp(c, varargin)
 %
 % ## Example ##
 % 
+%
 % The following string interpolation will fail because the `Count` field in
 % the struct is not a char or string value:
 %
@@ -76,10 +86,11 @@ function c = interp(c, varargin)
 %     >> c = 'X=$(Count)';
 %     >> textual.interp(c, s1) 
 %     ans =
-%         'X=$(Count)'
+%         'X=10'
 %
 %
 % ## Example ##
+%
 % 
 % The following string interpolation will use the value of `key` from the second
 % struct because it is the first char or string value found:
@@ -93,8 +104,8 @@ function c = interp(c, varargin)
 %
 %}
 
-% -IRIS Macroeconomic Modeling Toolbox
-% -Copyright (c) 2007-2020 IRIS Solutions Team
+% -[IrisToolbox] Macroeconomic Modeling Toolbox
+% -Copyright (c) 2007-2020 [IrisToolbox] Solutions Team
 
 if ~ischar(c) && ~iscell(c) && ~isa(c, 'string')
     return
@@ -103,45 +114,65 @@ end
 inputs = varargin;
 
 %
+% Struct
+%
+inxStruct = cellfun(@isstruct, varargin);
+% Do nothing
+
+%
 % Single char 'key:value' or string "key:value"
 %
 inxSingleString = cellfun(@(x) ischar(x) || (isa(x, 'string') && isscalar(x)), varargin);
 for i = find(inxSingleString)
-    tokens = regexp(inputs{i}, '(\<[A-Za-z]\w*\>):(.*)', 'tokens', 'once');
+    tokens = cellstr(split(inputs{i}, ':'));
     if numel(tokens)==2 && ~isempty(tokens{1}) && ~isempty(tokens{2})
         tokens = strtrim(tokens);
-        tokens = cellstr(tokens); % Legacy Matlab
-        inputs{i} = struct(tokens{1:2});
+        inputs{i} = struct( );
+        inputs{i}.(tokens{1}) = tokens{2};
     end
 end
 
 %
-% Two-element cellstr {'key:', 'value'} or string ["key:", "value"]
+% Two-element cell {'key:', 'value'} or {'key:', value}
 % Colon is optional
 %
-inxDoubleString = cellfun(@(x) (iscellstr(x) || isa(x, 'string')) && numel(x)==2, varargin);
-for i = find(inxDoubleString)
-    temp = cellstr(inputs{i});
-    temp{1} = strrep(temp{1}, ":", "");
-    temp = strtrim(temp);
-    inputs{i} = setfield(struct( ), temp{1:2});
+inxDoubleCell = cellfun(@(x) iscell(x) && numel(x)==2 && ischar(x{1}), varargin);
+for i = find(inxDoubleCell)
+    temp = struct( );
+    temp.(strtrim(replace(inputs{i}{1}, ':', ''))) = inputs{i}{2};
+    inputs{i} = temp;
 end
 
 %
-% Two-element cell with list {'key:', list}
-% where list is a cellstr
+% Two-element string ["key:", "value"]
 %
-inxList = cellfun(@(x) iscell(x) && numel(x)==2 && ischar(x{1}) && iscellstr(x{2}), varargin);
+inxDoubleString = cellfun(@(x) isa(x, 'string') && numel(x)==2, varargin);
+for i = find(inxDoubleString)
+    inputs{i} = cellstr(inputs{i});
+    temp = struct( );
+    temp.(strtrim(replace(inputs{i}{1}, ':', ''))) = inputs{i}{2};
+    inputs{i} = temp;
+end
+
+%
+% Two-element cell with a key list and a value list {keys, values}
+%
+inxList = cellfun(@(x) iscell(x) && numel(x)==2 && iscellstr(x{1}) && iscell(x{2}) && numel(x{1})==numel(x{2}), varargin);
 for i = find(inxList)
-    inputs{i} = setfield(struct( ), inputs{i}{1:2});
+    inputs{i} = cell2struct(reshape(inputs{i}{2}, [ ], 1), reshape(inputs{i}{1}, [ ], 1));
 end 
 
-replace = @hereInterpolate;
+inxValid = inxStruct | inxSingleString | inxDoubleCell | inxDoubleString | inxList;
+if any(~inxValid)
+    hereReportInvalidInput( );
+end
+
+replaceFunc = @hereInterpolate;
 if iscell(c)
     inx = cellfun('isclass', c, 'char');
-    c(inx) = regexprep(c(inx), '\$\([A-Za-z]\w*\)', '${replace($0)}');
+    c(inx) = regexprep(c(inx), '\$\([A-Za-z]\w*\)', '${replaceFunc($0)}');
 else
-    c = regexprep(c, '\$\([A-Za-z]\w*\)', '${replace($0)}');
+    c = regexprep(c, '\$\([A-Za-z]\w*\)', '${replaceFunc($0)}');
 end
 
 return
@@ -150,9 +181,9 @@ return
         key = c1(3:end-1);
         for i = 1 : numel(inputs)
             if isstruct(inputs{i}) && isfield(inputs{i}, key)
-                value = getfield(inputs{i}, key);
+                value = inputs{i}.(char(key));
                 if validate.string(value)
-                    c1 = char(value);
+                    c1 = strtrim(char(value));
                     return
                 elseif validate.numericScalar(value)
                     c1 = sprintf('%g', value);
@@ -160,6 +191,16 @@ return
                 end
             end
         end
+    end%
+
+
+    function hereReportInvalidInput( )
+        thisError = [
+            "Interpolate:InvalidaInput"
+            "Each input into textual.interpolate() defining the key-value pairs "
+            "must be struct, char, string, 1x2 string, 1x2 cellstr, or {cellstr, cell}."
+        ];
+        throw(exception.Base(thisError, 'error'));
     end%
 end%
 
