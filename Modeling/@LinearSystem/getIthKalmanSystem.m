659a62718d1e3a128d1cf86fd454d20921208bd0
Jaromir Benes <jaromir.benes@gmail.com>
2020-03-03 14:08:13 +0100

    Create index of necessary initial condtions based on the transition matrix in LinearSystem

--- Modeling/@LinearSystem/getIthKalmanSystem.m
+++ Modeling/@LinearSystem/getIthKalmanSystem.m
@@ -1,7 +1,7 @@
 function [T, R, k, Z, H, d, U, Zb, inxV, inxW, numUnitRoots, inxInit] = getIthKalmanSystem(this, ~, requiredForward)
 
 numXi = this.NumXi;
-numXib = this.NumXib;
+numXiB = this.NumXiB;
 numV = this.NumV;
 numY = this.NumY;
 numW = this.NumW;
@@ -28,9 +28,11 @@ H = [zeros(numY, numV, size(H, 3)), H];
 d = this.clipAndFillMissing(d);
 d = d(:, :);
 
-if all(isnan(U(:)))
+if isempty(U) || all(isnan(U(:)))
+    needsTransform = false;
     U = [ ];
 else
+    needsTransform = true;
     U = this.clipAndFillMissing(U);
 end
 
@@ -43,7 +45,22 @@ end
 inxV = [true(1, numV), false(1, numW)];
 inxW = [false(1, numV), true(1, numW)];
 numUnitRoots = this.NumUnitRoots;
-inxInit = true(numXi, 1);
+
+%
+% Necessary initial conditions
+%
+% Take the transition matrix 0->1 and find the elements of XiB that have
+% at least one non-zero entry in that transition matrix
+%
+T01 = T(:, :, min(2, end));
+if any(isnan(T01(:)))
+    inxInit = true(numXib, 1);
+else
+    if needsTransform
+        T01 = T01/U;
+    end
+    inxInit = reshape( any(abs(T01)>this.Tolerance.Solve, 1), [ ], 1 );
+end
 
 end%
 
