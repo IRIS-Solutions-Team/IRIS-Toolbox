c61308c05fc6ae0e6b1ccb811d6d25a5d0aca723
Jaromir Benes <jaromir.benes@gmail.com>
2020-04-14 22:55:09 +0200

    Clean up

--- Modeling/@ExplanatoryEquation/simulate.m
+++ Modeling/@ExplanatoryEquation/simulate.m
@@ -1,5 +1,5 @@
-function varargout = simulate(this, inputDatabank, range, varargin)
-% simulate  Simulate ExplanatoryEquation model
+function [outputDb, info] = simulate(this, inputDb, range, varargin)
+% simulate  Simulate Explanatory model
 %{
 % ## Syntax ##
 %
@@ -47,21 +47,12 @@ function varargout = simulate(this, inputDatabank, range, varargin)
 % -[IrisToolbox] for Macroeconomic Modeling
 % -Copyright (c) 2007-2020 IRIS Solutions Team
 
-% Invoke unit tests
-%(
-if nargin==2 && isequal(inputDatabank, '--test')
-    varargout{1} = unitTests( );
-    return
-end
-%)
-
-
 persistent pp
 if isempty(pp)
-    pp = extend.InputParser('ExplanatoryEquation.simulate');
+    pp = extend.InputParser('Explanatory/simulate');
 
-    addRequired(pp, 'explanatoryEquation', @(x) isa(x, 'ExplanatoryEquation'));
-    addRequired(pp, 'inputDatabank', @validate.databank);
+    addRequired(pp, 'explanatory', @(x) isa(x, 'ExplanatoryEquation'));
+    addRequired(pp, 'inputDb', @validate.databank);
     addRequired(pp, 'simulationRange', @DateWrapper.validateProperRangeInput);
 
     addParameter(pp, 'AddToDatabank', @auto, @(x) isequal(x, @auto) || isequal(x, [ ]) || validate.databank(x));
@@ -74,7 +65,7 @@ if isempty(pp)
     addParameter(pp, 'RaggedEdge', @auto, @(x) isequal(x, @auto) || validate.logicalScalar(x));
     addParameter(pp, 'Blazer', cell.empty(1, 0), @iscell);
 end
-parse(pp, this, inputDatabank, range, varargin{:});
+parse(pp, this, inputDb, range, varargin{:});
 opt = pp.Options;
 
 storeToDatabank = nargout>=1;
@@ -82,11 +73,10 @@ storeToDatabank = nargout>=1;
 %--------------------------------------------------------------------------
 
 if isempty(this)
-    outputDatabank = inputDatabank;
+    outputDb = inputDb;
     info = struct( );
     info.Blocks = cell.empty(1, 0);
     info.DynamicStatus = false;
-    varargout = {outputDatabank, info};
     return
 end
 
@@ -102,7 +92,7 @@ nv = countVariants(this);
 %
 lhsRequired = false;
 context = "for " + this(1).Context + " simulation";
-dataBlock = getDataBlock(this, inputDatabank, range, lhsRequired, context);
+dataBlock = getDataBlock(this, inputDb, range, lhsRequired, context);
 numExtendedPeriods = dataBlock.NumOfExtendedPeriods;
 numPages = dataBlock.NumOfPages;
 numRuns = max(nv, numPages);
@@ -114,7 +104,7 @@ extendedRange = DateWrapper(dataBlock.ExtendedRange);
 %
 % Create struct with controls
 %
-controls = assignControls(this, inputDatabank);
+controls = assignControls(this, inputDb);
 
 
 %
@@ -189,18 +179,12 @@ end
 %
 if storeToDatabank
     namesToInclude = [this.LhsName, this.ResidualName];
-    outputDatabank = createOutputDatabank(this, inputDatabank, dataBlock, namesToInclude, [ ], opt);
+    outputDb = createOutputDatabank(this, inputDb, dataBlock, namesToInclude, [ ], opt);
 end
 
 info = struct( );
 info.Blocks = humanBlocks;
 info.DynamicStatus = dynamicStatus;
-
-
-%<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
-varargout = {outputDatabank, info};
-%<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
-
 
 return
 
@@ -348,8 +332,8 @@ return
             return
         elseif numPages~=nv
             thisError = [ 
-                "ExplanatoryEquation:InconsistentPagesAndVariangs"
-                "The number of data pages and the number of ExplanatoryEquation "
+                "Explanatory:InconsistentPagesAndVariangs"
+                "The number of data pages and the number of Explanatory "
                 "parameter variants need to be identical unless one of them is 1." 
             ];
             throw(exception.Base(thisError, 'error'));
@@ -362,8 +346,8 @@ return
     function hereReportNaNParameters( )
         report = cellstr(lhsNames(inxNaNParameters));
         thisWarning  = [ 
-            "ExplanatoryEquation:MissingObservationInSimulationRange"
-            "Some Parameters are NaN or Inf in the ExplanatoryEquation object"
+            "Explanatory:MissingObservationInSimulationRange"
+            "Some Parameters are NaN or Inf in the Explanatory object"
             "for this LHS variables: %s" 
         ];
         throw(exception.Base(thisWarning, opt.NaNParameters), report{:});
@@ -375,296 +359,11 @@ return
     function hereReportNaNSimulation( )
         report = cellstr(dataBlock.Names(pos(inxNaNLhs)));
         thisWarning  = [ 
-            "ExplanatoryEquation:MissingObservationInSimulationRange"
-            "Simulation of an ExplanatoryEquation object produced "
+            "Explanatory:MissingObservationInSimulationRange"
+            "Simulation of an Explanatory object produced "
             "NaN or Inf values in this LHS variable: %s" 
         ];
         throw(exception.Base(thisWarning, opt.NaNSimulation), report{:});
     end%
 end%
 
-
-
-
-%
-% Unit Tests 
-%
-%(
-function tests = unitTests( )
-    tests = functiontests({ 
-        @setupOnce
-        @arxTest
-        @transformTest
-        @arxTestVariantsTest
-        @arxWithResidualsTest
-        @arxParametersTest    
-        @arxSystemTest      
-        @arxSystemVariantsTest      
-        @arxSystemWithPrependTest  
-        @blazerTest
-        @allExogenizeWhenDataTest
-        @someExogenizeWhenDataTest
-        @runtimeIfTest
-    });
-    tests = reshape(tests, [ ], 1);
-end%
-
-
-function setupOnce(testCase)
-    m1 = ExplanatoryEquation.fromString("x = 0.8*x{-1} + (1-0.8)*c");
-    m2 = ExplanatoryEquation.fromString("a = b + ?*(a{-1} - b) + ?");
-    m3 = ExplanatoryEquation.fromString("z = x + a{-1}");
-    m4 = ExplanatoryEquation.fromString("difflog(w) = 0.3*log(5/w{-1})");
-    m5 = ExplanatoryEquation.fromString("c = y");
-    startDate = numeric.qq(2001,1);
-    endDate = numeric.qq(2010, 4);
-    range = DateWrapper.roundColon(startDate, endDate);
-    db = struct( );
-    db.x = Series(DateWrapper.roundPlus(startDate, -1), 0);
-    db.y = Series(range, @rand)*0.5 + 5;
-    db.c = db.y;
-    db.a = Series(DateWrapper.roundPlus(startDate, -1), 0);
-    db.b = Series(range, @rand)*0.5 + 5;
-    db.w = Series(DateWrapper.roundPlus(startDate, -1), 0.5+rand(1, 3)*10);
-    testCase.TestData.Model1 = m1;
-    testCase.TestData.Model2 = m2;
-    testCase.TestData.Model3 = m3;
-    testCase.TestData.Model4 = m4;
-    testCase.TestData.Model5 = m5;
-    testCase.TestData.Range = range;
-    testCase.TestData.Databank = db;
-end%
-
-
-function arxTest(testCase)
-    m1 = testCase.TestData.Model1;
-    db = testCase.TestData.Databank;
-    range = testCase.TestData.Range;
-    s = simulate(m1, db, range);
-    exp = db.x;
-    for t = range
-        exp(t) = 0.8*exp(t-1) + (1-0.8)*db.y(t);
-    end
-    assertEqual(testCase, s.x.Data, exp.Data, 'AbsTol', 1e-12);
-    assertEqual(testCase, sort(fieldnames(db)), sort(setdiff(fieldnames(s), 'res_x')));
-    assertEqual(testCase, s.w.Data, db.w.Data);
-end%
-
-
-function transformTest(testCase)
-    m4 = testCase.TestData.Model4;
-    db = testCase.TestData.Databank;
-    range = testCase.TestData.Range;
-    s = simulate(m4, db, range);
-    for i = 1 : 3
-        exp_w = db.w{:, i};
-        for t = range
-            temp = 0.3*log(5/exp_w(t-1));
-            exp_w(t) = exp_w(t-1)*exp(temp);
-        end
-        assertEqual(testCase, s.w.Data(:, i), exp_w.Data, 'AbsTol', 1e-12);
-    end
-end%
-
-
-function arxTestVariantsTest(testCase)
-    m2 = testCase.TestData.Model2;
-    m2 = alter(m2, 3);
-    rho = rand(1, 1, 3);
-    m2.FreeParameters = [rho, zeros(1, 1, 3)];
-    db = testCase.TestData.Databank;
-    db.a = [db.a, db.a+1, db.a+2];
-    range = testCase.TestData.Range;
-    s = simulate(m2, db, range);
-    for i = 1 : 3
-        exp = db.a{:, i};
-        for t = range
-            exp(t) = rho(i)*exp(t-1) + (1-rho(i))*db.b(t);
-        end
-        assertEqual(testCase, s.a.Data(:, i), exp.Data, 'AbsTol', 1e-12);
-    end
-end%
-
-
-function arxWithResidualsTest(testCase)
-    m1 = testCase.TestData.Model1;
-    db = testCase.TestData.Databank;
-    range = testCase.TestData.Range;
-    db.res_x = Series(range(3:end-3), @randn)/10;
-    s = simulate(m1, db, range);
-    exp = db.x;
-    for t = range
-        exp(t) = 0.8*exp(t-1) + (1-0.8)*db.y(t);
-        res_x = db.res_x(t);
-        if isfinite(res_x)
-            exp(t) = exp(t) + res_x;
-        end
-    end
-    assertEqual(testCase, s.x.Data, exp.Data, 'AbsTol', 1e-12);
-end%
-
-
-function arxParametersTest(testCase)
-    m2 = testCase.TestData.Model2;
-    rho = rand(1);
-    m2.Parameters([1, 2]) = [rho, 0];
-    db = testCase.TestData.Databank;
-    range = testCase.TestData.Range;
-    s = simulate(m2, db, range);
-    exp_a = db.a;
-    for t = range
-        exp_a(t) = rho*exp_a(t-1) + (1-rho)*db.b(t);
-    end
-    assertEqual(testCase, s.a.Data, exp_a.Data, 'AbsTol', 1e-12);
-end%
-
-
-function arxSystemTest(testCase)
-    m = [ testCase.TestData.Model1
-          testCase.TestData.Model2
-          testCase.TestData.Model3 ];
-    rho = rand(1);
-    m(2).Parameters([1, 2]) = [rho, 0];
-    db = testCase.TestData.Databank;
-    range = testCase.TestData.Range;
-    s = simulate(m, db, range);
-    exp_z = s.x{range} + s.a{-1}{range};
-    assertEqual(testCase, s.z.Data, exp_z.Data, 'AbsTol', 1e-12);
-end%
-
-
-function arxSystemVariantsTest(testCase)
-    m = [ testCase.TestData.Model1
-          testCase.TestData.Model2
-          testCase.TestData.Model3 ];
-    m = alter(m, 3);
-    rho = rand(1, 1, 3);
-    m(2).FreeParameters = [rho, zeros(1, 1, 3)];
-    db = testCase.TestData.Databank;
-    range = testCase.TestData.Range;
-    s = simulate(m, db, range);
-    exp_z = s.x{range} + s.a{-1}{range};
-    assertEqual(testCase, s.z.Data, exp_z.Data, 'AbsTol', 1e-12);
-end%
-
-
-function arxSystemWithPrependTest(testCase)
-    m = [ testCase.TestData.Model1
-          testCase.TestData.Model2
-          testCase.TestData.Model3 ];
-    rho = rand(1);
-    m(2).FreeParameters = [rho, 0];
-    range = testCase.TestData.Range;
-    db = testCase.TestData.Databank;
-    db.x(range(1)+(-10:-2)) = rand(9, 1);
-    s = simulate(m, db, range, 'PrependInput=', true);
-    exp_z = s.x{range} + s.a{-1}{range};
-    assertEqual(testCase, s.z.Data, exp_z.Data, 'AbsTol', 1e-12);
-    assertEqual(testCase, double(s.x.Start), range(1)-10);
-    assertEqual(testCase, s.x(range(1)+(-10:-2)), db.x(range(1)+(-10:-2)));
-end%
-
-
-function blazerTest(testCase)
-    xq = ExplanatoryEquation.fromString([
-        "x = 0.8*y{-1} + 0.8*z{-1}"
-        "y = 0.8*x{-1}"
-        "a = 0.8*b{-1} + 0.8*a{-2}"
-        "b = 0.8*a{-1} + 0.8*b{-2}"
-        "z = a"
-    ]);
-    db = struct( );
-    db.a = Series(-1:1000, @rand);
-    db.b = Series(-1:1000, @rand);
-    db.x = Series(0, rand);
-    db.y = Series(0, rand);
-    db.z = Series(0, rand);
-    simRange = 1:1000;
-    [simDb1, info1] = simulate(xq, db, simRange);
-    [simDb2, info2] = simulate(xq, db, simRange, 'Blazer=', {'Reorder=', false, 'Dynamic=', true});
-    [simDb3, info3] = simulate(xq([1,3,4,5,2]), db, simRange);
-    for i = reshape(string(fieldnames(simDb1)), 1, [ ]);
-        assertEqual(testCase, simDb1.(i).Data, simDb2.(i).Data, 'AbsTol', 1e-12);
-        assertEqual(testCase, simDb1.(i).Data, simDb3.(i).Data, 'AbsTol', 1e-12);
-    end
-end%
-
-
-function allExogenizeWhenDataTest(testCase)
-    xq = ExplanatoryEquation.fromString([
-        "a = b{-1}"
-        "b = c{-1}"
-        "c = d{-1}"
-    ]);
-    db = struct( );
-    db.d = Series(0:10, 0:10);
-    db.c = Series(0:8, 0:10:80);
-    db.b = Series(0:6, 0:100:600);
-    simDb1 = simulate(xq, db, 1:10);
-
-    p2 = Plan.forExplanatoryEquation(xq, 1:10);
-    p2 = exogenizeWhenData(p2, 1:10, @all);
-    simDb2 = simulate(xq, db, 1:10, 'Plan=', p2);
-
-    assertEqual(testCase, simDb1.c(1:10), db.d{-1}(1:10), 'AbsTol', 1e-14);
-    assertEqual(testCase, simDb2.c(1:8), db.c(1:8), 'AbsTol', 1e-14);
-    assertEqual(testCase, simDb2.c(9:10), db.d{-1}(9:10), 'AbsTol', 1e-14);
-
-    assertEqual(testCase, simDb1.b(1:10), simDb1.c{-1}(1:10), 'AbsTol', 1e-14);
-    assertEqual(testCase, simDb2.b(1:6), db.b(1:6), 'AbsTol', 1e-14);
-    assertEqual(testCase, simDb2.b(7:10), simDb2.c{-1}(7:10), 'AbsTol', 1e-14);
-
-    assertEqual(testCase, simDb1.a(1:10), simDb1.b{-1}(1:10), 'AbsTol', 1e-14);
-    assertEqual(testCase, simDb2.a(1:10), simDb2.b{-1}(1:10), 'AbsTol', 1e-14);
-end%
-
-
-function someExogenizeWhenDataTest(testCase)
-    xq = ExplanatoryEquation.fromString([
-        "a = b{-1};"
-        ":exogenous b = c{-1};"
-        "c = d{-1};"
-    ]);
-    db = struct( );
-    db.d = Series(0:10, 0:10);
-    db.c = Series(0:8, 0:10:80);
-    db.b = Series(0:6, 0:100:600);
-    simDb1 = simulate(xq, db, 1:10);
-
-    [~, ~, listExogenous] = lookup(xq, ':exogenous');
-    p2 = Plan.forExplanatoryEquation(xq, 1:10);
-    p2 = exogenizeWhenData(p2, 1:10, listExogenous);
-    simDb2 = simulate(xq, db, 1:10, 'Plan=', p2);
-
-    assertEqual(testCase, simDb1.c(1:10), db.d{-1}(1:10), 'AbsTol', 1e-14);
-    assertEqual(testCase, simDb2.c(1:10), db.d{-1}(1:10), 'AbsTol', 1e-14);
-
-    assertEqual(testCase, simDb1.b(1:10), simDb1.c{-1}(1:10), 'AbsTol', 1e-14);
-    assertEqual(testCase, simDb2.b(1:6), db.b(1:6), 'AbsTol', 1e-14);
-    assertEqual(testCase, simDb2.b(7:10), simDb2.c{-1}(7:10), 'AbsTol', 1e-14);
-
-    assertEqual(testCase, simDb1.a(1:10), simDb1.b{-1}(1:10), 'AbsTol', 1e-14);
-    assertEqual(testCase, simDb2.a(1:10), simDb2.b{-1}(1:10), 'AbsTol', 1e-14);
-
-    simDb3 = simulate(xq, simDb2, 1:10);
-    assertEqual(testCase, simDb2.b(1:10), simDb3.b(1:10), 'AbsTol', 1e-14);
-end%
-
-
-function runtimeIfTest(testCase)
-    xq = ExplanatoryEquation.fromString([
-        "a = a{-1} + if(date__==qq(2001,4), 5, 0);"
-        "b = b{-1} + if(b{-1}<0, 1, 0);"
-    ]);
-    db = struct( );
-    db.a = Series(qq(2000,4), 10);
-    db.b = Series(qq(2000,4), -3);
-    simDb = simulate(xq, db, qq(2001,1):qq(2004,4));
-    exp_a = Series(qq(2000,4):qq(2004,4), 10);
-    exp_a(qq(2001,4):end) = 15;
-    assertEqual(testCase, simDb.a(:), exp_a(:), 'AbsTol', 1e-14);
-    exp_b = Series(qq(2000,4):qq(2004,4), 0);
-    exp_b(qq(2000,4):qq(2001,2)) = [-3;-2;-1];
-    assertEqual(testCase, simDb.b(:), exp_b(:), 'AbsTol', 1e-14);
-end%
-%)
--- Modeling/@Plan/forExplanatory.m
+++ Modeling/@Plan/forExplanatory.m
@@ -1,24 +1,24 @@
-function this = forExplanatoryEquation(xq, simulationRange)
-% forExplanatoryEquation  Construct a simulation Plan object for ExplanatoryEquation object or array
+function this = forExplanatory(explanatory, simulationRange)
+% forExplanatory  Construct a simulation Plan object for Explanatory object or array
 %{
 % ## Syntax ##
 %
 %
-%     p = Plan.forExplanatoryEquation(xq, simulationRange, ...)
+%     p = Plan.forExplanatory(explanatory, simulationRange, ...)
 %
 %
 % ## Input Arguments ##
 %
 %
-% __`xq`__ [ ExplanatoryEquation ]
+% __`explanatory`__ [ Explanatory ]
 % >
-% ExplanatoryEquation object or array for which the new simulation Plan `p`
+% Explanatory object or array for which the new simulation Plan `p`
 % will be created on the `simulationRange`.
 %
 %
 % __`simulationRange`__ [ DateWrapper ]
 % >
-% Range on which the `xq` will be simulated using the plan `p`.
+% Range on which the `explanatory` will be simulated using the plan `p`.
 %
 %
 % ## Output Arguments ##
@@ -26,15 +26,15 @@ function this = forExplanatoryEquation(xq, simulationRange)
 %
 % __`p`__ [ Plan ]
 % >
-% A new simulation Plan object that can be use when simulating the `xq`
+% A new simulation Plan object that can be use when simulating the `explanatory`
 % object or array on the `simulationRange`.
 %
 %
 % ## Description ##
 %
 %
-% The `Plan` object created by `Plan.forExplanatoryEquation( )` is to be
-% used in an `ExplanatoryEquation/simulate( )` function to specify the
+% The `Plan` object created by `Plan.forExplanatory( )` is to be
+% used in an `Explanatory/simulate( )` function to specify the
 % exogenized variables. Only the LHS variables in non-identities can be
 % exogenized.  When an LHS variables is exogenized, the respective residual
 % belonging to the equation is endogenized in the same periods; this is
@@ -42,7 +42,7 @@ function this = forExplanatoryEquation(xq, simulationRange)
 % called by the user.
 % 
 % There are two ways how to exogenize a variable in a `Plan` created for an
-% `Explanatory: 
+% `Explanatory`:
 %
 % * `exogenize( )` exogenizes some LHS variables in some periods no matter
 % what;
@@ -58,15 +58,15 @@ function this = forExplanatoryEquation(xq, simulationRange)
 %}
 
 % -[IrisToolbox] for Macroeconomic Modeling
-% -Copyright (c) 2007-2020 IRIS Solutions Team
+% -Copyright (c) 2007-2020 [IrisToolbox] Solutions Team
 
-persistent parser
-if isempty(parser)
-    parser = extend.InputParser('Plan.Plan');
-    parser.addRequired('xq', @(x) isa(x, 'ExplanatoryEquation'));
-    parser.addRequired('simulationRange', @DateWrapper.validateProperRangeInput);
+persistent pp
+if isempty(pp)
+    pp = extend.InputParser('Plan.Plan');
+    addRequired(pp, 'explanatory', @(x) isa(x, 'ExplanatoryEquation'));
+    addRequired(pp, 'simulationRange', @DateWrapper.validateProperRangeInput);
 end
-parser.parse(xq, simulationRange);
+parse(pp, explanatory, simulationRange);
 simulationRange = double(simulationRange);
 
 %--------------------------------------------------------------------------
@@ -74,7 +74,7 @@ simulationRange = double(simulationRange);
 this = Plan( );
 this.BaseStart = simulationRange(1);
 this.BaseEnd = simulationRange(end);
-this = preparePlan(xq, this);
+this = preparePlan(explanatory, this);
 
 numEndogenous = this.NumOfEndogenous;
 numExogenous = this.NumOfExogenous;
