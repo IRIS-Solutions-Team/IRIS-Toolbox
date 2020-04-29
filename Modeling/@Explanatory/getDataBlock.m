e95de2b3817c81993ea5df02d1626ae3e34b31be
Jaromir Benes <jaromir.benes@gmail.com>
2020-04-29 15:41:29 +0200

    Unit tests in Explanatory/getDataBlock

--- Modeling/@Explanatory/getDataBlock.m
+++ Modeling/@Explanatory/getDataBlock.m
@@ -1,30 +1,21 @@
-function varargout = getDataBlock(this, inputDatabank, range, lhsRequired, context)
+function [data, maxLag, maxLead] = getDataBlock(this, inputData, range, lhsRequired, context)
 % getDataBlock  Get DataBlock of all time series for LHS and RHS names
 %{
 %}
 
 % -[IrisToolbox] for Macroeconomic Modeling
-% -Copyright (c) 2007-2020 IRIS Solutions Team
-
-% Invoke unit tests
-%(
-if nargin==2 && isequal(inputDatabank, '--test')
-    varargout{1} = unitTests( );
-    return
-end
-%)
+% -Copyright (c) 2007-2020 [IrisToolbox] Solutions Team
 
 %--------------------------------------------------------------------------
 
+[maxLag, maxLead] = getActualMinMaxShifts(this);
 range = double(range);
 startDate = range(1);
 endDate = range(end);
-numEquations = numel(this);
-maxLag = min([0, this(:).MaxLag]);
-maxLead = max([0, this(:).MaxLead]);
-extendedStartDate = DateWrapper.roundPlus(startDate, maxLag);
-extendedEndDate = DateWrapper.roundPlus(endDate, maxLead);
-extendedRange = DateWrapper.roundColon(extendedStartDate, extendedEndDate);
+extStartDate = DateWrapper.roundPlus(startDate, maxLag);
+extEndDate = DateWrapper.roundPlus(endDate, maxLead);
+extRange = DateWrapper.roundColon(extStartDate, extEndDate);
+numExtPeriods = numel(extRange);
 
 [variableNames, residualNames] = collectAllNames(this);
 allNames = [variableNames, residualNames];
@@ -47,46 +38,62 @@ else
     optionalNames = [lhsNames(inxLhsOptional), residualNames];
 end
 
-scalarAllowed = @all;
-databankInfo = checkInputDatabank( ...
-    this, inputDatabank, extendedRange, ...
-    requiredNames, optionalNames, context, ...
-    scalarAllowed ...
-);
-
 data = shared.DataBlock( );
 data.Names = allNames;
-data.ExtendedRange = extendedRange;
-data.YXEPG = requestData(this, databankInfo, inputDatabank, extendedRange, allNames);
+data.ExtendedRange = extRange;
 
-numExtendedPeriods = numel(extendedRange);
-inxBaseRangeColumns = true(1, numExtendedPeriods);
+if isa(inputData, 'shared.DataBlock')
+    data.YXEPG = hereGetDataFromDataBlock( );
+else
+    data.YXEPG = hereGetDataFromDatabank( );
+end
+
+inxBaseRangeColumns = true(1, numExtPeriods);
 inxBaseRangeColumns(1:abs(maxLag)) = false;
 inxBaseRangeColumns = fliplr(inxBaseRangeColumns);
 inxBaseRangeColumns(1:abs(maxLead)) = false;
 inxBaseRangeColumns = fliplr(inxBaseRangeColumns);
 data.BaseRangeColumns = find(inxBaseRangeColumns);
 
-varargout = {data, maxLag, maxLead};
+return
 
+    function YXEPG = hereGetDataFromDatabank( )
+        scalarAllowed = @all;
+        databankInfo = checkInputDatabank( ...
+            this, inputData, extRange, ...
+            requiredNames, optionalNames, context, ...
+            scalarAllowed ...
+        );
+        YXEPG = requestData(this, databankInfo, inputData, extRange, allNames);
+    end%
+
+
+    function YXEPG = hereGetDataFromDataBlock( )
+        numPages = size(inputData.YXEPG, 3);
+        numAllNames = numel(allNames);
+        YXEPG = nan(numAllNames, numExtPeriods, numPages);
+        allNames = string(allNames);
+        namesInputData = string(inputData.Names);
+        for i = 1 : numAllNames
+            inx = allNames(i)==namesInputData;
+            if any(inx)
+                YXEPG(i, :, :) = inputData.YXEPG(inx, :, :);
+            end
+        end
+    end%
 end%
 
 
 
 
-%
 % Unit Tests
-%(
-function tests = unitTests( )
-    tests = functiontests({
-        @setupOnce 
-        @getDataBlockTest
-    });
-    tests = reshape(tests, [ ], 1);
-end%
+%{
+##### SOURCE BEGIN #####
+% saveAs=Explanatory/getDataBlockUnitTest.m
 
+testCase = matlab.unittest.FunctionTestCase.fromFunction(@(x)x);
 
-function setupOnce(testCase)
+% Set up once
     testCase.TestData.Model1 = Explanatory.fromString("log(x) = ?*a + b*x{-1} + ?*log(c) + ?*z{+1} - ? + d"); baseRange = qq(2001,1) : qq(2010,10);
     extendedRange = baseRange(1)-1 : baseRange(end)+1;
     db = struct( );
@@ -100,10 +107,10 @@ function setupOnce(testCase)
     testCase.TestData.BaseRange = baseRange;
     testCase.TestData.ExtendedRange = extendedRange;
     testCase.TestData.Databank = db;
-end%
 
 
-function getDataBlockTest(testCase)
+
+%% Test Plain Vanilla
     g = testCase.TestData.Model1;
     db = testCase.TestData.Databank;
     baseRange = testCase.TestData.BaseRange;
@@ -121,5 +128,6 @@ function getDataBlockTest(testCase)
     exp_YXEPG(6, :) = db.d(extendedRange);
     exp_YXEPG(7, :) = db.res_x(extendedRange);
     assertEqual(testCase, act.YXEPG, exp_YXEPG);
-end%
-%)
+
+##### SOURCE END #####
+%}
