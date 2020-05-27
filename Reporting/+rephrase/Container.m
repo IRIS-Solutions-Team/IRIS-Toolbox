bd36952c9d9edaf49785ca52f3a344cfd32a6fa1
Jaromir Benes <jaromir.benes@gmail.com>
2020-05-27 11:17:14 +0200

    Progress

--- Reporting/+rephrase/Container.m
+++ Reporting/+rephrase/Container.m
@@ -6,12 +6,17 @@ classdef Container ...
     end
 
 
+    properties
+        UnmatchedSettings = cell.empty(1, 0)
+    end
+
+
     methods
         function add(this, child)
             if ~any(child.Type==this.CanBeParentOf)
                 thisError = [
                     "Rephrase:InvalidChild"
-                    "Rephrase element of type %s cannot be added to %s."
+                    "Rephrase element of type %s cannot be added as a child to %s."
                 ];
                 throw(exception.Base(thisError, 'error'), string(child.Type), string(this.Type));
             end
