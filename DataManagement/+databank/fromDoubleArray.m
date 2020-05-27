a01fcc0078b7c647e0f2fca6d66d4a93d1242489
Jaromir Benes <jaromir.benes@gmail.com>
2020-05-27 11:44:21 +0200

    >>>>> Frontend: New +databank function fromDoubleArray

---
+++ DataManagement/+databank/fromDoubleArray.m
@@ -0,0 +1,26 @@
+function outputDb = fromDoubleArray(array, names, startDate, varargin)
+
+%( Input parser
+persistent pp
+if isempty(pp)
+    pp = extend.InputParser('+databank/fromDoubleArray');
+
+    addRequired(pp, 'array', @(x) isnumeric(x) && ndims(x)==2);
+    addRequired(pp, 'names', @(x) isstring(x) || ischar(x) || iscellstr(x));
+    addRequired(pp, 'startDate', @DateWrapper.validateProperDateInput);
+
+    addParameter(pp, 'Comments', [ ], @(x) isempty(x) || isstring(x) || ischar(x) || iscellstr(x));
+    addParameter(pp, 'OutputType', @default, @(x) isequal(x, @default) || validate.anyString(x, 'struct', 'Dictionary'));
+    addParameter(pp, 'AddToDatabank', false, @(x) isempty(x) || isequal(x, false) || validate.databank(x));
+end
+%)
+opt = parse(pp, array, names, startDate, varargin{:});
+
+%--------------------------------------------------------------------------
+
+outputDb = databank.backend.fromDoubleArrayNoFrills( ...
+    permute(array, [2, 1, 3:ndims(array)]) , names, startDate ...
+    , opt.Comments, @all, @default, opt.OutputType, opt.AddToDatabank ...
+);
+
+end%
