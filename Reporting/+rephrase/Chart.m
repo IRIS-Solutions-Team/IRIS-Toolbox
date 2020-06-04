35ec7e68ffb337049d16f0b6491045f1a1284283
Sergey Plotnikov <sergey.plotnikov@ogresearch.com>
2020-06-04 09:28:44 +0200

    highlight setting for the rephrase.Chart

--- Reporting/+rephrase/Chart.m
+++ Reporting/+rephrase/Chart.m
@@ -17,6 +17,16 @@ classdef Chart ...
             this.Content = cell.empty(1, 0);
             this.Settings.StartDate = DateWrapper.toIsoString(startDate, "s");
             this.Settings.EndDate = DateWrapper.toIsoString(endDate, "e");
+            if isfield(this.Settings, "Highlight")
+                for i=1:numel(this.Settings.Highlight)
+                    if isfield(this.Settings.Highlight{i},"StartDate")
+                        this.Settings.Highlight{i}.StartDate = DateWrapper.toIsoString(this.Settings.Highlight{i}.StartDate, "s");
+                    end
+                    if isfield(this.Settings.Highlight{i},"EndDate")
+                        this.Settings.Highlight{i}.EndDate = DateWrapper.toIsoString(this.Settings.Highlight{i}.EndDate, "e");
+                    end
+                end
+            end
         end%
     end
 end 
