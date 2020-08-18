708720d357564eef0c4802dd46fb7db6c21d8d70
Jaromir Benes <jaromir.benes@gmail.com>
2020-08-18 14:24:40 +0200

    Do not print log in Gradient; the derivatives are always w.r.t. plain variables.

--- Modeling/+model/+component/@Gradient/implementGet.m
+++ Modeling/+model/+component/@Gradient/implementGet.m
@@ -4,12 +4,12 @@ TYPE = @int8; %#ok<NASGU>
 response = [ ];
 isValid = true;
 
-if matches(query, ["Gradient", "Gradients"], "IgnoreCase", true)
+if startsWith(query, "Gradients", "IgnoreCase", true)
     response = this;
     numEquations = size(response.Dynamic, 2);
     for i = 1 : numEquations
-        response.Dynamic{2, i} = printVector(quantity, response.Dynamic{2, i}, "log()");
-        response.Steady{2, i} = printVector(quantity, response.Steady{2, i}, "log()");
+        response.Dynamic{2, i} = printVector(quantity, response.Dynamic{2, i}, "");
+        response.Steady{2, i} = printVector(quantity, response.Steady{2, i}, "");
     end
     response.Dynamic = transpose(response.Dynamic);
     response.Steady = transpose(response.Steady);
