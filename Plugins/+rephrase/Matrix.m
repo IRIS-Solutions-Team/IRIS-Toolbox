9f0e849ccc9ca4ff65f018b931661cd3c4163943
Jaromir Benes <jaromir.benes@gmail.com>
2020-09-03 09:04:30 +0200

    Fix missing +rephrase/Matrix element

---
+++ Plugins/+rephrase/Matrix.m
@@ -0,0 +1,18 @@
+classdef Matrix ...
+    < rephrase.Element ...
+    & rephrase.Terminus ...
+    & rephrase.Data
+
+    properties (Constant)
+        Type = rephrase.Type.MATRIX
+    end
+
+
+    methods
+        function this = Matrix(title, input, varargin)
+            this = this@rephrase.Element(title, varargin{:});
+            this.Content = input;
+        end%
+    end
+end 
+
