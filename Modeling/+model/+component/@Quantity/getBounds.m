949de8f063d0891fce654b2e7a6412c89133d876
Jaromir Benes <jaromir.benes@gmail.com>
2020-09-30 09:20:22 +0200

    >>>>> New option FiniteOnly in @Model/getBounds. Setting FiniteOnly=true returns a databank of bounds limited to those where at least one is a finite number.

--- Modeling/+model/+component/@Quantity/getBounds.m
+++ Modeling/+model/+component/@Quantity/getBounds.m
@@ -1,15 +1,16 @@
 function bounds = getBounds(this, varargin)
 
-try
-    finiteOnly = startsWith(varargin{1}, "finite", "ignoreCase", true);
-catch
-    finiteOnly = false;
+persistent pp
+if isempty(pp)
+    pp = extend.InputParser("@model.component.Quantity/getBounds");
+    addParameter(pp, "FiniteOnly", false, @validate.logicalScalar);
 end
+opt = parse(pp, varargin{:});
 
 bounds = struct( );
 names = reshape(string(this.Name), 1, [ ]);
 for i = 1 : numel(names)
-    if ~finiteOnly || ~all(isinf(this.Bounds(:, i)))
+    if ~opt.FiniteOnly || ~all(isinf(this.Bounds(:, i)))
         bounds.(names(i)) = this.Bounds(:, i);
     end
 end
