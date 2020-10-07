b67f4512180a1b6ef5d161251d02f7df2cb618c7
Jaromir Benes <jaromir.benes@gmail.com>
2020-10-07 16:10:11 +0200

    >>>>> Fix bug in databank.backend.resolveRange. The bug caused -Inf:date or date:Inf to behave as -Inf:Inf.

--- DataManagement/+databank/+backend/resolveRange.m
+++ DataManagement/+databank/+backend/resolveRange.m
@@ -1,4 +1,3 @@
-function [from, to] = resolveRange(inputDb, names, from, to)
 % resolveRange  Resolve start and end dates from databank time series
 %
 % Backend [IrisToolbox] method
@@ -7,7 +6,7 @@ function [from, to] = resolveRange(inputDb, names, from, to)
 % -[IrisToolbox] for Macroeconomic Modeling
 % -Copyright (c) 2007-2020 [IrisToolbox] Solutions Team
 
-%-------------------------------------------------------------------------- 
+function [from, to] = resolveRange(inputDb, names, from, to)
 
 if nargin<2
     names = @all;
@@ -34,13 +33,8 @@ else
 end
     
 if isinf(from) || isinf(to)
-    freq = @all;
-    if ~isinf(from)
-        freq = dater.getFrequency(from);
-    elseif ~isinf(to)
-        freq = dater.getFrequency(to);
-    end
-    range = databank.range(inputDb, "NameList=", names, "Frequency=", freq);
+    freq = locallyDetermineAndVerifyFrequency(from, to);
+    range = databank.range(inputDb, "nameList", names, "frequency", freq);
     if isempty(range)
         hereReportEmptyRange( );
     end
@@ -48,8 +42,12 @@ if isinf(from) || isinf(to)
         hereReportMultipleRange( );
     end
     range = double(range);
-    from = range(1);
-    to = range(end);
+    if isinf(from)
+        from = range(1);
+    end
+    if isinf(to)
+        to = range(end);
+    end
 end
 
 return
@@ -77,3 +75,68 @@ return
     end%
 end%
 
+%
+% Local Functions
+%
+
+function freq = locallyDetermineAndVerifyFrequency(from, to)
+    freq = @all;
+    freqFrom = [ ];
+    freqTo = [ ];
+    if ~isinf(from)
+        freqFrom = dater.getFrequency(from);
+    end
+    if ~isinf(to)
+        freqTo = dater.getFrequency(to);
+    end
+    if ~isempty(freqFrom) && ~isempty(freqTo) && ~isequal(freqFrom, freqTo)
+        exception.error([
+            "Databank:FrequencyMismatch"
+            "Frequency mismatch in the input data range: %s x %s"
+        ], Frequency.fromNumeric(freqFrom), Frequency.fromNumeric(freqTo));
+    end
+    if ~isempty(freqFrom)
+        freq = freqFrom;
+    elseif ~isempty(freqTo)
+        freq = freqTo;
+    end
+end%
+
+
+
+
+%
+% Unit Tests
+%
+%{
+##### SOURCE BEGIN #####
+% saveAs=databank/resolveRangeUnitTest.m
+
+testCase = matlab.unittest.FunctionTestCase.fromFunction(@(x)x);
+
+% Set up Once
+    d = struct( );
+    d.a = Series(qq(2001,1):qq(2010,4), 1);
+    d.b = Series(qq(2002,1):qq(2011,4), 1);
+    d.c = Series(mm(2001,1), 1);
+
+
+%% Test Names All Inf
+    [from, to] = databank.backend.resolveRange(d, "a", -Inf, Inf);
+    assertEqual(testCase, [from, to], [dater.qq(2001,1), dater.qq(2010,4)]);
+    %
+    [from, to] = databank.backend.resolveRange(d, ["a", "b"], -Inf, Inf);
+    assertEqual(testCase, [from, to], [dater.qq(2001,1), dater.qq(2011,4)]);
+
+
+%% Test Names Explicit Start
+    [from, to] = databank.backend.resolveRange(d, ["a", "b"], qq(2005,1), Inf);
+    assertEqual(testCase, [from, to], [dater.qq(2005,1), dater.qq(2011,4)]);
+
+
+%% Test Names Explicit End
+    [from, to] = databank.backend.resolveRange(d, ["a", "b"], -Inf, qq(2005,1));
+    assertEqual(testCase, [from, to], [dater.qq(2001,1), dater.qq(2005,1)]);
+
+##### SOURCE END #####
+%}
