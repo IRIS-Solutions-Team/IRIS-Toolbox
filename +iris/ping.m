c78b7d1453ea8428ce392b7a4d944cd1176b35bb
Jaromir Benes <jaromir.benes@gmail.com>
2020-12-10 21:28:58 +0100

    MPS test without loadNoReset

--- +iris/ping.m
+++ +iris/ping.m
@@ -1,17 +1,17 @@
 function ping()
 
-try
-    config = iris.Configuration.loadNoReset();
-catch
-    config = [];
-end
-
-if ~isa(config, 'iris.Configuration')
-    error(  ...
-        "IrisToolbox:ConfigurationDamaged" ...
-        , "Configuration data for [IrisToolbox] are not available and need to be reset." ...
-    );
-end
+% try
+    % config = iris.Configuration.loadNoReset();
+% catch
+    % config = [];
+% end
+% 
+% if ~isa(config, 'iris.Configuration')
+    % error(  ...
+        % "IrisToolbox:ConfigurationDamaged" ...
+        % , "Configuration data for [IrisToolbox] are not available and need to be reset." ...
+    % );
+% end
 
 end%
 
