46fff1b1fc08c8f86b7f38290b5177393a6a51f3
Jaromir Benes <jaromir.benes@gmail.com>
2020-12-16 12:57:04 +0100

    Typo in unit test in NumericTimeSubscriptable/roc

--- DataManagement/@NumericTimeSubscriptable/roc.m
+++ DataManagement/@NumericTimeSubscriptable/roc.m
@@ -102,3 +102,46 @@ end
 
 end%
 
+
+
+
+%
+% Unit Tests
+%
+%{
+##### SOURCE BEGIN #####
+% saveAs=Series/rocUnitTest.m
+
+testCase = matlab.unittest.FunctionTestCase.fromFunction(@(x)x);
+
+% Set up Once
+x = Series(qq(2020,1):qq(2024,4), @rand);
+
+%% Test Implicit -1
+rx1 = roc(x);
+rx2 = x/x{-1};
+assertEqual(testCase, rx1.Data, rx2.Data, "absTol", 1e-10);
+
+%% Test Explicit -4
+rx1 = roc(x, -4);
+rx2 = x/x{-4};
+assertEqual(testCase, rx1.Data, rx2.Data, "absTol", 1e-10);
+
+%% Test YoY
+rx1 = roc(x, "YoY");
+rx2 = x/x{-4};
+assertEqual(testCase, rx1.Data, rx2.Data, "absTol", 1e-10);
+
+%% Test EoPY
+rx1 = roc(x, "EoPY");
+rx2 = Series();
+for t = reshape(x.Range, 1, [])
+    t0 = qq(round(dater.getYearPeriodFrequency(t)-1), 4);
+    t
+    t0
+    rx2(t) = x(t) / x(t0);
+end
+assertEqual(testCase, rx1.Data, rx2.Data, "absTol", 1e-10);
+
+##### SOURCE END #####
+%}
