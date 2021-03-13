f5cdd92ea16eeaf02e4129e6916812ff3202fb8e
Jaromir Benes <jaromir.benes@gmail.com>
2021-03-13 09:12:23 +0100

    >>>>> New `@Model` method `printWithValue`. Print a model source file with the values of parameters and/or steady states of variables.

---
+++ DataManagement/+databank/@Chartpack/clear.m
@@ -0,0 +1,5 @@
+function this = clear(this, varargin)
+
+this.Charts = databank.chartpack.Chart.empty(1, 0);
+
+end%
---
+++ StructuralModeling/@Model/printWithValues.m
@@ -0,0 +1,55 @@
+function code = printWithValues(modelObject, modelFile, options)
+
+arguments
+    modelObject (1, 1) Model
+    modelFile (1, :) string
+    options.SaveAs (1, 1) string = ""
+    options.Parameters (1, 1) logical = true
+    options.Steady (1, 1) logical = true
+end
+
+modelFile = reshape(string(modelFile), 1, []);
+
+code = "";
+if isempty(modelFile)
+    return
+end
+
+code = string(fileread(modelFile(1)));
+
+if numel(modelFile)>1
+    lineBreak = string(newline());
+    for n = modelFile(2:end)
+        code = code + lineBreak + lineBreak + lineBreak + string(fileread(n));
+    end
+end
+
+types = modelObject.Quantity.Type;
+names = string(modelObject.Quantity.Name);
+values = modelObject.Variant.Values;
+
+selectTypes = [];
+if options.Parameters
+    selectTypes = [selectTypes, 4];
+end
+if options.Steady
+    selectTypes = [selectTypes, 1, 2];
+end
+inxSelect = ismember(types, selectTypes);
+
+for i = find(inxSelect)
+    name = names(i);
+    if ~contains(code, name)
+        continue
+    end
+    value = reshape(values(1, i, :), 1, []);
+    valueString = "<" + join(replace(string(value), " ", ""), ", ") + ">";
+    code = regexprep(code, "\<" + name + "\>(\{[^\}]*\})?", name + "$1" + valueString);
+end
+
+if ~isempty(options.SaveAs) && strlength(options.SaveAs)>0
+    writematrix(code, options.SaveAs, "fileType", "text", "quoteStrings", false);
+end
+
+end%
+
