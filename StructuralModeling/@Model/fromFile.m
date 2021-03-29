df466cccc70e425bd75371da46758dde6ed7a4b9
Jaromir Benes <jaromir.benes@gmail.com>
2021-03-29 14:11:19 +0200

    >> * Introduced static constructors for `Model` objects: >>     * `Model.fromFile` works the same way as `Model` but should be used instead >>.    * `Model.fromSnippet` allows creating the model from a model code snippet included directly in the very m-file

---
+++ StructuralModeling/@Model/fromFile.m
@@ -0,0 +1,11 @@
+% Type `web Model/fromFile.md` to get help on this function
+%
+% -[IrisToolbox] for Macroeconomic Modeling
+% -Copyright (c) 2007-2020 [IrisToolbox] Solutions Team
+
+function varargout = fromFile(varargin)
+
+[varargout{1:nargout}] = Model(varargin{:});
+
+end%
+
---
+++ StructuralModeling/@Model/fromSnippet.m
@@ -0,0 +1,94 @@
+% Type `web Model/fromSnippet.md` to get help on this function
+%
+% -[IrisToolbox] for Macroeconomic Modeling
+% -Copyright (c) 2007-2020 [IrisToolbox] Solutions Team
+
+function varargout = fromSnippet(snippetName, args)
+
+arguments
+    snippetName (1, :) string {mustBeNonempty}
+end
+
+arguments (Repeating)
+    args
+end
+
+OPEN_SNIPPET = ">>>";
+CLOSE_SNIPPET = "<<<";
+
+stack = dbstack("-completenames");
+callerFileName = stack(2).file;
+callerCode = string(fileread(callerFileName));
+
+inputModelFiles = model.File.empty(1, 0);
+snippetName = reshape(strip(snippetName), 1, []);
+for n = snippetName
+    open = n + OPEN_SNIPPET;
+    close =  CLOSE_SNIPPET + n;
+    snippet = extractBetween(callerCode, "% " + open, "% " + close);
+    snippet = regexprep(snippet, "^%", "", "lineAnchors");   
+    if isempty(snippet)
+        snippet = extractBetween(callerCode, open, close);
+    end
+    if ~isscalar(snippet)
+        hereThrowError();
+    end
+
+    mf = model.File;
+    mf.FileName = callerFileName + "#" + n;
+    mf.Code = char(snippet);
+    inputModelFiles(end+1) = mf;
+end
+
+[varargout{1:nargout}] = Model(mf, args{:});
+
+return
+
+    function hereThrowError()
+        if isempty(snippet)
+            exception.error([
+                "Model:NoSnippetFound"
+                "No snippet of code named %s found in %s"
+            ], snippetName, callerFileName);
+        else
+            exception.error([
+                "Model:NoSnippetFound"
+                "Multiple snippets of code named %s found in %s"
+            ], snippetName, callerFileName);
+        end
+    end%
+end%
+
+
+
+
+%
+% Unit tests
+%
+%{
+##### SOURCE BEGIN #####
+% saveAs=Model/fromSnippetUnitTest.m
+
+testCase = matlab.unittest.FunctionTestCase.fromFunction(@(x)x);
+
+%% Test plain vanilla
+
+m = Model.fromSnippet("test");
+% test>>>
+% !variables
+%     x
+% !shocks
+%     eps_x
+% !parameters
+%     rho_x
+% !equations
+%     x = rho_x*x{-1} + eps_x;
+% <<<test
+
+act = access(m, "equations");
+exp = "x=rho_x*x{-1}+eps_x;";
+assertEqual(testCase, act, exp);
+
+##### SOURCE END #####
+%}
+
