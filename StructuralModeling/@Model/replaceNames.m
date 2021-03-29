b347f7e909119d54696115102d2d798d077815a8
Jaromir Benes <jaromir.benes@gmail.com>
2021-03-29 14:12:59 +0200

    >> * Added new method `Model/replaceNames` to rename all or some model names (variables, parameters, shocks).

---
+++ StructuralModeling/@Model/replaceNames.m
@@ -0,0 +1,9 @@
+% Type `web Model/replaceNames.md` for help on this function
+%
+% -[IrisToolbox] for Macroeconomic Modeling
+% -Copyright (c) 2007-2020 [IrisToolbox] Solutions Team
+
+function this = replaceNames(this, varargin)
+this.Quantity = replaceNames(this.Quantity, varargin{:});
+end%
+
