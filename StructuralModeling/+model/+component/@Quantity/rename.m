cfba82d409efaa0aac7b79e8ed4e2f7c13117592
Jaromir Benes <jaromir.benes@gmail.com>
2021-04-26 21:01:17 +0200

    >> Created two mechanisms for renaming model names in `Model/rename`: `pair` and `list`

--- StructuralModeling/+model/+component/@Quantity/rename.m
+++ StructuralModeling/+model/+component/@Quantity/rename.m
@@ -1,77 +1,58 @@
 function this = rename(this, method, varargin)
 
 % >=R2019b
+%(
 arguments
     this model.component.Quantity
     method (1, 1) string {mustBeMember(method, ["pair", "list"])}
 end
+%)
 % >=R2019b
 
+stringify = @(x) reshape(string(x), 1, []);
+
 if isempty(this.OriginalNames)
-    this.OriginalNames = this.Name;
+    this.OriginalNames = stringify(this.Name);
 end
 
-%--------------------------------------------------------------------------
-
-renamePairs = cell(length(varargin), 2);
-validSpec = true(size(varargin));
-validOriginalNames = true(size(varargin));
-validNewName = true(size(varargin));
-newNames = this.Name;
-for i = 1 : numel(varargin)
-    ithRename = varargin{i};
-    match = regexp(varargin{i}, '\w+', 'match');
-    validSpec(i) = numel(match)==2;
-    if ~validSpec(i)
-        continue
-    end
-
-    renamePairs(i, 1:2) = match(:);
-    ell = lookup(this, renamePairs{i, 1});
-    indexOriginalNames = ell.IxName;
-    validOriginalNames(i) = any(indexOriginalNames);
-    if ~validOriginalNames(i)
-        continue
-    end
-    
-    validNewName(i) = isstrprop(renamePairs{i, 2}(1), 'alpha');
-    if ~validNewName(i)
-        continue
-    end
-
-    newNames{indexOriginalNames} = renamePairs{i, 2};
+if method=="pair"
+    [oldNames, newNames] = locallyListsFromPairs(this, varargin{:});
+else
+    oldNames = stringify(varargin{1});
+    newNames = stringify(varargin{2});
 end
 
-assert( ...
-    all(validSpec), ...
-    'model:component:Quantity:rename:IllegalSpec', ...
-    'Illegal rename specification: %s \n', ...
-    varargin{~validSpec} ...
-);
+oldNames = strip(oldNames);
+newNames = strip(newNames);
 
-assert( ...
-    all(validOriginalNames), ...
-    'model:component:Quantity:rename:IllegalOriginalNames', ...
-    'This name does not exist: %s \n', ...
-    renamePairs{~validOriginalNames, 1} ...
-);
+pos = lookupNames(this, oldNames, "error", []);
+this.Name(pos) = newNames;
+validateNames(this);
 
-assert( ...
-    all(validNewName), ...
-    'model:component:Quantity:rename:IllegalNewName', ...
-    'This is not a valid name: %s \n', ....
-    renamePairs{~validNewName, 2} ...
-);
+end%
 
-[flag, duplicateNames] = textual.nonunique(newNames);
-if flag
-    THIS_ERROR = { 'Model:Component:Quantity:DuplicateNames'
-                   'This is a duplicate name after renaming: %s \n' };
-    throw( exception.Base(THIS_ERROR, 'error'), ...
-           duplicateNames{:} );
-end
+%
+% Local functions
+%
 
-this.Name = newNames;
+function [oldNames, newNames] = locallyListsFromPairs(this, varargin)
+    %(
+    renamePairs = cell(numel(varargin), 2);
 
+    numPairs = numel(varargin);
+    oldNames = repmat("", 1, numPairs);
+    newNames = repmat("", 1, numPairs);
+
+    for i = 1 : numPairs
+        pair = string(varargin{i});
+        if numel(pair)==1
+            match = regexp(pair, "\w+", "match");
+            oldNames(i) = match(1);
+            newNames(i) = match(2);
+        else
+            oldNames(i) = pair(1);
+            newNames(i) = pair(2);
+        end
+    end
 end%
 
