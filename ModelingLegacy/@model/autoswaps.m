30a2d19f6231b9faafb6874b6f128517e6ef6ffa
Jaromir Benes <jaromir.benes@gmail.com>
2021-04-26 20:09:30 +0200

    Rename Model/autoswap -> Model/autoswaps; keep the old name for bkw compatibility

--- ModelingLegacy/@model/autoswaps.m
+++ ModelingLegacy/@model/autoswaps.m
@@ -1,14 +1,13 @@
-function varargout = autoswap(this, varargin)
-% autoswap  Inquire about or assign autoswap pairs
+% autoswaps  Inquire about or assign autoswap pairs
 %{
 % ## Syntax for Inquiring About Autoswap Pairs ##
 %
-%     a = autoswap(model)
+%     a = autoswaps(model)
 %
 %
 % ## Syntax for Assigning Autoswap Pairs ##
 %
-%     model = autoswap(model, a)
+%     model = autoswaps(model, a)
 %
 %
 % ## Input Arguments ##
@@ -41,16 +40,16 @@ function varargout = autoswap(this, varargin)
 %
 %}
 
-% -IRIS Macroeconomic Modeling Toolbox
-% -Copyright (c) 2007-2020 IRIS Solutions Team
+% -[IrisToolbox] for Macroeconomic Modeling
+% -Copyright (c) 2007-2020 [IrisToolbox] Solutions Team
 
-%--------------------------------------------------------------------------
+function varargout = autoswaps(this, varargin)
 
 if isempty(varargin)
     % ## Get Autoswap Structure ##
     auto = model.component.AutoswapStruct( );
-    [~, ~, auto.Simulate] = model.component.Pairing.getAutoswap(this.Pairing.Autoswap.Simulate, this.Quantity);
-    [~, ~, auto.Steady] = model.component.Pairing.getAutoswap(this.Pairing.Autoswap.Steady, this.Quantity);
+    [~, ~, auto.Simulate] = model.component.Pairing.getAutoswaps(this.Pairing.Autoswaps.Simulate, this.Quantity);
+    [~, ~, auto.Steady] = model.component.Pairing.getAutoswaps(this.Pairing.Autoswaps.Steady, this.Quantity);
     varargout{1} = auto;
 
 else
@@ -65,14 +64,14 @@ else
         auto.Steady = auto.steady;
     end
     if isfield(auto, 'Simulate') 
-        p = this.Pairing.Autoswap.Simulate;
-        setType(auto.Simulate, p, 'Simulate');
-        this.Pairing.Autoswap.Simulate = p;
+        p = this.Pairing.Autoswaps.Simulate;
+        locallySetType(auto.Simulate, p, 'Simulate');
+        this.Pairing.Autoswaps.Simulate = p;
     end
     if isfield(auto, 'Steady')
-        p = this.Pairing.Autoswap.Steady;
-        setType(auto.Steady, p, 'Steady');
-        this.Pairing.Autoswap.Steady = p;
+        p = this.Pairing.Autoswaps.Steady;
+        locallySetType(auto.Steady, p, 'Steady');
+        this.Pairing.Autoswaps.Steady = p;
     end
     varargout{1} = this;
 
@@ -80,14 +79,15 @@ end
 
 return
 
-
-    function p = setType(auto, p, type)
-            namesOfExogenized = fieldnames(auto);
-            namesOfExogenized = transpose(namesOfExogenized(:));
-            namesOfEndogenized = struct2cell(auto);
-            namesOfEndogenized = transpose(namesOfEndogenized(:));
-            p = model.component.Pairing.setAutoswap( p, type, this.Quantity, ...
-                                                     namesOfExogenized, namesOfEndogenized );
+    function p = locallySetType(auto, p, type)
+            namesExogenized = fieldnames(auto);
+            namesExogenized = transpose(namesExogenized(:));
+            namesEndogenized = struct2cell(auto);
+            namesEndogenized = transpose(namesEndogenized(:));
+            p = model.component.Pairing.setAutoswaps( ...
+                p, type, this.Quantity ...
+                , namesExogenized, namesEndogenized ...
+            );
     end%
 end%
 
