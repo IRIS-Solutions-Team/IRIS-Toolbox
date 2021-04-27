9deede5bd4314c302be175f44201968d4616458a
Jaromir Benes <jaromir.benes@gmail.com>
2021-04-27 09:20:01 +0200

    << Fix #301. Reset minimum Matlab requirements to R2019b. >> New iris.startup option: `--noMatlabCheck` skips Matlab version check.

--- +iris/startup.m
+++ +iris/startup.m
@@ -64,11 +64,12 @@
 
 function startup(varargin)
 
-MINIMUM_MATLAB = 'R2016b';
+MINIMUM_MATLAB = 'R2019b';
 
-%--------------------------------------------------------------------------
+options = locallyResolveInputOptions(varargin{:});
 
-if locallyGetMatlabRelease( )<locallyReleaseToNum(MINIMUM_MATLAB)
+if options.CheckMatlab ...
+        && locallyGetMatlabRelease()<locallyReleaseToNum(MINIMUM_MATLAB)
     error( ...
         "IrisToolbox:StartupError" ...
         , "Matlab %s or later is needed to run this [IrisToolbox] release" ...
@@ -76,10 +77,8 @@ if locallyGetMatlabRelease( )<locallyReleaseToNum(MINIMUM_MATLAB)
     );
 end
 
-options = locallyResolveInputOptions(varargin{:});
-
 % Reset path, remove other root folders
-[root, ~, rootsRemoved] = iris.path( );
+[root, ~, rootsRemoved] = iris.path();
 
 % Reset default function options and configuration options
 % Check [IrisToolbox] release and id file
@@ -91,13 +90,13 @@ config = iris.reset( ...
 );
 
 if ~options.Silent
-    hereDisplayIntro( );
-    hereDisplayDetails( );
+    hereDisplayIntro();
+    hereDisplayDetails();
 end
 
 return
 
-    function hereDisplayIntro( )
+    function hereDisplayIntro()
         release = config.Release;
         % Intro message
         fprintf('\n');
@@ -111,15 +110,19 @@ return
     end%
         
 
-    function hereDisplayDetails( )
+    function hereDisplayDetails()
+        % Matlab requirements
+        fprintf("\tMatlab requirements: %s or later", MINIMUM_MATLAB);
+        fprintf("\n");
+
         % IRIS root folder
-        fprintf('\tRoot Folder: %s', root);
+        fprintf('\tRoot folder: %s', root);
         fprintf('\n');
         
         % Default time series constructor
         defaultTimeSeriesConstructor = config.DefaultTimeSeriesConstructor;
         defaultTimeSeriesConstructor = func2str(defaultTimeSeriesConstructor);
-        fprintf('\tDefault Time Series Constructor: @%s', defaultTimeSeriesConstructor);
+        fprintf('\tDefault time series constructor: @%s', defaultTimeSeriesConstructor);
         fprintf('\n');
         
         % LaTeX engine
@@ -135,7 +138,7 @@ return
         if strlength(ghost)==0
             ghost = 'No Ghostscript engine configured';
         end
-        fprintf('\tGhostscript Engine: %s', ghost);
+        fprintf('\tGhostscript engine: %s', ghost);
         fprintf('\n');
         
         % X12/X13 version
@@ -158,7 +161,7 @@ return
 end%
 
 
-function r = locallyGetMatlabRelease( )
+function r = locallyGetMatlabRelease()
     r = uint16(0);
     try %#ok<TRYNC>
         s = ver('MATLAB');
@@ -190,7 +193,8 @@ end%
 
 
 function options = locallyResolveInputOptions(varargin)
-    options = struct( );
+    options = struct();
+    options.CheckMatlab = true;
     options.Silent = false;
     options.SeriesConstructor = @Series;
     options.CheckId = true;
@@ -202,16 +206,18 @@ function options = locallyResolveInputOptions(varargin)
     inputOptions = string(varargin);
     inputOptions = strip(erase(inputOptions, "-"));
     for n = inputOptions
-        if contains(n, ["Shutup", "Silent"], "IgnoreCase", true)
+        if contains(n, ["Shutup", "Silent"], "ignoreCase", true)
             options.Silent = true;
         elseif contains(n, "tseries")
             options.SeriesConstructor = @tseries;
         elseif contains(n, "Series")
             options.SeriesConstructor = @Series;
-        elseif contains(n, ["NoIdChk", "NoIdCheck"], "IgnoreCase", true)
+        elseif contains(n, ["NoIdChk", "NoIdCheck"], "ignoreCase", true)
             options.CheckId = false;
-        elseif contains(n, "NoTex", "IgnoreCase", true)
+        elseif contains(n, "NoTex", "ignoreCase", true)
             options.TeX = false;
+        elseif contains(n, "NoMatlabCheck", "ignoreCase", true)
+            options.CheckMatlab = false;
         end
     end
 end%
