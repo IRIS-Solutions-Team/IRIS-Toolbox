0658675a9513492bfc6f515479249126635be9c1
Jaromir Benes <jaromir.benes@gmail.com>
2021-05-24 10:17:49 +0200

    Added missing semicolons

--- StructuralModeling/+model/+component/@Quantity/initializeLogStatus.m
+++ StructuralModeling/+model/+component/@Quantity/initializeLogStatus.m
@@ -53,7 +53,7 @@ testCase = matlab.unittest.FunctionTestCase.fromFunction(@(x)x);
 %% Test Default False
 
 q = model.component.Quantity;
-q.Name = ["a", "b", "c", "d", "ea", "eb", "g", "ttrend"]
+q.Name = ["a", "b", "c", "d", "ea", "eb", "g", "ttrend"];
 q.Type = [1, 2, 2, 2, 3, 3, 5, 5];
 q.IxLog = false(size(q.Name));
 log = ["a", "b", "g"];
@@ -64,7 +64,7 @@ assertEqual(testCase, q.IxLog, [true, true, false, false, false, false, true, fa
 %% Test Default True
 
 q = model.component.Quantity;
-q.Name = ["a", "b", "c", "d", "ea", "eb", "g", "ttrend"]
+q.Name = ["a", "b", "c", "d", "ea", "eb", "g", "ttrend"];
 q.Type = [1, 2, 2, 2, 3, 3, 5, 5];
 q.IxLog = false(size(q.Name));
 log = Except(["a", "b", "g"]);
