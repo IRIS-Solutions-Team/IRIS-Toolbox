5a8ad2e883ffc35a434eeefc6af46a03a1d9a681
Jaromir Benes <jaromir.benes@gmail.com>
2021-06-03 16:24:21 +0200

    << * Fixed a typo in the legacy version of `databank.apply`. The typo prevented the function from running.

--- DataManagement/+databank/apply.m
+++ DataManagement/+databank/apply.m
@@ -66,11 +66,6 @@ end
 % >=R2019b
 
 
-if validate.databank(func)
-    [func, inputDb] = deal(inputDb, func);
-end
-
-
 % <=R2019a
 %{
 function [outputDb, appliedToNames, newNames] = apply(inputDb, func, varargin)
@@ -78,8 +73,8 @@ function [outputDb, appliedToNames, newNames] = apply(inputDb, func, varargin)
 persistent pp
 if isempty(pp)
     pp = extend.InputParser('databank.apply');
-    pp.addRequired('Function', @(x) isempty(x) || isa(x, 'function_handle'));
-    pp.addRequired('InputDatabank', @validate.databank);
+    pp.addRequired('inputDb', @locallyValidateInputDbOrFunc);
+    pp.addRequired('func', @locallyValidateInputDbOrFunc);
 
     pp.addParameter({'StartsWith', 'HasPrefix'}, '',  @(x) ischar(x) || (isa(x, 'string') && isscalar(x)));
     pp.addParameter({'EndsWith', 'HasSuffix'}, '',  @(x) ischar(x) || (isa(x, 'string') && isscalar(x)));
@@ -92,10 +87,14 @@ if isempty(pp)
     pp.addParameter({'TargetNames', 'OutputNames'}, @default, @(x) isequal(x, @default) || validate.list(x));
     pp.addParameter({'AddToDatabank', 'TargetDb'}, @default, @(x) isequal(x, @default) || validate.databank(x));
 end
-opt = pp.parse(func, inputDb, varargin{:});
+opt = pp.parse(inputDb, func, varargin{:});
 %}
 % <=R2019a
 
+
+if validate.databank(func)
+    [func, inputDb] = deal(inputDb, func);
+end
 
 if ~isa(opt.SourceNames, 'function_handle')
     if isa(opt.SourceNames, 'Rxp')
@@ -237,3 +236,55 @@ function locallyValidateDb(input)
     error("Validation:Failed", "Input value must be a struct or a Dictionary");
 end%
 
+
+
+
+%
+% Unit tests
+%{
+##### SOURCE BEGIN #####
+% saveAs=databank/applyUnitTest.m
+
+testCase = matlab.unittest.FunctionTestCase.fromFunction(@(x)x);
+
+d1 = struct();
+d1.x = Series(1:10, 1);
+d1.y = 1;
+d1.z = "aaa";
+
+%% Test plain vanilla 
+
+func = @(x) x + 1;
+d2 = databank.apply(d1, func);
+d3 = databank.apply(func, d1);
+%
+for n = databank.fieldNames(d1)
+    field1 = d1.(n);
+    field2 = d2.(n);
+    field3 = d3.(n);
+    if isa(field1, 'Series')
+        field1 = field1(:);
+        field2 = field2(:);
+        field3 = field3(:);
+    end
+    assertEqual(testCase, func(field1), field2);
+    assertEqual(testCase, func(field1), field3);
+end
+
+
+%% Test SourceNames
+
+sourceNames = ["x", "y"];
+func = @(x) x + 1;
+d2 = databank.apply(d1, func, "sourceNames", sourceNames, "addToDatabank", struct());
+d3 = databank.apply(func, d1, "sourceNames", sourceNames, "addToDatabank", struct());
+%
+assertEqual(testCase, databank.fieldNames(d2), sourceNames);
+assertEqual(testCase, databank.fieldNames(d3), sourceNames);
+
+
+##### SOURCE END #####
+%}
+
+
+%}
