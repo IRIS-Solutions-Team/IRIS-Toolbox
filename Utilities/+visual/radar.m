b093564c211f606bc8fb5862d87b966927bc2bdb
Jaromir Benes <jaromir.benes@gmail.com>
2022-06-07 13:14:12 +0200

    >> * New utility `visual.radar` for plotting radar charts of vectors

---
+++ Utilities/+visual/radar.m
@@ -0,0 +1,49 @@
+
+function varargout = radar(X, varargin)
+
+persistent ip
+if isempty(ip)
+    ip = inputParser();
+    addParameter(ip, "AxesHandle", @polaraxes);
+    addParameter(ip, "AxesSettings", cell.empty(1, 0));
+    addParameter(ip, "ColumnNames", string.empty(1, 0));
+    addParameter(ip, "PlotSettings", cell.empty(1, 0));
+end
+parse(ip, varargin{:});
+opt = ip.Results;
+
+if isa(opt.AxesHandle, 'function_handle')
+    opt.AxesHandle = opt.AxesHandle();
+end
+
+X = double(X);
+numDirections = size(X, 1);
+theta = linspace(0, 2*pi, numDirections+1);
+grad = linspace(0, 360, numDirections+1);
+rho = [X(:, :); X(1, :)]
+
+nextPlot = get(opt.AxesHandle, "nextPlot");
+set(opt.AxesHandle, "nextPlot", "add");
+numColumns = size(rho, 2);
+plotHandles = gobjects(1, numColumns);
+for i = 1 : numColumns
+    plotHandles(1, i) = polarplot(opt.AxesHandle, theta, rho(:, i), opt.PlotSettings{:});
+end
+set(opt.AxesHandle, "nextPlot", nextPlot);
+
+set(opt.AxesHandle, "thetaTick", grad(1:end-1));
+if ~isempty(opt.ColumnNames)
+    set(opt.AxesHandle, "thetaTickLabels", opt.ColumnNames);
+end
+if ~isempty(opt.AxesSettings)
+    set(opt.AxesHandle, opt.AxesSettings{:});
+end
+
+if nargout==0
+    return
+end
+
+varargout = {plotHandles};
+
+end%
+
