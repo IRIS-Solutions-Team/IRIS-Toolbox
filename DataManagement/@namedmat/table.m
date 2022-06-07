73589c1b0c69f6b7531bd4d8940724078ed16932
Jaromir Benes <jaromir.benes@gmail.com>
2022-06-07 11:07:59 +0200

    Fixed compatibility with old matlab

--- DataManagement/@namedmat/table.m
+++ DataManagement/@namedmat/table.m
@@ -1,16 +1,40 @@
 function outputTable = table(this, varargin)
 
-data = double(this);
+    data = double(this);
 
-if ~isempty(varargin)
-    data = data(:, :, varargin{:});
-end
+    if ~isempty(varargin)
+        data = data(:, :, varargin{:});
+    end
 
-outputTable = array2table( ...
-    data ...
-    , "VariableNames", this.ColumnNames ...
-    , "RowNames", this.RowNames ...
-);
+    columnNames = cellstr(this.ColumnNames);
+    rowNames = cellstr(this.RowNames);
+
+    try
+        outputTable = array2table( ...
+            data ...
+            , "VariableNames", columnNames ...
+            , "RowNames", rowNames ...
+        );
+    catch
+        % Runs in old Matlab
+        columnNames = local_replaceShifts(columnNames);
+        rowNames = local_replaceShifts(rowNames);
+        outputTable = array2table( ...
+            data ...
+            , "VariableNames", columnNames ...
+            , "RowNames", rowNames ...
+        );
+    end
 
 end%
 
+%
+% Local functions
+%
+
+function names = local_replaceShifts(names)
+    %(
+    names = regexprep(names, '{\-(\d+)}', '_Tminus$1');
+    names = regexprep(names, '{\+?(\d+)}', '_Tplus$1');
+    %)
+end%
