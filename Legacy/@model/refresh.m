3a9e688b7e29847152415e6c800d92cedf6dfadf
Jaromir Benes <jaromir.benes@gmail.com>
2022-06-07 11:40:45 +0200

    Docs

--- Legacy/@model/refresh.m
+++ Legacy/@model/refresh.m
@@ -1,75 +1,41 @@
-function this = refresh(this, variantsRequested);
-% refresh  Refresh dynamic links
-%{
-% Syntax
-%--------------------------------------------------------------------------
-%
-%
-%     model = refresh(model)
-%
-%
-% Input Arguments
-%--------------------------------------------------------------------------
-%
-%
-% __`model`__ [ Model ] 
-% > Model object whose dynamic links will be refreshed.
-%
-%
-% Output Arguments
-%--------------------------------------------------------------------------
-%
-%
-% __`model`__ [ Model ] 
-% > Model object with dynamic links refreshed.
-%
-%
-% Description
-%--------------------------------------------------------------------------
-%
-%
-% Example
-%--------------------------------------------------------------------------
-%
-%}
 
 % -[IrisToolbox] for Macroeconomic Modeling
 % -Copyright (c) 2007-2022 [IrisToolbox] Solutions Team
 
-if ~any(this.Link)
-    return
-end
+function this = refresh(this, variantsRequested)
 
-nv = countVariants(this);
-try
-    if isequal(variantsRequested, Inf) || isequal(variantsRequested, @all)
+    if ~any(this.Link)
+        return
+    end
+
+    nv = countVariants(this);
+    try
+        if isequal(variantsRequested, Inf) || isequal(variantsRequested, @all)
+            variantsRequested = 1 : nv;
+        end
+    catch %#ok<CTCH>
         variantsRequested = 1 : nv;
     end
-catch %#ok<CTCH>
-    variantsRequested = 1 : nv;
-end
 
-%--------------------------------------------------------------------------
+    numQuantities = numel(this.Quantity);
 
-numQuantities = numel(this.Quantity);
+    % Get a 1-(numQuantities+numStdCorr)-nv matrix of quantities and stdcorrs
+    x = [
+        this.Variant.Values(:, :, variantsRequested), ...
+        this.Variant.StdCorr(:, :, variantsRequested)
+    ];
 
-% Get a 1-(numQuantities+numStdCorr)-nv matrix of quantities and stdcorrs
-x = [
-    this.Variant.Values(:, :, variantsRequested), ...
-    this.Variant.StdCorr(:, :, variantsRequested)
-];
+    % Permute from 1-numQuantities-nv to numQuantities-nv-1
+    x = permute(x, [2, 3, 1]);
 
-% Permute from 1-numQuantities-nv to numQuantities-nv-1
-x = permute(x, [2, 3, 1]);
+    x = refresh(this.Link, x);
 
-x = refresh(this.Link, x);
+    % Permute from (numQuantities+numStdCorr)-nv-1 to
+    % 1-(numQuantities+numStdCorr)-nv
+    x = ipermute(x, [2, 3, 1]);
 
-% Permute from (numQuantities+numStdCorr)-nv-1 to
-% 1-(numQuantities+numStdCorr)-nv
-x = ipermute(x, [2, 3, 1]);
-
-this.Variant.Values(:, :, variantsRequested) = x(:, 1:numQuantities, :);
-this.Variant.StdCorr(:, :, variantsRequested) = x(:, numQuantities+1:end, :);
+    this.Variant.Values(:, :, variantsRequested) = x(:, 1:numQuantities, :);
+    this.Variant.StdCorr(:, :, variantsRequested) = x(:, numQuantities+1:end, :);
 
 end%
 
