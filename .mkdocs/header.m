c40e8c1ebb57e705253a142e99e40a69459753c5
Jaromir Benes <jaromir.benes@gmail.com>
2022-08-17 09:24:59 +0200

    Updated md template, gitignore

--- .mkdocs/header.m
+++ .mkdocs/header.m
@@ -1,5 +1,8 @@
-% Type `ihelp ___.md` for help on this function
+
+%
+% Type `ihelp qualifier/function-name` for help on this function
 %
 % -[IrisToolbox] for Macroeconomic Modeling
 % -Copyright (c) 2007-2020 [IrisToolbox] Solutions Team
+%
 
--- StructuralModeling/@Model/simulate.m
+++ StructuralModeling/@Model/simulate.m
@@ -1,4 +1,11 @@
-% Type `web Model/simulate` for help on this function
+
+%
+% Type `ihelp Model/simulate` for help on this function
+%
+% -[IrisToolbox] for Macroeconomic Modeling
+% -Copyright (c) 2007-2020 [IrisToolbox] Solutions Team
+%
+
 
 %{
 ---
